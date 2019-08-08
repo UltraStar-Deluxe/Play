fad2b5a3059b088b92ba455dd39da8c5c559fe03
Andreas Stange <anst.dev@gmail.com>
2019-08-08 23:46:42 +0200

    added microphone demo using PitchTracker (licensed under Ms-PL)

---
+++ UltraStar Play/Assets/src/audio/MicrophoneDemo.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using Pitch;
+
+[RequireComponent(typeof(AudioSource))]
+public class MicrophoneDemo : MonoBehaviour
+{
+    private const int SAMPLE_RATE = 22050;
+    private const int BUFFER_STEPS = 10;
+    private const int BUFFER_SIZE = SAMPLE_RATE / BUFFER_STEPS;
+    private int bufferIndex;
+
+    public string micDeviceName = "Mikrofon (2- USB Microphone)";
+    public Text currentNoteLabel;
+    public bool playRecordedAudio;
+
+    private AudioSource micAudioSource;
+    private float[] audioClipData = new float[BUFFER_SIZE];
+
+    private PitchTracker pitchTracker;
+
+    void Start()
+    {
+        pitchTracker = new PitchTracker();
+        pitchTracker.PitchRecordHistorySize = 5;
+        pitchTracker.SampleRate = SAMPLE_RATE;
+        
+        micAudioSource = GetComponent<AudioSource>();
+
+        if(Microphone.devices.Length == 0) {
+            throw new Exception("No mic found");
+        }
+        foreach(var device in Microphone.devices) {
+            Debug.Log("Found mic: " + device);
+        }
+
+        if( Array.Exists( Microphone.devices, (device) => device.Equals(micDeviceName) ) ) {
+            Debug.Log($"Start recording with '{micDeviceName}'");
+            // Code for low-latency microphone input taken from
+            // https://support.unity3d.com/hc/en-us/articles/206485253-How-do-I-get-Unity-to-playback-a-Microphone-input-in-real-time-
+            // It seems that there is still a latency of more than 200ms, which is too much for real-time processing.
+            AudioSource audio = GetComponent<AudioSource>();
+            micAudioSource.clip = Microphone.Start(null, true, 1, SAMPLE_RATE);
+            micAudioSource.loop = true;
+            if(playRecordedAudio) {
+                while (!(Microphone.GetPosition(null) > 0)) { /* Busy waiting */ }
+                Debug.Log("Start playing... position is " + Microphone.GetPosition(null));
+                micAudioSource.Play();
+            }
+            pitchTracker.PitchDetected += new PitchTracker.PitchDetectedHandler(OnPitchDetected);
+        } else {
+            throw new Exception($"Microphone '{micDeviceName}' not found. Please, provide the name of a found microphone.");
+        }
+    }
+
+    void Update()
+    {
+        if(playRecordedAudio && !micAudioSource.isPlaying) {
+            micAudioSource.Play();
+        } else if(!playRecordedAudio && micAudioSource.isPlaying) {
+            micAudioSource.Stop();
+        }
+
+        // Fill buffer with raw sample data from microphone
+        micAudioSource.clip.GetData(audioClipData, BUFFER_SIZE);
+
+        // Detect the pitch of the sample
+        pitchTracker.ProcessBuffer(audioClipData, 0);
+    }
+
+    private void OnPitchDetected(PitchTracker sender, PitchTracker.PitchRecord pitchRecord)
+    {
+        // Show the note that has been detected
+        var midiNote = pitchRecord.MidiNote;
+        if(midiNote > 0) {
+            currentNoteLabel.text = "Note: "+MidiUtils.MidiNoteToAbsoluteName(midiNote);
+        } else {
+            currentNoteLabel.text = "Note: unkown";
+        }
+    }
+
+    void OnDestroy()
+    {
+        Microphone.End(micDeviceName);
+    }
+}
