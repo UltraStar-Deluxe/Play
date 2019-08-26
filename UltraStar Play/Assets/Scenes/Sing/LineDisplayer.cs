1c19aa4cd603475d170e7a14cbcd0e44999e195c
Andreas Stange <anst.dev@gmail.com>
2019-08-26 22:30:12 +0200

    draw lines and notes on lines

---
+++ UltraStar Play/Assets/Scenes/Sing/LineDisplayer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineDisplayer : MonoBehaviour
+{
+    public RectTransform LinePrefab;
+
+    public const int LineCount = SentenceDisplayer.NoteLineCount / 2;
+
+    void Start() {
+        // Create lines
+        for(var i = 0; i < LineCount; i++) {
+            CreateLine(i);
+        }
+    }
+
+    private void CreateLine(int index)
+    {
+        var line = Instantiate(LinePrefab);
+        line.SetParent(transform);
+
+        var anchor = new Vector2(0.5f, (float)((double)index / (double)LineCount));
+        line.anchorMin = anchor;
+        line.anchorMax = anchor;
+        line.anchoredPosition = Vector2.zero;
+    }
+}
