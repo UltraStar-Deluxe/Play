b63589e5b1c7d10c51609d9661b8fdb889588b24
Andreas Stange <anst.dev@gmail.com>
2019-09-06 10:46:40 +0200

    implemented I18N using properties files

---
+++ UltraStar Play/Assets/Editor/I18NManagerInspector.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(I18NManager))]
+public class I18NManagerInspector : EditorBase
+{
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        var myScript = target as I18NManager;
+        if (GUILayout.Button("Update All Translations"))
+        {
+            I18NText[] i18nTexts = GameObject.FindObjectsOfType<I18NText>();
+            Debug.Log($"I18NText instances in scene: {i18nTexts.Length}");
+            foreach (I18NText i18nText in i18nTexts)
+            {
+                i18nText.UpdateTranslation();
+                EditorUtility.SetDirty(i18nText);
+            }
+        }
+    }
+
+}
