93b26fe02715410b0808909db95759ac8d3d6bd8
Andreas Stange <anst.dev@gmail.com>
2019-10-04 17:22:28 +0200

    SingScene: show TimeBar for the song including the sentence positions of all voices
    SingScene: handle more keyboard shortcuts

--- UltraStar Play/Assets/Common/Model/Player/PlayerProfile.cs
+++ UltraStar Play/Assets/Common/Model/Player/PlayerProfile.cs
@@ -9,4 +9,5 @@ public class PlayerProfile
     public string Name { get; set; } = "New Player";
     public string MicDevice { get; set; } = "";
     public Difficulty Difficulty { get; private set; } = Difficulty.Medium;
+    public Color Color { get; set; } = new Color(0, 0.8f, 0, 1);
 }
---
+++ UltraStar Play/Assets/Scenes/Sing/TimeBar/TimeBarTimeLabel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class TimeBarTimeLabel : MonoBehaviour
+{
+    public bool showRemainingTime = true;
+
+    private Text text;
+    private SingSceneController singSceneController;
+
+    void OnEnable()
+    {
+        text = GetComponent<Text>();
+        singSceneController = SingSceneController.Instance;
+        InvokeRepeating("UpdateText", 0f, 1f);
+    }
+
+    void UpdateText()
+    {
+        SetText(singSceneController.PositionInSongInMillis, singSceneController.DurationOfSongInMillis);
+    }
+
+    private void SetText(double positionInSongInMillis, double durationOfSongInMillis)
+    {
+        if (showRemainingTime)
+        {
+            double remainingTimeInSeconds = (durationOfSongInMillis - positionInSongInMillis) / 1000;
+            SetText(remainingTimeInSeconds);
+        }
+        else
+        {
+            double positionInSongInSeconds = positionInSongInMillis / 1000;
+            SetText(positionInSongInSeconds);
+        }
+    }
+
+    private void SetText(double timeInSeconds)
+    {
+        int mins = (int)Math.Floor(timeInSeconds / 60);
+        string minsPadding = (mins < 10) ? "0" : "";
+        int secs = (int)Math.Floor(timeInSeconds % 60);
+        string secsPadding = (secs < 10) ? "0" : "";
+        text.text = minsPadding + mins + ":" + secsPadding + secs;
+    }
+}
