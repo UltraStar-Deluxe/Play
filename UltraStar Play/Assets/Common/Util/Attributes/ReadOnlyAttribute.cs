cc40d8a9c681b4a527a6a8eb9c97455d28d155ac
Andreas Stange <anst.dev@gmail.com>
2019-10-09 10:33:22 +0200

    added ReadOnly attribute to make public fields in the inspector unchangeable

---
+++ UltraStar Play/Assets/Common/Util/Attributes/ReadOnlyAttribute.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// Prevents a variable from being changed in the inspector.
+public class ReadOnlyAttribute : PropertyAttribute
+{
+
+}
---
+++ UltraStar Play/Assets/Editor/Drawers/ReadOnlyDrawer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+/// Draws fields that use the ReadOnlyAttribute but disables user editing beforehand.
+[CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+public class ReadOnlyDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position,
+        SerializedProperty property,
+        GUIContent label)
+    {
+        GUI.enabled = false;
+        EditorGUI.PropertyField(position, property, label, true);
+        GUI.enabled = true;
+    }
+}
