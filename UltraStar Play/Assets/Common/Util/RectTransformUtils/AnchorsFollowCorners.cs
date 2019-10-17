67d1f3801511ce8e0abf8159549c5165628d7f72
Andreas Stange <anst.dev@gmail.com>
2019-10-17 16:58:53 +0200

    code style: split very long lines

--- UltraStar Play/Assets/Common/Util/RectTransformUtils/AnchorsFollowCorners.cs
+++ UltraStar Play/Assets/Common/Util/RectTransformUtils/AnchorsFollowCorners.cs
@@ -42,8 +42,9 @@ public class AnchorsFollowCorners : MonoBehaviour
     {
         float pivotX = anchorRect.width * ownRectTransform.pivot.x;
         float pivotY = anchorRect.height * (1 - ownRectTransform.pivot.y);
-        Vector2 newXY = new Vector2(ownRectTransform.anchorMin.x * parentRectTransform.rect.width + ownRectTransform.offsetMin.x + pivotX - parentRectTransform.rect.width * anchorVector.x,
-                                  -(1 - ownRectTransform.anchorMax.y) * parentRectTransform.rect.height + ownRectTransform.offsetMax.y - pivotY + parentRectTransform.rect.height * (1 - anchorVector.y));
+        float newX = ownRectTransform.anchorMin.x * parentRectTransform.rect.width + ownRectTransform.offsetMin.x + pivotX - parentRectTransform.rect.width * anchorVector.x;
+        float newY = -(1 - ownRectTransform.anchorMax.y) * parentRectTransform.rect.height + ownRectTransform.offsetMax.y - pivotY + parentRectTransform.rect.height * (1 - anchorVector.y);
+        Vector2 newXY = new Vector2(newX, newY);
         anchorRect.x = newXY.x;
         anchorRect.y = newXY.y;
     }
@@ -60,13 +61,26 @@ public class AnchorsFollowCorners : MonoBehaviour
         float pivotY = anchorRect.height * (1 - ownRectTransform.pivot.y);
         ownRectTransform.anchorMin = new Vector2(0f, 1f);
         ownRectTransform.anchorMax = new Vector2(0f, 1f);
-        ownRectTransform.offsetMin = new Vector2(anchorRect.x / ownRectTransform.localScale.x, anchorRect.y / ownRectTransform.localScale.y - anchorRect.height);
-        ownRectTransform.offsetMax = new Vector2(anchorRect.x / ownRectTransform.localScale.x + anchorRect.width, anchorRect.y / ownRectTransform.localScale.y);
-        ownRectTransform.anchorMin = new Vector2(ownRectTransform.anchorMin.x + anchorVector.x + (ownRectTransform.offsetMin.x - pivotX) / parentRectTransform.rect.width * ownRectTransform.localScale.x,
-                                                 ownRectTransform.anchorMin.y - (1 - anchorVector.y) + (ownRectTransform.offsetMin.y + pivotY) / parentRectTransform.rect.height * ownRectTransform.localScale.y);
-        ownRectTransform.anchorMax = new Vector2(ownRectTransform.anchorMax.x + anchorVector.x + (ownRectTransform.offsetMax.x - pivotX) / parentRectTransform.rect.width * ownRectTransform.localScale.x,
-                                                 ownRectTransform.anchorMax.y - (1 - anchorVector.y) + (ownRectTransform.offsetMax.y + pivotY) / parentRectTransform.rect.height * ownRectTransform.localScale.y);
-        ownRectTransform.offsetMin = new Vector2((0 - ownRectTransform.pivot.x) * anchorRect.width * (1 - ownRectTransform.localScale.x), (0 - ownRectTransform.pivot.y) * anchorRect.height * (1 - ownRectTransform.localScale.y));
-        ownRectTransform.offsetMax = new Vector2((1 - ownRectTransform.pivot.x) * anchorRect.width * (1 - ownRectTransform.localScale.x), (1 - ownRectTransform.pivot.y) * anchorRect.height * (1 - ownRectTransform.localScale.y));
+
+        float offsetMinX = anchorRect.x / ownRectTransform.localScale.x;
+        float offsetMinY = anchorRect.y / ownRectTransform.localScale.y - anchorRect.height;
+        ownRectTransform.offsetMin = new Vector2(offsetMinX, offsetMinY);
+        float offsetMaxX = anchorRect.x / ownRectTransform.localScale.x + anchorRect.width;
+        float offsetMaxY = anchorRect.y / ownRectTransform.localScale.y;
+        ownRectTransform.offsetMax = new Vector2(offsetMaxX, offsetMaxY);
+
+        float anchorMinX = ownRectTransform.anchorMin.x + anchorVector.x + (ownRectTransform.offsetMin.x - pivotX) / parentRectTransform.rect.width * ownRectTransform.localScale.x;
+        float anchorMinY = ownRectTransform.anchorMin.y - (1 - anchorVector.y) + (ownRectTransform.offsetMin.y + pivotY) / parentRectTransform.rect.height * ownRectTransform.localScale.y;
+        ownRectTransform.anchorMin = new Vector2(anchorMinX, anchorMinY);
+        float anchorMaxX = ownRectTransform.anchorMax.x + anchorVector.x + (ownRectTransform.offsetMax.x - pivotX) / parentRectTransform.rect.width * ownRectTransform.localScale.x;
+        float anchorMaxY = ownRectTransform.anchorMax.y - (1 - anchorVector.y) + (ownRectTransform.offsetMax.y + pivotY) / parentRectTransform.rect.height * ownRectTransform.localScale.y;
+        ownRectTransform.anchorMax = new Vector2(anchorMaxX, anchorMaxY);
+
+        offsetMinX = (0 - ownRectTransform.pivot.x) * anchorRect.width * (1 - ownRectTransform.localScale.x);
+        offsetMinY = (0 - ownRectTransform.pivot.y) * anchorRect.height * (1 - ownRectTransform.localScale.y);
+        ownRectTransform.offsetMin = new Vector2(offsetMinX, offsetMinY);
+        offsetMaxX = (1 - ownRectTransform.pivot.x) * anchorRect.width * (1 - ownRectTransform.localScale.x);
+        offsetMaxY = (1 - ownRectTransform.pivot.y) * anchorRect.height * (1 - ownRectTransform.localScale.y);
+        ownRectTransform.offsetMax = new Vector2(offsetMaxX, offsetMaxY);
     }
 }
