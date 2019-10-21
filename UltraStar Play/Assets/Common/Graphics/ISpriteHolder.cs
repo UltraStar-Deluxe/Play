16b57300fcfdb898534bc4476ecdc6def7c39fad
Andreas Stange <anst.dev@gmail.com>
2019-10-21 22:25:03 +0200

    added ISpriteHolder to indicate that a script is using a sprite to avoid its removal of the ImageManager's cache

---
+++ UltraStar Play/Assets/Common/Graphics/ISpriteHolder.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+// Indicates a script that references a sprite, which has been loaded at runtime via the ImageManager.
+// When loading an image at runtime via the ImageManager for something different than a UnityEngine.UI.Image,
+// then this interface must be used to indicate where the Sprite is used in the scene.
+// Otherwise the Sprite may be considered as unused and could be removed from the ImageManger's cache to free memory.
+public interface ISpriteHolder
+{
+    Sprite GetSprite();
+}
