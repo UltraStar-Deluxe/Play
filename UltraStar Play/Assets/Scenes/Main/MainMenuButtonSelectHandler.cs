db8f79b99c4ecb796c195c0552d136ef3a15ed71
Andreas Stange <anst.dev@gmail.com>
2019-11-04 19:10:25 +0100

    MainScene: re-implemented design of USDX

---
+++ UltraStar Play/Assets/Scenes/Main/MainMenuButtonSelectHandler.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class MainMenuButtonSelectHandler : MonoBehaviour, ISelectHandler
+{
+    public string buttonDescription;
+    private ButtonDescription uiButtonDescriptionText;
+
+    void Start()
+    {
+        uiButtonDescriptionText = FindObjectOfType<ButtonDescription>();
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        uiButtonDescriptionText.SetText(buttonDescription);
+    }
+}
--- UltraStar Play/Assets/Scenes/Main/MainSceneController.cs
+++ UltraStar Play/Assets/Scenes/Main/MainSceneController.cs
@@ -1,7 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+using UnityEngine.EventSystems;
+using System;
 
 public class MainSceneController : MonoBehaviour
 {
+    public Button defaultButton;
+
+    void Start()
+    {
+        if (defaultButton != null)
+        {
+            defaultButton.Select();
+        }
+    }
+
+    public void QuitGame()
+    {
+        ApplicationUtils.QuitOrStopPlayMode();
+    }
 }
