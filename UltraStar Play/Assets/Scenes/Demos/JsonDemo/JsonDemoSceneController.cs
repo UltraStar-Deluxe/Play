7c73cd1c9b41079f7fea75db04b2a16abe8f9cd8
Andreas Stange <anst.dev@gmail.com>
2019-11-07 11:00:12 +0100

    added JsonDemoScene and SerializableDictionary

---
+++ UltraStar Play/Assets/Scenes/Demos/JsonDemo/JsonDemoSceneController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JsonDemoSceneController : MonoBehaviour
+{
+    public Text uiText;
+
+    public JsonDemoSerializable loadedInstance;
+
+    void Start()
+    {
+        JsonDemoSerializable root = new JsonDemoSerializable();
+        JsonDemoSerializable child = new JsonDemoSerializable();
+        root.otherSerializable = child;
+
+        string json = JsonUtility.ToJson(root);
+        uiText.text = json;
+
+        loadedInstance = JsonUtility.FromJson<JsonDemoSerializable>(json);
+        Debug.Log("loaded: " + JsonUtility.ToJson(loadedInstance));
+    }
+}
---
+++ UltraStar Play/Assets/Scenes/Demos/JsonDemo/StringToStringMap.cs
@@ -0,0 +1,9 @@
+
+using System;
+using System.Collections.Generic;
+
+[Serializable]
+public class StringToStringMap : SerializableDictionary<string, string>
+{
+
+}
