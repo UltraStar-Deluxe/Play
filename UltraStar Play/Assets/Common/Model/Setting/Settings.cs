fa137db7952fd4ea9eacc81f4600bfe090c4375a
Andreas Stange <anst.dev@gmail.com>
2019-11-11 21:44:25 +0100

    PlayerProfiles in Settings

--- UltraStar Play/Assets/Common/Model/Setting/Settings.cs
+++ UltraStar Play/Assets/Common/Model/Setting/Settings.cs
@@ -1,9 +1,20 @@
 
 using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 [Serializable]
 public class Settings
 {
     public GameSettings GameSettings { get; set; } = new GameSettings();
     public GraphicSettings GraphicSettings { get; set; } = new GraphicSettings();
+    public List<PlayerProfile> PlayerProfiles { get; set; } = CreateDefaultPlayerProfiles();
+
+    private static List<PlayerProfile> CreateDefaultPlayerProfiles()
+    {
+        List<PlayerProfile> result = new List<PlayerProfile>();
+        result.Add(new PlayerProfile("Player01", "Mikrofon (2- USB Microphone)", Difficulty.Medium, Color.cyan));
+        result.Add(new PlayerProfile("Player02", "", Difficulty.Easy, Color.magenta));
+        return result;
+    }
 }
