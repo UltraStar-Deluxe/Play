77378453cb25a08a9c285d5aaefcb0a7d46da1fc
Andreas Stange <anst.dev@gmail.com>
2019-11-11 21:49:37 +0100

    GraphicOptionsScene: FullScreenSlider and apply settings on click of the back button

---
+++ UltraStar Play/Assets/Scenes/Options/GraphicOptions/ApplyGraphicSettingsOnClick.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+
+[RequireComponent(typeof(Button))]
+public class ApplyGraphicSettingsOnClick : MonoBehaviour
+{
+    void Start()
+    {
+        Button button = GetComponent<Button>();
+        button.OnClickAsObservable().Subscribe(_ => ApplyGraphicSettings());
+    }
+
+    void ApplyGraphicSettings()
+    {
+        ScreenResolution res = SettingsManager.Instance.Settings.GraphicSettings.resolution;
+        FullScreenMode fullScreenMode = SettingsManager.Instance.Settings.GraphicSettings.fullScreenMode;
+        Screen.SetResolution(res.Width, res.Height, fullScreenMode, res.RefreshRate);
+    }
+}
