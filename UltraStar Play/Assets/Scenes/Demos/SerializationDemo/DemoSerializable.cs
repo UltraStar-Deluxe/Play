e73e85123c147645fc1855c1b6b212cf8c00e600
Andreas Stange <anst.dev@gmail.com>
2019-11-11 21:49:50 +0100

    updated serialization demo

--- UltraStar Play/Assets/Scenes/Demos/SerializationDemo/DemoSerializable.cs
+++ UltraStar Play/Assets/Scenes/Demos/SerializationDemo/DemoSerializable.cs
@@ -9,20 +9,23 @@ public class DemoSerializable
     public string name = "theName";
     public int intNumber = 42;
     public double doubleNumber = 42.5;
-    // LitJson does not support serialization of float values (but works in FullSerializer)
+    // float values are not serialized by LitJson
     public float floatNumber = 42.5f;
     public string textWithBackslash = @"C:\this\is\a\path.txt";
 
-    // A Dictionary is not serialized to JSON per default in Unity (but works in LitJson).
+    // A Dictionary is not serialized to JSON per default in Unity.
     // A Dictionary is not serialized using System.Xml.Serialization.
     public Dictionary<string, string> dict = new Dictionary<string, string>();
 
     public List<string> list = new List<string>();
 
-    // A nested Serializable is not serialized to JSON by Unity (but works in LitJson).
+    // A nested Serializable is not serialized to JSON by Unity.
     // A nested Serializable is not serialized using System.Xml.Serialization per default.
     [SerializeField]
     public DemoSerializable otherSerializable;
+
+    // PlayerProfile is perfectly (de)serialized using FullSerialized.
+    // public PlayerProfile playerProfile = new PlayerProfile();
 
     public DemoSerializable()
     {
