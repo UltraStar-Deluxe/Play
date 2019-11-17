23c8775ab7b0d13022cfe2e619c980b4d988c115
Andreas Stange <anst.dev@gmail.com>
2019-11-17 20:28:24 +0100

    create class with keys of i18n properties via menu item.
    use these i18n fields instead raw strings to have compiler checks.
    use these i18n keys in a drop-down for the I18NText custom inspector.

---
+++ UltraStar Play/Assets/Common/I18N/I18NKeys.cs
@@ -0,0 +1,39 @@
+// Generated code with the keys of the I18N properties.
+// To update this file use the corresponding menu item in the Unity Editor under 'I18N'.
+public static class I18NKeys {
+    public static readonly string cancel = "cancel";
+    public static readonly string close = "close";
+    public static readonly string difficulty_easy = "difficulty.easy";
+    public static readonly string difficulty_hard = "difficulty.hard";
+    public static readonly string difficulty_medium = "difficulty.medium";
+    public static readonly string exit = "exit";
+    public static readonly string i18n_demo_scene_hello = "i18n_demo_scene.hello";
+    public static readonly string i18n_demo_scene_love = "i18n_demo_scene.love";
+    public static readonly string i18n_demo_scene_multiline = "i18n_demo_scene.multiline";
+    public static readonly string load = "load";
+    public static readonly string loading = "loading";
+    public static readonly string main_scene_button_about_label = "main_scene.button.about.label";
+    public static readonly string main_scene_button_quit_label = "main_scene.button.quit.label";
+    public static readonly string new_ = "new";
+    public static readonly string no = "no";
+    public static readonly string ok = "ok";
+    public static readonly string open = "open";
+    public static readonly string rating_sentence_awful = "rating.sentence.awful";
+    public static readonly string rating_sentence_bad = "rating.sentence.bad";
+    public static readonly string rating_sentence_cool = "rating.sentence.cool";
+    public static readonly string rating_sentence_good = "rating.sentence.good";
+    public static readonly string rating_sentence_great = "rating.sentence.great";
+    public static readonly string rating_sentence_notbad = "rating.sentence.notbad";
+    public static readonly string rating_sentence_perfect = "rating.sentence.perfect";
+    public static readonly string rating_sentence_poor = "rating.sentence.poor";
+    public static readonly string rating_song_amateur = "rating.song.amateur";
+    public static readonly string rating_song_hopeful = "rating.song.hopeful";
+    public static readonly string rating_song_lead_singer = "rating.song.lead_singer";
+    public static readonly string rating_song_rising_star = "rating.song.rising_star";
+    public static readonly string rating_song_superstar = "rating.song.superstar";
+    public static readonly string rating_song_tone_deaf = "rating.song.tone_deaf";
+    public static readonly string rating_song_ultrastar = "rating.song.ultrastar";
+    public static readonly string rating_song_wannabe = "rating.song.wannabe";
+    public static readonly string save = "save";
+    public static readonly string yes = "yes";
+}
--- UltraStar Play/Assets/Scenes/Sing/PlayerController/PlayerUiController/SentenceRating.cs
+++ UltraStar Play/Assets/Scenes/Sing/PlayerController/PlayerUiController/SentenceRating.cs
@@ -3,16 +3,16 @@ using UnityEngine;
 
 public class SentenceRating
 {
-    public static readonly SentenceRating Perfect = new SentenceRating(0.95, "rating.sentence.perfect", "3AFF4E");
-    public static readonly SentenceRating Cool = new SentenceRating(0.8, "rating.sentence.cool", "7FE82A");
-    public static readonly SentenceRating Great = new SentenceRating(0.7, "rating.sentence.great", "7FE82A");
-    public static readonly SentenceRating Good = new SentenceRating(0.6, "rating.sentence.good", "E4FF1F");
-    public static readonly SentenceRating NotBad = new SentenceRating(0.5, "rating.sentence.notbad", "E4FF1F");
-    public static readonly SentenceRating Bad = new SentenceRating(0.3, "rating.sentence.bad", "FF9C4F");
-    public static readonly SentenceRating Poor = new SentenceRating(0.2, "rating.sentence.poor", "E848E6");
-    public static readonly SentenceRating Awful = new SentenceRating(0, "rating.sentence.awful", "764FFF");
+    public static readonly SentenceRating Perfect = new SentenceRating(0.95, I18NKeys.rating_sentence_perfect, "3AFF4E");
+    public static readonly SentenceRating Cool = new SentenceRating(0.8, I18NKeys.rating_sentence_cool, "7FE82A");
+    public static readonly SentenceRating Great = new SentenceRating(0.7, I18NKeys.rating_sentence_great, "7FE82A");
+    public static readonly SentenceRating Good = new SentenceRating(0.6, I18NKeys.rating_sentence_good, "E4FF1F");
+    public static readonly SentenceRating NotBad = new SentenceRating(0.5, I18NKeys.rating_sentence_notbad, "E4FF1F");
+    public static readonly SentenceRating Bad = new SentenceRating(0.3, I18NKeys.rating_sentence_bad, "FF9C4F");
+    public static readonly SentenceRating Poor = new SentenceRating(0.2, I18NKeys.rating_sentence_poor, "E848E6");
+    public static readonly SentenceRating Awful = new SentenceRating(0, I18NKeys.rating_sentence_awful, "764FFF");
 
-    private string i18nCode;
+    private readonly string i18nCode;
     public double PercentageThreshold { get; private set; }
 
     public string Text
