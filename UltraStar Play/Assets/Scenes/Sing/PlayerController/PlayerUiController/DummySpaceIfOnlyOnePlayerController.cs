d42ec9f6204c7b451c6ed730b4c55f8765a9c839
Andreas Stange <anst.dev@gmail.com>
2019-11-24 17:30:33 +0100

    SingScene: first player ui is at top, others are added below

--- UltraStar Play/Assets/Scenes/Sing/PlayerController/PlayerUiController/DummySpaceIfOnlyOnePlayerController.cs
+++ UltraStar Play/Assets/Scenes/Sing/PlayerController/PlayerUiController/DummySpaceIfOnlyOnePlayerController.cs
@@ -13,5 +13,9 @@ public class DummySpaceIfOnlyOnePlayerController : MonoBehaviour
         {
             Destroy(gameObject);
         }
+        else
+        {
+            transform.SetAsLastSibling();
+        }
     }
 }
