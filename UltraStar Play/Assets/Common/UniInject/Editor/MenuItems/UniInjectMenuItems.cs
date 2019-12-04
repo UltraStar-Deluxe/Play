823c1d17eaef2a7eca2e42ab3ce9455a678db190
Andreas Stange <anst.dev@gmail.com>
2019-12-04 18:35:01 +0100

    UniInject: implemented static (i.e. in the editor, not in play mode) check of current scene

--- UltraStar Play/Assets/Common/UniInject/Editor/MenuItems/UniInjectMenuItems.cs
+++ UltraStar Play/Assets/Common/UniInject/Editor/MenuItems/UniInjectMenuItems.cs
@@ -1,8 +1,6 @@
-using System;
-using System.Reflection;
+using System.Threading;
 using UnityEditor;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 namespace UniInject
 {
@@ -10,72 +8,16 @@ namespace UniInject
     {
 
         [MenuItem("UniInject/Check current scene &v")]
-        public static void CheckScene()
+        public static void CheckCurrentScene()
         {
-            bool hasErrors = false;
-            GameObject[] rootObjects = SceneManager.GetActiveScene().GetRootGameObjects();
-            foreach (GameObject rootObject in rootObjects)
-            {
-                MonoBehaviour[] scripts = rootObject.GetComponentsInChildren<MonoBehaviour>();
-                foreach (MonoBehaviour script in scripts)
-                {
-                    hasErrors |= CheckScript(script);
-                }
-            }
+            System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
+            stopwatch.Start();
 
-            if (!hasErrors)
-            {
-                Debug.Log("UniInject - No issues found in current scene. Yay!");
-            }
+            int errorCount = CheckingUtils.CheckCurrentScene();
+
+            stopwatch.Stop();
+            Debug.Log($"Scene check found {errorCount} issue(s) in {stopwatch.ElapsedMilliseconds} ms.");
         }
 
-        // Returns true if a problem has been found.
-        private static bool CheckScript(MonoBehaviour script)
-        {
-            bool hasErrors = false;
-
-            Type type = script.GetType();
-            MemberInfo[] memberInfos = type.GetMembers(BindingFlags.Public
-                                                        | BindingFlags.NonPublic
-                                                        | BindingFlags.Instance);
-            foreach (MemberInfo memberInfo in memberInfos)
-            {
-                hasErrors |= CheckInjectedInInspectorAttribute(script, type, memberInfo);
-            }
-
-            return hasErrors;
-        }
-
-        // Returns true if a problem has been found.
-        private static bool CheckInjectedInInspectorAttribute(MonoBehaviour script, Type type, MemberInfo memberInfo)
-        {
-            InjectedInInspectorAttribute attribute = memberInfo.GetCustomAttribute<InjectedInInspectorAttribute>();
-            if (attribute != null)
-            {
-                // Check that the value has been set.
-                object value = null;
-                if (memberInfo is FieldInfo)
-                {
-                    value = (memberInfo as FieldInfo).GetValue(script);
-                }
-                else if (memberInfo is PropertyInfo)
-                {
-                    value = (memberInfo as PropertyInfo).GetValue(script);
-                }
-                else
-                {
-                    // This should never happen
-                    // because the attribute can only be used on fields and properties.
-                    return false;
-                }
-
-                if (value == null || value.ToString() == "null")
-                {
-                    Debug.LogError($"Missing value in {script.name}: {type.Name}.{memberInfo.Name} is null", script);
-                    return true;
-                }
-            }
-            return false;
-        }
     }
 }
--- UltraStar Play/Assets/Scenes/Demos/DependencyInjectionDemo/SearchMethodMockupDemo/SearchMethodMockupDemo.cs
+++ UltraStar Play/Assets/Scenes/Demos/DependencyInjectionDemo/SearchMethodMockupDemo/SearchMethodMockupDemo.cs
@@ -4,15 +4,17 @@ using UniInject;
 using static UniInject.UniInjectUtils;
 using UnityEngine;
 
+// Ignore warnings about unassigned fields.
+// Their values are injected, but this is not visible to the compiler.
+#pragma warning disable CS0649
 public class SearchMethodMockupDemo : MonoBehaviour
 {
-    public ScriptWithTextHolder prefab;
-
     void Start()
     {
         // Manual injection with dummy component.
         // This can be useful for tests because the tests do not need the real scene hierarchy anymore.
-        ScriptWithTextHolder dummyInstance = Instantiate(prefab);
+        GameObject gameObject = new GameObject();
+        ScriptThatNeedsInjectionOfTextHolder dummyInstance = gameObject.AddComponent<ScriptThatNeedsInjectionOfTextHolder>();
         GlobalInjector.MockUnitySearchMethod(dummyInstance, SearchMethods.GetComponentInChildren, new TextHolderMock());
         GlobalInjector.Inject(dummyInstance);
 
@@ -24,6 +26,17 @@ public class SearchMethodMockupDemo : MonoBehaviour
         public string GetText()
         {
             return "Hi, I am TextHolderMock";
+        }
+    }
+
+    private class ScriptThatNeedsInjectionOfTextHolder : MonoBehaviour
+    {
+        [Inject(searchMethod = SearchMethods.GetComponentInChildren)]
+        private readonly ITextHolder textHolder;
+
+        public string GetText()
+        {
+            return textHolder.GetText();
         }
     }
 }
