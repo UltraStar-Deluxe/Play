eca65e6de8fcf889d72287147be2b8e4ef24408f
Andreas Stange <anst.dev@gmail.com>
2019-12-04 20:28:46 +0100

    Added CommonSceneObjectsBinder and SceneInjectionManager to CommonSceneObjects

--- UltraStar Play/Assets/Common/Scene/CommonSceneObjectsBinder.cs
+++ UltraStar Play/Assets/Common/Scene/CommonSceneObjectsBinder.cs
@@ -7,8 +7,7 @@ public class CommonSceneObjectsBinder : MonoBehaviour, IBinder
     public List<IBinding> GetBindings()
     {
         BindingBuilder bb = new BindingBuilder();
-        SettingsManager settingsManager = SettingsManager.Instance;
-        bb.Bind(typeof(SettingsManager)).ToExistingInstance(settingsManager);
+        bb.BindExistingInstance(SettingsManager.Instance);
         return bb.GetBindings();
     }
 }
