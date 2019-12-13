025074d10b2298ed2329cf4a02a4d801de3daf25
Andreas Stange <anst.dev@gmail.com>
2019-12-13 17:32:44 +0100

    refactored SingSceneController into SongAudioPlayer and SongVideoPlayer

---
+++ UltraStar Play/Assets/Scenes/Sing/SongAudioPlayer.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UniInject;
+using UnityEngine;
+
+public class SongAudioPlayer : MonoBehaviour
+{
+    [InjectedInInspector]
+    public AudioSource audioPlayer;
+
+    // The last frame in which the position in the song was calculated
+    private int positionInSongInMillisFrame;
+
+    // The current position in the song in milliseconds.
+    private double positionInSongInMillis;
+    public double PositionInSongInMillis
+    {
+        get
+        {
+            if (audioPlayer == null || audioPlayer.clip == null)
+            {
+                return 0;
+            }
+            // The samples of an AudioClip change concurrently,
+            // even when they are queried in the same frame (e.g. Update() of different scripts).
+            // For a given frame, the position in the song should be the same for all scripts,
+            // which is why the value is only updated once per frame.
+            if (positionInSongInMillisFrame != Time.frameCount)
+            {
+                positionInSongInMillisFrame = Time.frameCount;
+                positionInSongInMillis = 1000.0f * (double)audioPlayer.timeSamples / (double)audioPlayer.clip.frequency;
+            }
+            return positionInSongInMillis;
+        }
+
+        set
+        {
+            if (DurationOfSongInMillis <= 0)
+            {
+                return;
+            }
+            if (value < 0 || value > DurationOfSongInMillis)
+            {
+                throw new UnityException($"new PositionInSongInMillis is out of range ({value}/{DurationOfSongInMillis})");
+            }
+            int newTimeSamples = (int)((value / 1000.0) * audioPlayer.clip.frequency);
+            audioPlayer.timeSamples = newTimeSamples;
+        }
+    }
+
+    public double DurationOfSongInMillis { get; private set; }
+
+    public double PositionInSongInPercent
+    {
+        get
+        {
+            if (DurationOfSongInMillis <= 0)
+            {
+                return 0;
+            }
+
+            return PositionInSongInMillis / DurationOfSongInMillis;
+        }
+    }
+
+    public double CurrentBeat
+    {
+        get
+        {
+            if (audioPlayer.clip == null)
+            {
+                return 0;
+            }
+            else
+            {
+                double millisInSong = PositionInSongInMillis;
+                double result = BpmUtils.MillisecondInSongToBeat(SongMeta, millisInSong);
+                if (result < 0)
+                {
+                    result = 0;
+                }
+                return result;
+            }
+        }
+    }
+
+    public bool IsPlaying
+    {
+        get
+        {
+            return audioPlayer.isPlaying;
+        }
+    }
+
+    public bool HasAudioClip
+    {
+        get
+        {
+            return audioPlayer.clip != null;
+        }
+    }
+
+    private SongMeta SongMeta { get; set; }
+
+    public void Init(SongMeta songMeta)
+    {
+        this.SongMeta = songMeta;
+
+        LoadAudio(songMeta);
+    }
+
+    private void LoadAudio(SongMeta songMeta)
+    {
+        string songPath = SongMeta.Directory + Path.DirectorySeparatorChar + SongMeta.Mp3;
+        AudioClip audioClip = AudioManager.GetAudioClip(songPath);
+        if (audioClip != null)
+        {
+            audioPlayer.clip = audioClip;
+            DurationOfSongInMillis = 1000.0 * audioClip.samples / audioClip.frequency;
+        }
+        else
+        {
+            DurationOfSongInMillis = 0;
+        }
+    }
+
+    public void PauseAudio()
+    {
+        audioPlayer.Pause();
+    }
+
+    public void PlayAudio()
+    {
+        audioPlayer.Play();
+    }
+}
