290fd284216f6a8eae268957ace4a4c084312ed1
Andreas Stange <anst.dev@gmail.com>
2019-12-25 10:48:43 +0100

    SongEditor: UI controls for missing note recording parameters

---
+++ UltraStar Play/Assets/Scenes/SongEditor/Controls/RecordingOptions/SongEditorButtonPitchInputField.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using UniInject;
+using UniRx;
+
+// Disable warning about fields that are never assigned, their values are injected.
+#pragma warning disable CS0649
+
+public class SongEditorButtonPitchInputField : MonoBehaviour, INeedInjection
+{
+    [Inject]
+    private SongEditorNoteRecorder songEditorNoteRecorder;
+
+    [Inject(searchMethod = SearchMethods.GetComponentInChildren)]
+    private InputField inputField;
+
+    void Start()
+    {
+        inputField.text = songEditorNoteRecorder.midiNoteForButtonRecording.ToString();
+        inputField.OnValueChangedAsObservable().Subscribe(OnTextChanged);
+    }
+
+    private void OnTextChanged(string newText)
+    {
+        int newInt = newText.TryParseAsInteger(songEditorNoteRecorder.midiNoteForButtonRecording);
+        songEditorNoteRecorder.midiNoteForButtonRecording = newInt;
+    }
+}
---
+++ UltraStar Play/Assets/Scenes/SongEditor/Controls/RecordingOptions/SongEditorOctaveOffsetInputField.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using UniInject;
+using UniRx;
+
+// Disable warning about fields that are never assigned, their values are injected.
+#pragma warning disable CS0649
+
+public class SongEditorOctaveOffsetInputField : MonoBehaviour, INeedInjection
+{
+    [Inject]
+    private SongEditorNoteRecorder songEditorNoteRecorder;
+
+    [Inject(searchMethod = SearchMethods.GetComponentInChildren)]
+    private InputField inputField;
+
+    void Start()
+    {
+        inputField.text = songEditorNoteRecorder.octaveOffset.ToString();
+        inputField.OnValueChangedAsObservable().Subscribe(OnTextChanged); ;
+    }
+
+    private void OnTextChanged(string newText)
+    {
+        int newInt = newText.TryParseAsInteger(songEditorNoteRecorder.octaveOffset);
+        songEditorNoteRecorder.octaveOffset = newInt;
+    }
+}
