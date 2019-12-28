346c03ee08bb38252763a11c37ba94b36d162bde
Andreas Stange <anst.dev@gmail.com>
2019-12-28 10:08:54 +0100

    log exception when parsing of string to int fails

--- UltraStar Play/Assets/Common/Util/Extensions/StringExtensions.cs
+++ UltraStar Play/Assets/Common/Util/Extensions/StringExtensions.cs
@@ -1,15 +1,17 @@
 using System;
+using UnityEngine;
 
 public static class StringExtensions
 {
-    public static int TryParseAsInteger(this string newText, int fallbackValue)
+    public static int TryParseAsInteger(this string text, int fallbackValue)
     {
         try
         {
-            return int.Parse(newText);
+            return int.Parse(text);
         }
         catch (FormatException)
         {
+            Debug.Log("Cannot parse '" + text + "' as int. Using fallback value " + fallbackValue + " instead.");
             return fallbackValue;
         }
     }
