f1204934bece2b6c992c68596c579f8fe7e3a06c
Andreas Stange <anst.dev@gmail.com>
2019-12-28 16:41:35 +0100

    SongEditor: added context menu for NoteArea

---
+++ UltraStar Play/Assets/Scenes/SongEditor/NoteArea/NoteAreaContextMenuHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using UniInject;
+using UniRx;
+
+// Disable warning about fields that are never assigned, their values are injected.
+#pragma warning disable CS0649
+
+public class NoteAreaContextMenuHandler : AbstractContextMenuHandler, INeedInjection
+{
+    [Inject]
+    private NoteArea noteArea;
+
+    protected override void FillContextMenu(ContextMenu contextMenu)
+    {
+        contextMenu.AddItem("Fit vertical", () => noteArea.FitViewportVerticalToNotes());
+    }
+
+}
