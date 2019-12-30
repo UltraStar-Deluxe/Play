580846666cfcae9419bb1eedf55eff1d96008dff
Andreas Stange <anst.dev@gmail.com>
2019-12-30 21:17:31 +0100

    SongEditor: create and delete notes / sentences via context menu

---
+++ UltraStar Play/Assets/Scenes/SongEditor/EditorSentence/EditorSentenceContextMenuHandler.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using UniInject;
+using UniRx;
+
+// Disable warning about fields that are never assigned, their values are injected.
+#pragma warning disable CS0649
+
+public class EditorSentenceContextMenuHandler : AbstractContextMenuHandler, INeedInjection
+{
+    [Inject]
+    private SongEditorSceneController songEditorSceneController;
+
+    private EditorUiSentence uiSentence;
+
+    protected override void FillContextMenu(ContextMenu contextMenu)
+    {
+        if (uiSentence == null)
+        {
+            uiSentence = GetComponent<EditorUiSentence>();
+        }
+
+        contextMenu.AddItem("Fit to notes", () => OnFitToNotes());
+        contextMenu.AddSeparator();
+        contextMenu.AddItem("Delete", () => OnDelete());
+    }
+
+    private void OnDelete()
+    {
+        songEditorSceneController.DeleteSentence(uiSentence.Sentence);
+        songEditorSceneController.OnNotesChanged();
+    }
+
+    private void OnFitToNotes()
+    {
+        uiSentence.Sentence.UpdateMinAndMaxBeat();
+        uiSentence.Sentence.SetLinebreakBeat(uiSentence.Sentence.MaxBeat);
+        songEditorSceneController.OnNotesChanged();
+    }
+}
--- UltraStar Play/Assets/Scenes/SongEditor/NoteArea/NoteAreaContextMenuHandler.cs
+++ UltraStar Play/Assets/Scenes/SongEditor/NoteArea/NoteAreaContextMenuHandler.cs
@@ -13,11 +13,64 @@ using UniRx;
 public class NoteAreaContextMenuHandler : AbstractContextMenuHandler, INeedInjection
 {
     [Inject]
+    private SongEditorSceneController songEditorSceneController;
+
+    [Inject]
     private NoteArea noteArea;
+
+    [Inject(key = "voices")]
+    private List<Voice> voices;
 
     protected override void FillContextMenu(ContextMenu contextMenu)
     {
+        int midiNote = noteArea.GetVerticalMousePositionInMidiNote();
+        int beat = (int)noteArea.GetHorizontalMousePositionInBeats();
         contextMenu.AddItem("Fit vertical", () => noteArea.FitViewportVerticalToNotes());
+        contextMenu.AddItem($"Add note", () => OnAddNote(midiNote, beat));
+        contextMenu.AddItem("Add sentence", () => OnAddSentence(midiNote, beat));
     }
 
+    private void OnAddSentence(int midiNote, int beat)
+    {
+        List<Sentence> sentencesAtBeat = GetSentencesAtBeat(beat);
+        if (!(sentencesAtBeat.Count == 0))
+        {
+            return;
+        }
+
+        Note newNote = new Note(ENoteType.Normal, beat - 2, 4, 0, "~");
+        newNote.SetMidiNote(midiNote);
+        Sentence newSentence = new Sentence(new List<Note> { newNote }, newNote.EndBeat);
+        newSentence.SetVoice(voices[0]);
+
+        songEditorSceneController.OnNotesChanged();
+    }
+
+    private void OnAddNote(int midiNote, int beat)
+    {
+        List<Sentence> sentencesAtBeat = GetSentencesAtBeat(beat);
+        if (sentencesAtBeat.Count == 0)
+        {
+            OnAddSentence(midiNote, beat);
+        }
+        else
+        {
+            Note newNote = new Note(ENoteType.Normal, beat - 2, 4, 0, "~");
+            newNote.SetMidiNote(midiNote);
+            newNote.SetSentence(sentencesAtBeat[0]);
+
+            songEditorSceneController.OnNotesChanged();
+        }
+    }
+
+    private List<Sentence> GetSentencesAtBeat(int beat)
+    {
+        return voices.SelectMany(voice => voice.Sentences)
+            .Where(sentence => IsBeatInSentence(sentence, beat)).ToList();
+    }
+
+    private bool IsBeatInSentence(Sentence sentence, int beat)
+    {
+        return sentence.MinBeat <= beat && beat <= Math.Max(sentence.MaxBeat, sentence.LinebreakBeat);
+    }
 }
