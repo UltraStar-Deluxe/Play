82419ff21649b19b917462f4e34fe7edda6a398e
Andreas Stange <anst.dev@gmail.com>
2019-12-30 18:53:00 +0100

    SongEditor: move and stretch notes via keyboard buttons (similar to Yass controls)

--- UltraStar Play/Assets/Scenes/SongEditor/SongEditorSceneKeyboardController.cs
+++ UltraStar Play/Assets/Scenes/SongEditor/SongEditorSceneKeyboardController.cs
@@ -15,11 +15,18 @@ public class SongEditorSceneKeyboardController : MonoBehaviour, INeedInjection
     [Inject(searchMethod = SearchMethods.FindObjectOfType)]
     private NoteArea noteArea;
 
+    [Inject]
+    private SongEditorSelectionController selectionController;
+
+    [Inject]
+    private EditorNoteDisplayer editorNoteDisplayer;
+
     public void Update()
     {
         bool noModifier = !Input.GetKey(KeyCode.LeftControl) && !Input.GetKey(KeyCode.LeftShift) && !Input.GetKey(KeyCode.LeftAlt);
         bool ctrlExclusive = Input.GetKey(KeyCode.LeftControl) && !Input.GetKey(KeyCode.LeftShift) && !Input.GetKey(KeyCode.LeftAlt);
         bool shiftExclusive = !Input.GetKey(KeyCode.LeftControl) && Input.GetKey(KeyCode.LeftShift) && !Input.GetKey(KeyCode.LeftAlt);
+        bool altExclusive = !Input.GetKey(KeyCode.LeftControl) && !Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.LeftAlt);
 
         bool ctrlShiftExclusive = Input.GetKey(KeyCode.LeftControl) && Input.GetKey(KeyCode.LeftShift) && !Input.GetKey(KeyCode.LeftAlt);
 
@@ -30,6 +37,48 @@ public class SongEditorSceneKeyboardController : MonoBehaviour, INeedInjection
             songEditorSceneController.TogglePlayPause();
         }
 
+        // Move and stretch notes
+        Vector2 arrowKeyDirection = GetArrowKeyDirection();
+        if (arrowKeyDirection != Vector2.zero)
+        {
+            List<Note> selectedNotes = selectionController.GetSelectedNotes();
+            foreach (Note note in selectedNotes)
+            {
+                // Move with Shift
+                if (shiftExclusive)
+                {
+                    int newStartBeat = note.StartBeat + (int)arrowKeyDirection.x;
+                    int newEndBeat = note.EndBeat + (int)arrowKeyDirection.x;
+                    note.SetStartAndEndBeat(newStartBeat, newEndBeat);
+
+                    int newMidiNote = note.MidiNote + (int)arrowKeyDirection.y;
+                    note.SetMidiNote(newMidiNote);
+                }
+
+                // Extend right side with Alt
+                if (altExclusive)
+                {
+                    int newEndBeat = note.EndBeat + (int)arrowKeyDirection.x;
+                    if (newEndBeat > note.StartBeat)
+                    {
+                        note.SetEndBeat(newEndBeat);
+                    }
+                }
+
+                // Extend left side with Ctrl
+                if (ctrlExclusive)
+                {
+                    int newStartBeat = note.StartBeat + (int)arrowKeyDirection.x;
+                    if (newStartBeat < note.EndBeat)
+                    {
+                        note.SetStartBeat(newStartBeat);
+                    }
+                }
+            }
+            editorNoteDisplayer.UpdateNotesAndSentences();
+        }
+
+        // Scroll and zoom in NoteArea
         if (scrollDirection != 0 && noteArea.IsPointerOver)
         {
             // Scroll horizontal in NoteArea with mouse wheel
@@ -58,4 +107,25 @@ public class SongEditorSceneKeyboardController : MonoBehaviour, INeedInjection
         }
     }
 
+    private Vector2 GetArrowKeyDirection()
+    {
+        Vector2 result = Vector2.zero;
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            result += new Vector2(-1, 0);
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            result += new Vector2(1, 0);
+        }
+        if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            result += new Vector2(0, 1);
+        }
+        if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            result += new Vector2(0, -1);
+        }
+        return result;
+    }
 }
