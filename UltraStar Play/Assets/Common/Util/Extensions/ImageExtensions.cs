4b42f844c9f091c709d988846bf93f3540e68cf5
Andreas Stange <anst.dev@gmail.com>
2020-01-29 22:26:22 +0100

    SongEditor: draw sentence rectangles alternatingly dark/light

--- UltraStar Play/Assets/Common/Util/Extensions/ImageExtensions.cs
+++ UltraStar Play/Assets/Common/Util/Extensions/ImageExtensions.cs
@@ -8,4 +8,28 @@ public static class ImageExtensions
         Color lastColor = image.color;
         image.color = new Color(lastColor.r, lastColor.g, lastColor.b, newAlpha);
     }
+
+    // Make the color of an image darker with a factor < 1, or brighter with a factor > 1.
+    public static void MultiplyColor(this Image image, float factor, bool includeAlpha = false)
+    {
+        Color lastColor = image.color;
+        float newR = Limit(lastColor.r * factor, 0, 1);
+        float newG = Limit(lastColor.g * factor, 0, 1);
+        float newB = Limit(lastColor.b * factor, 0, 1);
+        float newAlpha = includeAlpha ? Limit(lastColor.a * factor, 0, 1) : lastColor.a;
+        image.color = new Color(newR, newG, newB, newAlpha);
+    }
+
+    private static float Limit(float value, float min, float max)
+    {
+        if (value < min)
+        {
+            return min;
+        }
+        if (value > max)
+        {
+            return max;
+        }
+        return value;
+    }
 }
