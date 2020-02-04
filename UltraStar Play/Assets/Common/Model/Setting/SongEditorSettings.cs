f20d91e67c77bf3f910dd10f3f128f1077554cd4
Andreas Stange <anst.dev@gmail.com>
2020-02-04 20:40:27 +0100

    SongEditor: Option to play along midi sounds for the notes
    SongEditor: Play midi sound for note via Ctrl+Click

--- UltraStar Play/Assets/Common/Model/Setting/SongEditorSettings.cs
+++ UltraStar Play/Assets/Common/Model/Setting/SongEditorSettings.cs
@@ -12,6 +12,9 @@ public class SongEditorSettings
 
     public bool AdjustFollowingNotes { get; set; }
 
+    public int MidiNoteVolume { get; set; } = 250;
+    public bool MidiSoundPlayAlongEnabled { get; set; } = true;
+
     // Option to show / hide voices.
     // Contains the names of the voices that should be hidden.
     public List<string> HideVoices { get; private set; } = new List<string>();
---
+++ UltraStar Play/Assets/Scenes/SongEditor/VirtualPiano/SongEditorMidiVolumeSlider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using UniInject;
+using UniRx;
+
+// Disable warning about fields that are never assigned, their values are injected.
+#pragma warning disable CS0649
+
+public class SongEditorMidiVolumeSlider : MonoBehaviour, INeedInjection
+{
+
+    [Inject(searchMethod = SearchMethods.GetComponentInChildren)]
+    private Slider slider;
+
+    [Inject]
+    private Settings settings;
+
+    void Start()
+    {
+        slider.value = (float)settings.SongEditorSettings.MidiNoteVolume;
+        slider.OnValueChangedAsObservable()
+            .Subscribe(newValue => settings.SongEditorSettings.MidiNoteVolume = (int)newValue);
+    }
+}
