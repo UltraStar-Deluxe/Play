d173af61008f0dbd491c64d07bfa2233222665df
Andreas Stange <anst.dev@gmail.com>
2020-02-13 19:28:10 +0100

    download specific commit of CSharpSynthForUnity

--- tools/download-dependencies/download-csharpsynthforunity.sh
+++ tools/download-dependencies/download-csharpsynthforunity.sh
@@ -15,7 +15,8 @@ git config core.sparsecheckout true
 echo "Assets/*" >> .git/info/sparse-checkout
 # UltraStar-Play-subset is a dedicated branch for the UltraStar Play project
 git pull --depth=100 origin UltraStar-Play-subset
-git checkout UltraStar-Play-subset
+# Commit from 09 February 2020: 0bc2afbe88cceceac6d618d612f4aeeb8cfcecf0
+git checkout 0bc2afbe88cceceac6d618d612f4aeeb8cfcecf0
 
 echo "Moving downloaded files to correct position for this project..."
 mv -v Assets/* ./
