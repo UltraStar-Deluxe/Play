47ba4b1b320a4ba58b31d7d7ec717b1761da69b0
Andreas Stange <anst.dev@gmail.com>
2020-03-08 15:18:09 +0100

    renamed ConcurrencyUtils -> CoroutineUtils

--- UltraStar Play/Assets/Common/Util/CoroutineUtils.cs
+++ UltraStar Play/Assets/Common/Util/CoroutineUtils.cs
@@ -2,12 +2,18 @@ using System;
 using System.Collections;
 using UnityEngine;
 
-public class ConcurrencyUtils
+public class CoroutineUtils
 {
     public static IEnumerator ExecuteAfterDelayInSeconds(float delayInSeconds, Action action)
     {
         yield return new WaitForSeconds(delayInSeconds);
         // Code to execute after the delay
+        action();
+    }
+
+    public static IEnumerator Execute(Action action)
+    {
+        yield return null;
         action();
     }
 }
