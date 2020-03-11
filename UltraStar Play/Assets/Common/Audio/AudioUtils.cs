b7a05fa2af69c3277974c664c32f2b76137b1567
Andreas Stange <anst.dev@gmail.com>
2020-03-11 19:09:25 +0100

    refactored AudioManager methods back to AudioUtils

--- UltraStar Play/Assets/Common/Audio/AudioUtils.cs
+++ UltraStar Play/Assets/Common/Audio/AudioUtils.cs
@@ -7,6 +7,51 @@ using UniRx;
 
 public static class AudioUtils
 {
+    // This method should only be called from tests and the AudioManager.
+    // Use the cached version of the AudioManager for the normal game logic.
+    public static AudioClip GetAudioClipUncached(string path, bool streamAudio)
+    {
+        if (!System.IO.File.Exists(path))
+        {
+            Debug.LogWarning($"Can not open audio file because it does not exist: {path}");
+            return null;
+        }
+        string fileExtension = System.IO.Path.GetExtension(path);
+
+        if (fileExtension.ToLowerInvariant().Equals(".mp3"))
+        {
+            AudioClip audioClip = LoadMp3(path);
+            return audioClip;
+        }
+        else
+        {
+            return LoadAudio(path, streamAudio);
+        }
+    }
+
+    private static AudioClip LoadAudio(string path, bool streamAudio)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequestMultimedia.GetAudioClip("file://" + path, AudioType.UNKNOWN))
+        {
+            DownloadHandlerAudioClip downloadHandler = webRequest.downloadHandler as DownloadHandlerAudioClip;
+            downloadHandler.streamAudio = streamAudio;
+
+            webRequest.SendWebRequest();
+            if (webRequest.isNetworkError || webRequest.isHttpError)
+            {
+                Debug.LogError("Error Loading Audio: " + path);
+                Debug.LogError(webRequest.error);
+                return null;
+            }
+
+            while (!webRequest.isDone)
+            {
+                Task.Delay(30);
+            }
+            return downloadHandler.audioClip;
+        }
+    }
+
     public static AudioClip LoadMp3(string path)
     {
         string filename = System.IO.Path.GetFileNameWithoutExtension(path);
