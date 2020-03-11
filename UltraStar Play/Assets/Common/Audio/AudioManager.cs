872387981a3d3ad017ad368b7d4c595692a27eeb
Andreas Stange <anst.dev@gmail.com>
2020-03-11 19:09:25 +0100

    refactored AudioManager methods back to AudioUtils

--- UltraStar Play/Assets/Common/Audio/AudioManager.cs
+++ UltraStar Play/Assets/Common/Audio/AudioManager.cs
@@ -50,7 +50,7 @@ public class AudioManager : MonoBehaviour
             return null;
         }
 
-        AudioClip audioClip = GetAudioClipUncached(path, streamAudio);
+        AudioClip audioClip = AudioUtils.GetAudioClipUncached(path, streamAudio);
         if (audioClip == null)
         {
             Debug.LogError("Could not load not AudioClip from path: " + path);
@@ -59,51 +59,6 @@ public class AudioManager : MonoBehaviour
 
         AddAudioClipToCache(path, audioClip, streamAudio);
         return audioClip;
-    }
-
-    // This method should only be called from tests and the AudioManager.
-    // Use the cached version of the AudioManager for the normal game logic.
-    public static AudioClip GetAudioClipUncached(string path, bool streamAudio)
-    {
-        if (!System.IO.File.Exists(path))
-        {
-            Debug.LogWarning($"Can not open audio file because it does not exist: {path}");
-            return null;
-        }
-        string fileExtension = System.IO.Path.GetExtension(path);
-
-        if (fileExtension.ToLowerInvariant().Equals(".mp3"))
-        {
-            AudioClip audioClip = AudioUtils.LoadMp3(path);
-            return audioClip;
-        }
-        else
-        {
-            return LoadAudio(path, streamAudio);
-        }
-    }
-
-    private static AudioClip LoadAudio(string path, bool streamAudio)
-    {
-        using (UnityWebRequest webRequest = UnityWebRequestMultimedia.GetAudioClip("file://" + path, AudioType.UNKNOWN))
-        {
-            DownloadHandlerAudioClip downloadHandler = webRequest.downloadHandler as DownloadHandlerAudioClip;
-            downloadHandler.streamAudio = streamAudio;
-
-            webRequest.SendWebRequest();
-            if (webRequest.isNetworkError || webRequest.isHttpError)
-            {
-                Debug.LogError("Error Loading Audio: " + path);
-                Debug.LogError(webRequest.error);
-                return null;
-            }
-
-            while (!webRequest.isDone)
-            {
-                Task.Delay(30);
-            }
-            return downloadHandler.audioClip;
-        }
     }
 
     private static void AddAudioClipToCache(string path, AudioClip audioClip, bool streamAudio)
