32098d66bed78d5bd5c9a0bbf608ddc929478d49
Andreas Stange <anst.dev@gmail.com>
2020-03-21 11:09:33 +0100

    fixed missing button callbacks
    - button callbacks that have been wired in the unity editor have been lost when moving scripts around
    - instead of wiring button callbacks in the editor, now scripts are used that subscribe to OnCickAsObservable

---
+++ UltraStar Play/Assets/Common/UI/Buttons/ChangeSceneButton.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using UniInject;
+using UniRx;
+
+// Disable warning about fields that are never assigned, their values are injected.
+#pragma warning disable CS0649
+
+[RequireComponent(typeof(Button))]
+public class ChangeSceneButton : MonoBehaviour, INeedInjection
+{
+    public EScene targetScene;
+
+    void Start()
+    {
+        GetComponent<Button>().OnClickAsObservable()
+            .Subscribe(_ => SceneNavigator.Instance.LoadScene(targetScene));
+    }
+}
