ea42bdd0ed8dfeac68dc407b7e2f60d628d0ee89
Andreas Stange <anst.dev@gmail.com>
2020-04-28 22:36:13 +0200

    UniInject: added UniInjectExtensions for utility extension methods

---
+++ UltraStar Play/Assets/Plugins/UniInject/Extensions/UniInjectExtensions.cs
@@ -0,0 +1,13 @@
+﻿using UniInject;
+using UnityEngine;
+
+public static class UniInjectExtensions
+{
+    public static void InjectAllComponentsInChildren(this Injector injector, MonoBehaviour monoBehaviour)
+    {
+        foreach (INeedInjection childThatNeedsInjection in monoBehaviour.GetComponentsInChildren<INeedInjection>())
+        {
+            injector.Inject(childThatNeedsInjection);
+        }
+    }
+}
