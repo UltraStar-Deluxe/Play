355a2402be7b502f054843b79cc2061838246636
Andreas Stange <anst.dev@gmail.com>
2020-09-13 13:23:43 +0200

    SingScene: fix and fine-tune player layout
    SingingResultsScene: added layout for multiple players like in SingScene

--- UltraStar Play/Assets/Scenes/Sing/PlayerController/PlayerUiController/PlayerUiArea.cs
+++ UltraStar Play/Assets/Scenes/Sing/PlayerController/PlayerUiController/PlayerUiArea.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using UniInject;
 using UnityEngine;
+using UnityEngine.UI;
+
+// Disable warning about fields that are never assigned, their values are injected.
+#pragma warning disable CS0649
 
 public class PlayerUiArea : MonoBehaviour, INeedInjection
 {
-    [Inject(searchMethod = SearchMethods.GetComponent)]
-    private GridLayoutGroupCellSizer gridLayoutGroupCellSizer;
-
     void Awake()
     {
         // Remove dummies from the Editor scene
@@ -17,27 +18,38 @@ public class PlayerUiArea : MonoBehaviour, INeedInjection
         }
     }
 
-    public void SetPlayerCount(int playerCount)
+    public static void SetupPlayerUiGrid(int playerCount, GridLayoutGroupCellSizer gridLayoutGroupCellSizer)
     {
+        GridLayoutGroup gridLayoutGroup = gridLayoutGroupCellSizer.GetComponent<GridLayoutGroup>();
+        int gridSpacing = 20;
+
         int columns = 1;
         int rows = 2;
         if (playerCount >= 3)
         {
+            columns = 1;
+            rows = 3;
+            gridLayoutGroup.spacing = new Vector2(0, gridSpacing);
+        }
+        if (playerCount >= 4)
+        {
             columns = 2;
             rows = 2;
+            gridLayoutGroup.spacing = new Vector2(gridSpacing, gridSpacing);
         }
         if (playerCount >= 5)
         {
             columns = 2;
             rows = 3;
+            gridLayoutGroup.spacing = new Vector2(gridSpacing, gridSpacing);
         }
         if (playerCount >= 7)
         {
             columns = 3;
             rows = 3;
+            gridLayoutGroup.spacing = new Vector2(gridSpacing, gridSpacing);
         }
         gridLayoutGroupCellSizer.columns = columns;
         gridLayoutGroupCellSizer.rows = rows;
     }
-
 }
