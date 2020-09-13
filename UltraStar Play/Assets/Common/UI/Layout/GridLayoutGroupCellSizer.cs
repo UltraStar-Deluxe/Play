ef2fa7af2090704ada38b983a1cc3d6f11af7bbe
Andreas Stange <anst.dev@gmail.com>
2020-09-13 10:38:18 +0200

    SingScene: better layout using HorizontalLayoutGroup, VerticalLayoutGroup and GridLayoutGroup
    - added GridLayoutGroupCellSizer to set the size by number of columns and rows
    - DummySpaceIfOnlyOnePlayerController is thus obsolete and removed

---
+++ UltraStar Play/Assets/Common/UI/Layout/GridLayoutGroupCellSizer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(GridLayoutGroup))]
+[ExecuteInEditMode]
+public class GridLayoutGroupCellSizer : MonoBehaviour
+{
+    [Min(1)]
+    public int columns = 1;
+
+    [Min(1)]
+    public int rows = 1;
+
+    private GridLayoutGroup gridLayoutGroup;
+    private RectTransform rectTransform;
+
+    void Awake()
+    {
+        gridLayoutGroup = GetComponent<GridLayoutGroup>();
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    void Update()
+    {
+        gridLayoutGroup.cellSize = new Vector2(rectTransform.rect.width / columns, rectTransform.rect.height / rows);
+    }
+}
--- UltraStar Play/Assets/Scenes/Sing/PlayerController/PlayerUiController/PlayerUiArea.cs
+++ UltraStar Play/Assets/Scenes/Sing/PlayerController/PlayerUiController/PlayerUiArea.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UniInject;
 using UnityEngine;
 
-public class PlayerUiArea : MonoBehaviour
+public class PlayerUiArea : MonoBehaviour, INeedInjection
 {
+    [Inject(searchMethod = SearchMethods.GetComponent)]
+    private GridLayoutGroupCellSizer gridLayoutGroupCellSizer;
 
     void Awake()
     {
@@ -12,6 +15,29 @@ public class PlayerUiArea : MonoBehaviour
         {
             GameObject.Destroy(playerUiController.gameObject);
         }
+    }
+
+    public void SetPlayerCount(int playerCount)
+    {
+        int columns = 1;
+        int rows = 2;
+        if (playerCount >= 3)
+        {
+            columns = 2;
+            rows = 2;
+        }
+        if (playerCount >= 5)
+        {
+            columns = 2;
+            rows = 3;
+        }
+        if (playerCount >= 7)
+        {
+            columns = 3;
+            rows = 3;
+        }
+        gridLayoutGroupCellSizer.columns = columns;
+        gridLayoutGroupCellSizer.rows = rows;
     }
 
 }
