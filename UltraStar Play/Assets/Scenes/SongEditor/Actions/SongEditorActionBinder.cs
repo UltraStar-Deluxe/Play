f32ac2544992f20b507433f1ee8b3fe7ae1518de
Andreas Stange <anst.dev@gmail.com>
2020-09-27 20:13:07 +0200

    SongEditor: Action to change bpm and recalculate note start/end to keep same position in song

--- UltraStar Play/Assets/Scenes/SongEditor/Actions/SongEditorActionBinder.cs
+++ UltraStar Play/Assets/Scenes/SongEditor/Actions/SongEditorActionBinder.cs
@@ -29,6 +29,7 @@ public class SongEditorActionBinder : MonoBehaviour, IBinder
         bb.BindTypeToNewInstances(typeof(SetMusicGapAction));
         bb.BindTypeToNewInstances(typeof(SetVideoGapAction));
         bb.BindTypeToNewInstances(typeof(ChangeBpmAction));
+        bb.BindTypeToNewInstances(typeof(ApplyBpmAction));
         bb.BindTypeToNewInstances(typeof(SpaceBetweenNotesAction));
         return bb.GetBindings();
     }
---
+++ UltraStar Play/Assets/Scenes/SongEditor/Controls/SongMetaTags/Bpm/ApplyBpmButton.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using UniInject;
+using UniRx;
+
+// Disable warning about fields that are never assigned, their values are injected.
+#pragma warning disable CS0649
+
+public class ApplyBpmButton : MonoBehaviour, INeedInjection
+{
+    [InjectedInInspector]
+    public InputField newBpmInputField;
+
+    [Inject(searchMethod = SearchMethods.GetComponentInChildren)]
+    private Button button;
+
+    [Inject]
+    private SongMeta songMeta;
+
+    [Inject]
+    private ApplyBpmAction applyBpmAction;
+
+    void Start()
+    {
+        button.OnClickAsObservable().Subscribe(_ =>
+        {
+            if (float.TryParse(newBpmInputField.text, out float newBpm))
+            {
+                applyBpmAction.ExecuteAndNotify(newBpm);
+            }
+        });
+    }
+}
