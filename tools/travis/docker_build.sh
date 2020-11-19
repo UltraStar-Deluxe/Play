eeb22cda45388ed58e0df544bfc1ef926f0e86ea
Andreas Stange <anst.dev@gmail.com>
2020-11-19 21:04:08 +0100

    docker build:: removed references to UNITY_USERNAME and UNITY_PASSWORD because these are not needed and not set anymore

--- tools/travis/docker_build.sh
+++ tools/travis/docker_build.sh
@@ -5,11 +5,9 @@ set -e
 travis_fold start "unity.docker.build"
 
 docker run \
-  -e BUILD_NAME \
   -e UNITY_LICENSE_CONTENT \
+  -e BUILD_NAME \
   -e BUILD_TARGET \
-  -e UNITY_USERNAME \
-  -e UNITY_PASSWORD \
   -w /project/ \
   -v $(pwd):/project/ \
   $IMAGE_NAME \
--- tools/travis/docker_test.sh
+++ tools/travis/docker_test.sh
@@ -3,10 +3,8 @@
 set -e
 
 docker run \
-  -e UNITY_LICENSE_CONTENT \
+ -e UNITY_LICENSE_CONTENT \
   -e TEST_PLATFORM \
-  -e UNITY_USERNAME \
-  -e UNITY_PASSWORD \
   -w /project/ \
   -v $(pwd):/project/ \
   $IMAGE_NAME \
