c6e09efe58837951df790642460eafcfd78737e6
Andreas Stange <anst.dev@gmail.com>
2020-11-21 14:09:32 +0100

    download-dependencies: use UniInject repo of UltraStar-Play organization

--- tools/download-dependencies/download-uniinject.sh
+++ tools/download-dependencies/download-uniinject.sh
@@ -10,16 +10,16 @@ cd UniInject
 
 echo "Cloning UniInject from remote..."
 git init
-git remote add origin https://github.com/achimmihca/UniInject.git
+git remote add origin https://github.com/UltraStar-Deluxe/UniInject.git
 git config core.sparsecheckout true
 echo "UniInject/Assets/Scripts/*" >> .git/info/sparse-checkout
 echo "UniInject/Assets/Editor/UniInjectEditor.asmdef" >> .git/info/sparse-checkout
 echo "UniInject/Assets/Editor/MenuItems/*" >> .git/info/sparse-checkout
 echo "README.md" >> .git/info/sparse-checkout
 echo "LICENSE" >> .git/info/sparse-checkout
-# commit from 18th November 2020: b12f45828c081b03a6decaf8ca10058098f736f5
+# commit from 21th November 2020: 913136b70cd712fc57d3831cfaedf2f4e4cd5f99
 git pull --depth=100 origin main
-git checkout b12f45828c081b03a6decaf8ca10058098f736f5
+git checkout 913136b70cd712fc57d3831cfaedf2f4e4cd5f99
 
 echo "Moving downloaded files to correct position for this project..."
 mv -v UniInject/Assets/* ./
