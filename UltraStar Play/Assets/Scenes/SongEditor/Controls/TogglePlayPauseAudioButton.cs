0a426ff368d57d2b4c8881cc55bed365c02dee98
Andreas Stange <anst.dev@gmail.com>
2021-02-09 20:24:11 +0100

    SongEditor: new button to play/pause audio

---
+++ UltraStar Play/Assets/Scenes/SongEditor/Controls/TogglePlayPauseAudioButton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UniInject;
+using UniRx;
+
+// Disable warning about fields that are never assigned, their values are injected.
+#pragma warning disable CS0649
+
+public class TogglePlayPauseAudioButton : MonoBehaviour, INeedInjection
+{
+    [Inject]
+    private SongEditorSceneController songEditorSceneController;
+
+    [Inject(searchMethod = SearchMethods.GetComponentInChildren)]
+    private Button button;
+
+    void Start()
+    {
+        button.OnClickAsObservable().Subscribe(_ => songEditorSceneController.ToggleAudioPlayPause());
+    }
+}
