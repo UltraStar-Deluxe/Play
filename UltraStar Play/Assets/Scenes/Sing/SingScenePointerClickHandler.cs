fd0ef81a175bd096b3a7dbc570758a56550743a9
Andreas Stange <anst.dev@gmail.com>
2021-02-09 20:55:49 +0100

    SingScene: Toggle play/pause via double click

---
+++ UltraStar Play/Assets/Scenes/Sing/SingScenePointerClickHandler.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using UniInject;
+using UniRx;
+using UnityEngine.EventSystems;
+
+// Disable warning about fields that are never assigned, their values are injected.
+#pragma warning disable CS0649
+
+public class SingScenePointerClickHandler : MonoBehaviour, INeedInjection, IPointerClickHandler
+{
+    [Inject]
+    private SingSceneController singSceneController;
+    
+    private float lastClickTime;
+    
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        bool isDoubleClick = Time.time - lastClickTime < InputUtils.DoubleClickThresholdInSeconds;
+        lastClickTime = Time.time;
+        if (isDoubleClick)
+        {
+            singSceneController.TogglePlayPause();
+        }
+    }
+}
