9ecbb973b5607fbc83a35e08668d2600241a8f9a
Andreas Stange <anst.dev@gmail.com>
2021-10-19 21:29:23 +0200

    SongEditor: scroll while selecting via drag (selection frame is still WIP)

--- UltraStar Play/Assets/Scenes/SongEditor/NoteArea/Drag/NoteAreaSelectionDragListener.cs
+++ UltraStar Play/Assets/Scenes/SongEditor/NoteArea/Drag/NoteAreaSelectionDragListener.cs
@@ -18,9 +18,6 @@ public class NoteAreaSelectionDragListener : MonoBehaviour, INeedInjection, IDra
     [InjectedInInspector]
     public RectTransform selectionFrame;
 
-    [InjectedInInspector]
-    public RectTransform noteContainer;
-
     [Inject]
     private SongEditorSelectionController selectionController;
 
@@ -33,16 +30,39 @@ public class NoteAreaSelectionDragListener : MonoBehaviour, INeedInjection, IDra
     [Inject]
     private Canvas canvas;
 
-    bool isCanceled;
+    [Inject]
+    private SongEditorSceneController songEditorSceneController;
+
+    [Inject]
+    private SongMeta songMeta;
+
+    private float scrollBorderPercent = 0.05f;
+
+    private bool isCanceled;
+
+    private Vector2 scrollAmount;
+    private float lastScrollVerticalTime;
+
+    private NoteAreaDragEvent startDragEvent;
+    private NoteAreaDragEvent lastDragEvent;
+    private Vector2 scrollDistanceSinceDragStart;
 
     void Start()
     {
         noteAreaDragHandler.AddListener(this);
     }
 
+    void Update()
+    {
+        UpdateScroll();
+    }
+
     public void OnBeginDrag(NoteAreaDragEvent dragEvent)
     {
         isCanceled = false;
+        lastDragEvent = dragEvent;
+        startDragEvent = dragEvent;
+        scrollDistanceSinceDragStart = Vector2.zero;
         if (dragEvent.GeneralDragEvent.InputButton != PointerEventData.InputButton.Left)
         {
             CancelDrag();
@@ -61,8 +81,11 @@ public class NoteAreaSelectionDragListener : MonoBehaviour, INeedInjection, IDra
 
     public void OnDrag(NoteAreaDragEvent dragEvent)
     {
+        lastDragEvent = dragEvent;
         UpdateSelectionFrame(dragEvent);
         UpdateSelection(dragEvent);
+
+        UpdateScrollAmount(dragEvent);
 
         if (Touch.activeTouches.Count > 1)
         {
@@ -72,11 +95,14 @@ public class NoteAreaSelectionDragListener : MonoBehaviour, INeedInjection, IDra
 
     public void OnEndDrag(NoteAreaDragEvent dragEvent)
     {
+        lastDragEvent = dragEvent;
+        scrollAmount = Vector2.zero;
         selectionFrame.gameObject.SetActive(false);
     }
 
     public void CancelDrag()
     {
+        scrollAmount = Vector2.zero;
         selectionFrame.gameObject.SetActive(false);
         isCanceled = true;
     }
@@ -86,53 +112,54 @@ public class NoteAreaSelectionDragListener : MonoBehaviour, INeedInjection, IDra
         return isCanceled;
     }
 
-    private void UpdateSelection(NoteAreaDragEvent dragEvent)
+    private void UpdateSelection(NoteAreaDragEvent currentDragEvent)
     {
-        List<EditorUiNote> selectedUiNotes = new List<EditorUiNote>();
-        foreach (Transform child in noteContainer.transform)
-        {
-            EditorUiNote uiNote = child.GetComponent<EditorUiNote>();
-            if (uiNote != null && IsInSelectionFrame(uiNote, dragEvent))
-            {
-                selectedUiNotes.Add(uiNote);
-            }
-        }
+        int startBeat = startDragEvent.PositionInSongInBeatsDragStart;
+        int endBeat = startBeat + currentDragEvent.BeatDistance + (int) (scrollDistanceSinceDragStart.x / BpmUtils.MillisecondsPerBeat(songMeta));
+
+        int startMidiNote = startDragEvent.MidiNoteDragStart;
+        int endMidiNote = startMidiNote + currentDragEvent.MidiNoteDistance + ((int) scrollDistanceSinceDragStart.y);
+
+        List<Note> visibleNotes = songEditorSceneController.GetAllVisibleNotes();
+        List<Note> selectedNotes = visibleNotes
+            .Where(note => IsInSelectionFrame(note, startMidiNote, endMidiNote, startBeat, endBeat))
+            .ToList();
 
         // Add to selection via Shift. Remove from selection via Ctrl+Shift. Without modifier, set selection.
         if (InputUtils.IsKeyboardShiftPressed())
         {
             if (InputUtils.IsKeyboardControlPressed())
             {
-                selectionController.RemoveFromSelection(selectedUiNotes);
+                selectionController.RemoveFromSelection(selectedNotes);
             }
             else
             {
-                selectionController.AddToSelection(selectedUiNotes);
+                selectionController.AddToSelection(selectedNotes);
             }
         }
         else
         {
-            selectionController.SetSelection(selectedUiNotes);
+            selectionController.SetSelection(selectedNotes);
         }
     }
 
-    private bool IsInSelectionFrame(EditorUiNote uiNote, NoteAreaDragEvent dragEvent)
+    private bool IsInSelectionFrame(
+        Note note,
+        int startMidiNote,
+        int endMidiNote,
+        int startBeat,
+        int endBeat)
     {
-        Note note = uiNote.Note;
         if (note == null)
         {
             return false;
         }
 
-        int startMidiNote = dragEvent.MidiNoteDragStart;
-        int endMidiNote = dragEvent.MidiNoteDragStart + dragEvent.MidiNoteDistance;
         if (startMidiNote > endMidiNote)
         {
             ObjectUtils.Swap(ref startMidiNote, ref endMidiNote);
         }
 
-        int startBeat = dragEvent.PositionInSongInBeatsDragStart;
-        int endBeat = dragEvent.PositionInSongInBeatsDragStart + dragEvent.BeatDistance;
         if (startBeat > endBeat)
         {
             ObjectUtils.Swap(ref startBeat, ref endBeat);
@@ -166,5 +193,65 @@ public class NoteAreaSelectionDragListener : MonoBehaviour, INeedInjection, IDra
         }
         selectionFrame.position = new Vector2(x, y);
         selectionFrame.sizeDelta = new Vector2(width, height);
+    }
+
+    private void UpdateScrollAmount(NoteAreaDragEvent dragEvent)
+    {
+        int scrollAmountX = 200;
+        scrollAmountX += (int)(Math.Abs(dragEvent.GeneralDragEvent.RectTransformCoordinateInPercent.Distance.x) - scrollBorderPercent) * 1000;
+
+        int scrollAmountY = 1;
+
+        // X-Coordinate
+        if (dragEvent.GeneralDragEvent.RectTransformCoordinateInPercent.CurrentPosition.x > (1 - scrollBorderPercent))
+        {
+            scrollAmount = new Vector2(scrollAmountX, scrollAmount.y);
+        }
+        else if (dragEvent.GeneralDragEvent.RectTransformCoordinateInPercent.CurrentPosition.x < scrollBorderPercent)
+        {
+            scrollAmount = new Vector2(-scrollAmountX, scrollAmount.y);
+        }
+        else
+        {
+            scrollAmount = new Vector2(0, scrollAmount.y);
+        }
+
+        // Y-Coordinate
+        if (dragEvent.GeneralDragEvent.RectTransformCoordinateInPercent.CurrentPosition.y > (1 - scrollBorderPercent))
+        {
+            scrollAmount = new Vector2(scrollAmount.x, scrollAmountY);
+        }
+        else if (dragEvent.GeneralDragEvent.RectTransformCoordinateInPercent.CurrentPosition.y < scrollBorderPercent)
+        {
+            scrollAmount = new Vector2(scrollAmount.x, -scrollAmountY);
+        }
+        else
+        {
+            scrollAmount = new Vector2(scrollAmount.x, 0);
+        }
+    }
+
+    private void UpdateScroll()
+    {
+        if (scrollAmount.x != 0)
+        {
+            noteArea.SetViewportX(noteArea.ViewportX + (int)(scrollAmount.x));
+            scrollDistanceSinceDragStart = new Vector2(scrollDistanceSinceDragStart.x + scrollAmount.x, scrollDistanceSinceDragStart.y);
+        }
+
+        if (scrollAmount.y != 0
+            && lastScrollVerticalTime + 0.1f < Time.time)
+        {
+            lastScrollVerticalTime = Time.time;
+            noteArea.SetViewportY(noteArea.ViewportY + (int)(scrollAmount.y));
+            scrollDistanceSinceDragStart = new Vector2(scrollDistanceSinceDragStart.x, scrollDistanceSinceDragStart.y + scrollAmount.y);
+        }
+
+        if (scrollAmount.x != 0
+            || scrollAmount.y != 0)
+        {
+            UpdateSelectionFrame(lastDragEvent);
+            UpdateSelection(lastDragEvent);
+        }
     }
 }
