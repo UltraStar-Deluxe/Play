00ebc1df49e1da5d9b29853ddd9fbfa6072772a3
Andreas Stange <anst.dev@gmail.com>
2021-10-20 19:50:38 +0200

    SongEditor: calculate selection frame by converting pixel coordinates to viewport coordinates and vice versa

--- UltraStar Play/Assets/Common/Util/RectTransformUtils/RectTransformUtils.cs
+++ UltraStar Play/Assets/Common/Util/RectTransformUtils/RectTransformUtils.cs
@@ -7,4 +7,16 @@ public static class RectTransformUtils
         Vector2 localMousePosition = rectTransform.InverseTransformPoint(Input.mousePosition);
         return rectTransform.rect.Contains(localMousePosition);
     }
+
+    public static Rect GetScreenCoordinates(RectTransform rectTransform)
+    {
+        Vector3[] worldCorners = new Vector3[4];
+        rectTransform.GetWorldCorners(worldCorners);
+        Rect result = new Rect(
+            worldCorners[0].x,
+            worldCorners[0].y,
+            worldCorners[2].x - worldCorners[0].x,
+            worldCorners[2].y - worldCorners[0].y);
+        return result;
+    }
 }
