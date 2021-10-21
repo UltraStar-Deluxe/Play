0db23e967714d49e7ba4692a996abfc8903e4ce1
Andreas Stange <anst.dev@gmail.com>
2021-10-21 18:39:33 +0200

    - imp: drag handler for sentence to (a) move all notes of sentence (b) change LinebreakBeat

--- UltraStar Play/Assets/Scenes/SongEditor/EditorSentence/EditorUiSentence.cs
+++ UltraStar Play/Assets/Scenes/SongEditor/EditorSentence/EditorUiSentence.cs
@@ -6,14 +6,20 @@ using UnityEngine;
 using UnityEngine.UI;
 using UniInject;
 using UniRx;
+using UnityEngine.EventSystems;
 
 // Disable warning about fields that are never assigned, their values are injected.
 #pragma warning disable CS0649
 
-public class EditorUiSentence : MonoBehaviour, INeedInjection
+public class EditorUiSentence : MonoBehaviour, INeedInjection, IPointerEnterHandler, IPointerExitHandler
 {
+    private static readonly double handleWidthInPercent = 0.25;
+
     [InjectedInInspector]
     public Image backgroundImage;
+
+    [InjectedInInspector]
+    public RectTransform rightHandle;
 
     public Sentence Sentence { get; private set; }
 
@@ -25,6 +31,23 @@ public class EditorUiSentence : MonoBehaviour, INeedInjection
 
     [Inject]
     private SongEditorSceneController songEditorSceneController;
+
+    [Inject]
+    private UiManager uiManager;
+
+    [Inject]
+    private CursorManager cursorManager;
+
+    public bool IsPointerOver { get; private set; }
+    public bool IsPointerOverRightHandle { get; private set; }
+
+    public void Start()
+    {
+        rightHandle.gameObject.SetActive(false);
+        uiManager.MousePositionChangeEventStream
+            .Subscribe(_ => OnMousePositionChanged())
+            .AddTo(gameObject);
+    }
 
     public void Init(Sentence sentence)
     {
@@ -45,5 +68,75 @@ public class EditorUiSentence : MonoBehaviour, INeedInjection
     public void SetText(string label)
     {
         uiText.text = label;
+    }
+
+    private void OnMousePositionChanged()
+    {
+        if (IsPointerOver)
+        {
+            OnPointerOver();
+        }
+
+        UpdateHandles();
+    }
+
+    private void OnPointerOver()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        Vector2 localPoint = RectTransform.InverseTransformPoint(mousePosition);
+        float width = RectTransform.rect.width;
+        double xPercent = (localPoint.x + (width / 2)) / width;
+        if (xPercent > (1 - handleWidthInPercent))
+        {
+            OnPointerOverRightHandle();
+        }
+        else
+        {
+            OnPointerOverCenter();
+        }
+
+        UpdateHandles();
+    }
+
+    private void OnPointerOverCenter()
+    {
+        IsPointerOverRightHandle = false;
+        cursorManager.SetCursor(ECursor.Grab);
+    }
+
+    private void OnPointerOverRightHandle()
+    {
+        IsPointerOverRightHandle = true;
+        cursorManager.SetCursor(ECursor.ArrowsLeftRight);
+    }
+
+    private void UpdateHandles()
+    {
+        bool isRightHandleVisible = IsPointerOverRightHandle;
+        if (rightHandle.gameObject.activeSelf != isRightHandleVisible)
+        {
+            rightHandle.gameObject.SetActive(isRightHandleVisible);
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        IsPointerOver = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        IsPointerOver = false;
+        IsPointerOverRightHandle = false;
+        UpdateHandles();
+        cursorManager.SetDefaultCursor();
+    }
+
+    public bool IsPositionOverRightHandle(Vector2 position)
+    {
+        Vector2 localPoint = RectTransform.InverseTransformPoint(position);
+        float width = RectTransform.rect.width;
+        double xPercent = (localPoint.x + (width / 2)) / width;
+        return (xPercent > (1 - handleWidthInPercent));
     }
 }
