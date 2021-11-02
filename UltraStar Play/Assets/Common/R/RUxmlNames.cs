1cf358aedf6ee00749d41f59e50732d08e001679
Andreas Stange <anst.dev@gmail.com>
2021-11-02 20:32:37 +0100

    imp: generate C# constants for UXML names and classes

---
+++ UltraStar Play/Assets/Common/R/RUxmlNames.cs
@@ -0,0 +1,71 @@
+﻿// GENERATED CODE. To update this file use the corresponding menu item in the Unity Editor.
+public static partial class R
+{
+    public static class UxmlNames
+    {
+        public const string aboutButtonHashed = "#aboutButton";
+        public const string aboutButton = "aboutButton";
+        public const string background = "background";
+        public const string backgroundHashed = "#background";
+        public const string buildTimeStampText = "buildTimeStampText";
+        public const string buildTimeStampTextHashed = "#buildTimeStampText";
+        public const string buttonContainerHashed = "#buttonContainer";
+        public const string buttonContainer = "buttonContainer";
+        public const string commitHashTextHashed = "#commitHashText";
+        public const string commitHashText = "commitHashText";
+        public const string dialogButtonContainerHashed = "#dialogButtonContainer";
+        public const string dialogButtonContainer = "dialogButtonContainer";
+        public const string dialogCloseButtonHashed = "#dialogCloseButton";
+        public const string dialogCloseButton = "dialogCloseButton";
+        public const string dialogContent = "dialogContent";
+        public const string dialogContentHashed = "#dialogContent";
+        public const string dialogMessageHashed = "#dialogMessage";
+        public const string dialogMessage = "dialogMessage";
+        public const string dialogMessageContainer = "dialogMessageContainer";
+        public const string dialogMessageContainerHashed = "#dialogMessageContainer";
+        public const string dialogTextButtonContainer = "dialogTextButtonContainer";
+        public const string dialogTextButtonContainerHashed = "#dialogTextButtonContainer";
+        public const string dialogTitleHashed = "#dialogTitle";
+        public const string dialogTitle = "dialogTitle";
+        public const string dialogTitleImageHashed = "#dialogTitleImage";
+        public const string dialogTitleImage = "dialogTitleImage";
+        public const string dialogTitleImageAndTextContainerHashed = "#dialogTitleImageAndTextContainer";
+        public const string dialogTitleImageAndTextContainer = "dialogTitleImageAndTextContainer";
+        public const string dummyButton = "dummyButton";
+        public const string dummyButtonHashed = "#dummyButton";
+        public const string ignoreAllFutureVersionsButton = "ignoreAllFutureVersionsButton";
+        public const string ignoreAllFutureVersionsButtonHashed = "#ignoreAllFutureVersionsButton";
+        public const string ignoreThisVersionButton = "ignoreThisVersionButton";
+        public const string ignoreThisVersionButtonHashed = "#ignoreThisVersionButton";
+        public const string jukeboxButtonHashed = "#jukeboxButton";
+        public const string jukeboxButton = "jukeboxButton";
+        public const string NewVersionAvailableDialog = "NewVersionAvailableDialog";
+        public const string NewVersionAvailableDialogHashed = "#NewVersionAvailableDialog";
+        public const string newVersionDialogRootHashed = "#newVersionDialogRoot";
+        public const string newVersionDialogRoot = "newVersionDialogRoot";
+        public const string partyButton = "partyButton";
+        public const string partyButtonHashed = "#partyButton";
+        public const string quitButtonHashed = "#quitButton";
+        public const string quitButton = "quitButton";
+        public const string sceneSubtitleHashed = "#sceneSubtitle";
+        public const string sceneSubtitle = "sceneSubtitle";
+        public const string sceneTitle = "sceneTitle";
+        public const string sceneTitleHashed = "#sceneTitle";
+        public const string semanticVersionText = "semanticVersionText";
+        public const string semanticVersionTextHashed = "#semanticVersionText";
+        public const string settingsButton = "settingsButton";
+        public const string settingsButtonHashed = "#settingsButton";
+        public const string startButton = "startButton";
+        public const string startButtonHashed = "#startButton";
+        public const string titleAndDescriptionHashed = "#titleAndDescription";
+        public const string titleAndDescription = "titleAndDescription";
+        public const string titleContainerHashed = "#titleContainer";
+        public const string titleContainer = "titleContainer";
+        public const string titleImageHashed = "#titleImage";
+        public const string titleImage = "titleImage";
+        public const string titleImageAndTextHashed = "#titleImageAndText";
+        public const string titleImageAndText = "titleImageAndText";
+        public const string versionInfoContainerHashed = "#versionInfoContainer";
+        public const string versionInfoContainer = "versionInfoContainer";
+    }
+}
