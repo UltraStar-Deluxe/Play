1470c9d6b62ac30a15dcc8242b869d6c3145cf09
Andreas Stange <anst.dev@gmail.com>
2021-11-14 19:36:46 +0100

    new: implemented GraphicOptionsScene with UIToolkit

--- UltraStar Play/Assets/Scenes/Options/GraphicOptions/ScreenResolutionPickerControl.cs
+++ UltraStar Play/Assets/Scenes/Options/GraphicOptions/ScreenResolutionPickerControl.cs
@@ -1,35 +1,51 @@
-﻿using System;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
+using UniInject;
 using UniRx;
 
-public class ResolutionSlider : TextItemSlider<ScreenResolution>
+// Disable warning about fields that are never assigned, their values are injected.
+#pragma warning disable CS0649
+
+public class ScreenResolutionPickerControl : LabeledItemPickerControl<ScreenResolution>
 {
-    protected override void Start()
+    public ScreenResolutionPickerControl(ItemPicker itemPicker)
+        : base(itemPicker, GetItems())
     {
-        base.Start();
         if (Application.isEditor)
         {
-            Items = GetDummyResolutions();
             Selection.Value = Items[0];
         }
         else
         {
-            Items = GetResolutions();
             ScreenResolution currentScreenResolution = ApplicationUtils.GetCurrentAppResolution();
-            Selection.Value = Items.Where(it => it.Equals(currentScreenResolution))
-                .FirstOrDefault().OrIfNull(Items[0]);
+            Selection.Value = Items.FirstOrDefault(it => it.Equals(currentScreenResolution))
+                .OrIfNull(Items[0]);
         }
         Selection.Subscribe(newValue => SettingsManager.Instance.Settings.GraphicSettings.resolution = newValue);
     }
 
-    protected override string GetDisplayString(ScreenResolution item)
+    private static List<ScreenResolution> GetItems()
+    {
+        if (Application.isEditor)
+        {
+            return GetDummyResolutions();
+        }
+        else
+        {
+            return GetResolutions();
+        }
+    }
+
+    protected override string GetLabelText(ScreenResolution item)
     {
         return $"{item.Width} x {item.Height} ({item.RefreshRate} Hz)";
     }
 
-    private List<ScreenResolution> GetDummyResolutions()
+    private static List<ScreenResolution> GetDummyResolutions()
     {
         List<ScreenResolution> result = new List<ScreenResolution>();
         result.Add(CreateResolution(800, 600, 60));
@@ -38,13 +54,13 @@ public class ResolutionSlider : TextItemSlider<ScreenResolution>
         return result;
     }
 
-    private ScreenResolution CreateResolution(int width, int height, int refreshRate)
+    private static ScreenResolution CreateResolution(int width, int height, int refreshRate)
     {
         ScreenResolution res = new ScreenResolution(width, height, refreshRate);
         return res;
     }
 
-    private List<ScreenResolution> GetResolutions()
+    private static List<ScreenResolution> GetResolutions()
     {
         List<ScreenResolution> result = Screen.resolutions.Select(it => new ScreenResolution(it)).ToList();
         return result;
