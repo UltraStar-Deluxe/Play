01ce471ef241c34547fdc9386e21cf9e5543a9c0
Andreas Stange <anst.dev@gmail.com>
2021-11-20 17:44:30 +0100

    new: implemented PlayerProfileScene with UIToolkit

---
+++ UltraStar Play/Assets/Common/UIToolkit/PicturedItemPickerControl.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine.UIElements;
+
+abstract public class PicturedItemPickerControl<T> : ListedItemPickerControl<T>
+{
+    public PicturedItemPickerControl(ItemPicker itemPicker, List<T> items)
+        : base(itemPicker)
+    {
+        Selection.Subscribe(UpdateImageElement);
+        Items = items;
+    }
+
+    public virtual void UpdateImageElement(T item)
+    {
+        ItemPicker.ItemLabel.style.backgroundImage = GetBackgroundImageValue(item);
+    }
+
+    protected abstract StyleBackground GetBackgroundImageValue(T item);
+}
---
+++ UltraStar Play/Assets/Scenes/Options/PlayerProfileOptions/DifficultyPickerControl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DifficultyPicker : LabeledItemPickerControl<EDifficulty>
+{
+    public DifficultyPicker(ItemPicker itemPicker)
+        : base(itemPicker, EnumUtils.GetValuesAsList<EDifficulty>())
+    {
+    }
+
+    protected override string GetLabelText(EDifficulty item)
+    {
+        return item.GetTranslatedName();
+    }
+}
