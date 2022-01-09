521da7a2e4b02dd10be785801ffc1e466753fdda
Andreas Stange <anst.dev@gmail.com>
2022-01-09 15:53:55 +0100

    determine current primary InputDevice from most recent input

--- UltraStar Play/Assets/Common/Input/UltraStarPlayInputManager.cs
+++ UltraStar Play/Assets/Common/Input/UltraStarPlayInputManager.cs
@@ -9,7 +9,9 @@ using UnityEngine.InputSystem.EnhancedTouch;
 using PrimeInputActions;
 using UniInject;
 using UniRx;
+using UnityEngine.InputSystem.Controls;
 using UnityEngine.UIElements;
+using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
 
 public class UltraStarPlayInputManager : InputManager, INeedInjection
 {
@@ -17,9 +19,11 @@ public class UltraStarPlayInputManager : InputManager, INeedInjection
     static void Init()
     {
         AdditionalInputActionInfos.Clear();
+        inputDeviceToLastChange.Clear();
     }
 
-    public static List<InputActionInfo> AdditionalInputActionInfos { get; private set; }= new List<InputActionInfo>();
+    public static List<InputActionInfo> AdditionalInputActionInfos { get; private set; } = new List<InputActionInfo>();
+    private static readonly Dictionary<InputDevice, InputDeviceChange> inputDeviceToLastChange = new Dictionary<InputDevice, InputDeviceChange>();
 
     [InjectedInInspector]
     public VectorImage gamepadIcon;
@@ -32,6 +36,19 @@ public class UltraStarPlayInputManager : InputManager, INeedInjection
 
     private Subject<InputDeviceChangeEvent> inputDeviceChangeEventStream = new Subject<InputDeviceChangeEvent>();
     public IObservable<InputDeviceChangeEvent> InputDeviceChangeEventStream => inputDeviceChangeEventStream;
+
+    private EInputDevice inputDeviceEnum = GetDefaultInputDeviceEnum();
+    public EInputDevice InputDeviceEnum {
+        get
+        {
+            return inputDeviceEnum;
+        }
+        private set
+        {
+            inputDeviceEnum = value;
+            inputDeviceChangeEventStream.OnNext(new InputDeviceChangeEvent());
+        }
+    }
 
     [Inject(UxmlName = R.UxmlNames.inputDeviceIcon, Optional = true)]
     private VisualElement inputDeviceIcon;
@@ -61,7 +78,36 @@ public class UltraStarPlayInputManager : InputManager, INeedInjection
 
         if (inputDeviceIcon != null)
         {
+            UpdateInputDeviceIcon();
             InputDeviceChangeEventStream.Subscribe(_ => UpdateInputDeviceIcon());
+        }
+
+        StartCoroutine(CoroutineUtils.ExecuteRepeatedlyInSeconds(0.1f, () => UpdateInputDeviceEnum()));
+    }
+
+    private void UpdateInputDeviceEnum()
+    {
+        // Check any keyboard input
+        if (Keyboard.current != null
+            && Keyboard.current.anyKey.isPressed)
+        {
+            InputDeviceEnum = EInputDevice.KeyboardAndMouse;
+        }
+
+        // Check any gamepad input (of all gamepads)
+        bool anyGamepadButtonPressed = Gamepad.current != null
+                                       && Gamepad.all.AnyMatch(gamepad => gamepad.allControls.Any(inputControl => inputControl is ButtonControl button
+                                           && button.isPressed
+                                           && !button.synthetic));
+        if (anyGamepadButtonPressed)
+        {
+            InputDeviceEnum = EInputDevice.Gamepad;
+        }
+
+        // Check any touch gesture
+        if (Touch.activeTouches.Count > 0)
+        {
+            InputDeviceEnum = EInputDevice.Touch;
         }
     }
 
@@ -76,20 +122,18 @@ public class UltraStarPlayInputManager : InputManager, INeedInjection
 
     private void OnDeviceChange(InputDevice inputDevice, InputDeviceChange inputDeviceChange)
     {
-        Debug.Log($"inputDeviceChange: {inputDeviceChange}");
-        inputDeviceChangeEventStream.OnNext(new InputDeviceChangeEvent(inputDevice, inputDeviceChange));
+        inputDeviceToLastChange[inputDevice] = inputDeviceChange;
+        inputDeviceChangeEventStream.OnNext(new InputDeviceChangeEvent());
     }
 
-    public static EInputDevice GetCurrentInputDeviceEnum()
+    private static EInputDevice GetDefaultInputDeviceEnum()
     {
-        if (Gamepad.current != null
-            && Gamepad.current.enabled)
+        if (Gamepad.current != null)
         {
             return EInputDevice.Gamepad;
         }
 
-        if (Keyboard.current != null
-            && Keyboard.current.enabled)
+        if (Keyboard.current != null)
         {
             return EInputDevice.KeyboardAndMouse;
         }
@@ -97,9 +141,19 @@ public class UltraStarPlayInputManager : InputManager, INeedInjection
         return EInputDevice.Touch;
     }
 
+    private static bool IsLastInputDeviceChangeOneOf(InputDevice inputDevice, params InputDeviceChange[] values)
+    {
+        if (!inputDeviceToLastChange.TryGetValue(inputDevice, out InputDeviceChange inputDeviceChange))
+        {
+            return false;
+        }
+
+        return values.Contains(inputDeviceChange);
+    }
+
     private void UpdateInputDeviceIcon()
     {
-        switch (GetCurrentInputDeviceEnum())
+        switch (inputDeviceEnum)
         {
             case EInputDevice.Gamepad:
                 inputDeviceIcon.style.backgroundImage = new StyleBackground(gamepadIcon);
