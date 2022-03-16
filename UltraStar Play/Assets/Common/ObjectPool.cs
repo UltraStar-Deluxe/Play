f8767b4ec30adcb5255def6ae23b6f2e95849ba4
Andreas Stange <anst.dev@gmail.com>
2022-03-16 20:57:26 +0100

    add ObjectPool

---
+++ UltraStar Play/Assets/Common/ObjectPool.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class ObjectPool<T>
+    where T : class
+{
+    private readonly HashSet<T> usedObjects = new HashSet<T>();
+    private readonly HashSet<T> freeObjects = new HashSet<T>();
+
+    private readonly Action<T> onFreeObject;
+    private readonly Action<T> onUseObject;
+
+    public ObjectPool(Action<T> onFreeObject = null, Action<T> onUseObject = null)
+    {
+        this.onFreeObject = onFreeObject;
+        this.onUseObject = onUseObject;
+    }
+
+    public bool TryGetFreeObject(out T obj)
+    {
+        if (freeObjects.Count > 0)
+        {
+            obj = freeObjects.FirstOrDefault();
+            freeObjects.Remove(obj);
+            usedObjects.Add(obj);
+
+            if (onUseObject != null)
+            {
+                onUseObject(obj);
+            }
+
+            return true;
+        }
+
+        obj = null;
+        return false;
+    }
+
+    public void AddFreeObject(T obj, bool runOnFreeObject = true)
+    {
+        if (freeObjects.Contains(obj))
+        {
+            return;
+        }
+
+        freeObjects.Add(obj);
+        usedObjects.Remove(obj);
+
+        if (runOnFreeObject
+            && onFreeObject != null)
+        {
+            onFreeObject(obj);
+        }
+    }
+
+    public void AddUsedObject(T obj, bool runOnUsedObject = true)
+    {
+        if (usedObjects.Contains(obj))
+        {
+            return;
+        }
+
+        freeObjects.Remove(obj);
+        usedObjects.Add(obj);
+
+        if (runOnUsedObject
+            && onUseObject != null)
+        {
+            onUseObject(obj);
+        }
+    }
+
+    public void FreeAllObjects()
+    {
+        new List<T>(usedObjects).ForEach(obj => AddFreeObject(obj));
+    }
+}
