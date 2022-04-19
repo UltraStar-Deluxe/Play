bd509abc2abd5d6ff6c490bfc4dee045ba4500f1
Andreas Stange <anst.dev@gmail.com>
2022-04-19 12:45:08 +0200

    trying to reduce latency of tcp stream

--- UltraStar Play/Packages/playshared/Runtime/Network/Dto/PositionInSongDto.cs
+++ UltraStar Play/Packages/playshared/Runtime/Network/Dto/PositionInSongDto.cs
@@ -2,7 +2,7 @@ public class PositionInSongDto : CompanionAppMessageDto
 {
     public float SongBpm { get; set; }
     public float SongGap { get; set; }
-    public int PositionInSongInMillis { get; set; }
+    public double PositionInSongInMillis { get; set; }
 
     public PositionInSongDto() : base(CompanionAppMessageType.PositionInSong) { }
 }
