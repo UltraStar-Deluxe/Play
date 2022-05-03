a79d33f766dd9d970217749db6017ea6f468268d
Andreas Stange <anst.dev@gmail.com>
2022-05-03 19:07:55 +0200

    Companion App: single button to iterate over recording devices, instead of one button per device

--- UltraStar Play Companion/Assets/Common/R/RUxmlClasses.cs
+++ UltraStar Play Companion/Assets/Common/R/RUxmlClasses.cs
@@ -3,22 +3,8 @@ public static partial class R
 {
     public static class UxmlClasses
     {
-        public const string contextMenu = "contextMenu";
-        public const string contextMenuSeparator = "contextMenuSeparator";
-        public const string dialogBackground = "dialogBackground";
-        public const string dialogButton = "dialogButton";
-        public const string dialogMessage = "dialogMessage";
-        public const string dialogTitle = "dialogTitle";
-        public const string dialogTitleImage = "dialogTitleImage";
-        public const string focusableNavigatorRoot = "focusableNavigatorRoot";
-        public const string iconImageTextSized = "iconImageTextSized";
-        public const string itemPickerButton = "itemPickerButton";
-        public const string itemPickerItemLabel = "itemPickerItemLabel";
         public const string onlyVisibleWhenConnected = "onlyVisibleWhenConnected";
-        public const string roundCorners = "roundCorners";
-        public const string smallFont = "smallFont";
         public const string startButtonClass = "startButtonClass";
         public const string titleText = "titleText";
-        public const string warning = "warning";
     }
 }
