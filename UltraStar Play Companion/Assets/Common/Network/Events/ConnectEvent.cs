d417f6d72a6b40900ffa9bd2d8e286da96ecc44e
Andreas Stange <anst.dev@gmail.com>
2022-05-06 09:54:12 +0200

    refactoring: renamed "MircophonePort" to "MessagingPort"

--- UltraStar Play Companion/Assets/Common/Network/Events/ConnectEvent.cs
+++ UltraStar Play Companion/Assets/Common/Network/Events/ConnectEvent.cs
@@ -4,7 +4,7 @@ public class ConnectEvent
 {
     public bool IsSuccess { get; set; }
     public int ConnectRequestCount { get; set; }
-    public int MicrophonePort { get; set; }
+    public int MessagingPort { get; set; }
     public int HttpServerPort { get; set; }
     public string ErrorMessage { get; set; }
     public IPEndPoint ServerIpEndPoint { get; set; }
