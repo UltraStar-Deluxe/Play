f3cf594e4046efcf2535133858503932e1961796
Andreas Stange <anst.dev@gmail.com>
2022-05-06 10:42:36 +0200

    refactoring: ConnectedClientHandler references IServerSideConnectRequestManager (the interface)

--- UltraStar Play/Packages/playshared/Runtime/Network/IServerSideConnectRequestManager.cs
+++ UltraStar Play/Packages/playshared/Runtime/Network/IServerSideConnectRequestManager.cs
@@ -1,4 +1,5 @@
 ﻿public interface IServerSideConnectRequestManager
 {
     public bool TryGetConnectedClientHandler(string clientId, out IConnectedClientHandler connectedClientHandler);
+    public void RemoveConnectedClientHandler(IConnectedClientHandler connectedClientHandler);
 }
