a6167ed2e190f867364e423fd9878773a16d631b
Andreas Stange <anst.dev@gmail.com>
2022-06-04 18:23:27 +0200

    Use Unity's implementation to play and stream MP3 files. Removed NLayer library

--- tools/download-dependencies-main-game/download-dependencies-main-game.sh
+++ tools/download-dependencies-main-game/download-dependencies-main-game.sh
@@ -2,7 +2,6 @@
 sh download-leantween.sh
 sh download-unirx.sh
 sh download-fullserializer.sh
-sh download-nlayer.sh
 sh download-csharpsynthforunity.sh
 sh download-text-mesh-pro-resources.sh
 sh download-sharpziplib.sh
