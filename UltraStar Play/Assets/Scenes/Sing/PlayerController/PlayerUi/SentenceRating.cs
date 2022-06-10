a286a14c7489ba6ce3ef43aa0738280cc80b1c6a
Andreas Stange <anst.dev@gmail.com>
2022-06-10 20:06:44 +0200

    reduce number of sentence ratings to 5 (perfect, great, good, ok, bad)

--- UltraStar Play/Assets/Scenes/Sing/PlayerController/PlayerUi/SentenceRating.cs
+++ UltraStar Play/Assets/Scenes/Sing/PlayerController/PlayerUi/SentenceRating.cs
@@ -5,23 +5,17 @@ using UnityEngine;
 public class SentenceRating
 {
     // TODO: Add style sheet classes and define in style sheet
-    private const string SentenceRatingColorPerfect = "#3AFF4E7F";
-    private const string SentenceRatingColorCool = "#7FE82A7F";
-    private const string SentenceRatingColorGreat = "#7FE82A7F";
-    private const string SentenceRatingColorGood = "#E4FF1F7F";
-    private const string SentenceRatingColorNotBad = "#E4FF1F7F";
-    private const string SentenceRatingColorBad = "#FF9C4F7F";
-    private const string SentenceRatingColorPoor = "#E848E67F";
-    private const string SentenceRatingColorAwful = "#764FFF7F";
+    private const string SentenceRatingColorPerfect = "#3AFF4EAF";
+    private const string SentenceRatingColorGreat = "#20CF327F";
+    private const string SentenceRatingColorGood = "#E7B41C7F";
+    private const string SentenceRatingColorNotBad = "#44ABDC7F";
+    private const string SentenceRatingColorBad = "#961CE77F";
 
     public static readonly SentenceRating perfect = new(0.95, R.Messages.rating_sentence_perfect, SentenceRatingColorPerfect);
-    public static readonly SentenceRating cool = new(0.8, R.Messages.rating_sentence_cool, SentenceRatingColorCool);
-    public static readonly SentenceRating great = new(0.7, R.Messages.rating_sentence_great, SentenceRatingColorGreat);
-    public static readonly SentenceRating good = new(0.6, R.Messages.rating_sentence_good, SentenceRatingColorGood);
-    public static readonly SentenceRating notBad = new(0.5, R.Messages.rating_sentence_notBad, SentenceRatingColorNotBad);
-    public static readonly SentenceRating bad = new(0.3, R.Messages.rating_sentence_bad, SentenceRatingColorBad);
-    public static readonly SentenceRating poor = new(0.2, R.Messages.rating_sentence_poor, SentenceRatingColorPoor);
-    public static readonly SentenceRating awful = new(0, R.Messages.rating_sentence_awful, SentenceRatingColorAwful);
+    public static readonly SentenceRating great = new(0.75, R.Messages.rating_sentence_great, SentenceRatingColorGreat);
+    public static readonly SentenceRating good = new(0.50, R.Messages.rating_sentence_good, SentenceRatingColorGood);
+    public static readonly SentenceRating notBad = new(0.25, R.Messages.rating_sentence_notBad, SentenceRatingColorNotBad);
+    public static readonly SentenceRating bad = new(0, R.Messages.rating_sentence_bad, SentenceRatingColorBad);
 
     private readonly string i18nCode;
     public double PercentageThreshold { get; private set; }
@@ -45,13 +39,10 @@ public class SentenceRating
             {
                 values = new List<SentenceRating>();
                 values.Add(perfect);
-                values.Add(cool);
                 values.Add(great);
                 values.Add(good);
                 values.Add(notBad);
                 values.Add(bad);
-                values.Add(poor);
-                values.Add(awful);
             }
             return values;
         }
