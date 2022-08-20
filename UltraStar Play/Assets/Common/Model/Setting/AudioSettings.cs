a3fa2a3cf70eddfb7f27c97722ccaa717910f596
Andreas Stange <anst.dev@gmail.com>
2022-08-20 18:54:09 +0200

    changed default volume

--- UltraStar Play/Assets/Common/Model/Setting/AudioSettings.cs
+++ UltraStar Play/Assets/Common/Model/Setting/AudioSettings.cs
@@ -7,9 +7,9 @@ public class AudioSettings
     // Range: 0..100
     public int PreviewVolumePercent { get; set; } = 50;
     public int VolumePercent { get; set; } = 100;
-    public int BackgroundMusicVolumePercent { get; set; } = 70;
+    public int BackgroundMusicVolumePercent { get; set; } = 50;
 
-    public int SceneChangeSoundVolumePercent { get; set; } = 100;
+    public int SceneChangeSoundVolumePercent { get; set; } = 50;
 
     public EPitchDetectionAlgorithm pitchDetectionAlgorithm = EPitchDetectionAlgorithm.Dywa;
 }
