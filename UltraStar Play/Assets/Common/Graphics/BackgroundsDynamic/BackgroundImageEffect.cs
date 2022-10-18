f1be4c3dea536b6ec2d8d329a18275f4b6633013
Jean Moreno <jean.moreno.public+github@gmail.com>
2022-10-18 11:12:45 +0200

    Themes: overhauled scene change system so that it works with new themes
    Previously the UI was rendered into a RenderTexture, which was copied to a Texture2D, which was rendered as a new VisualElement in the UIDocument and then animated using its style.
    The new system works entirely through the GPU:
    - A copy of the current target render texture of the UIDocument is made when the transition begins (the UI is always rendered to a render texture to work with the new themes system)
    - This copy is provided to the background shader
    - The animation keyword and time are provided to the background shader, which handles the scaling up and fade animations of both the new UI and the old one
    
    This allows more advanced shader-based transitions easily in the future.

--- UltraStar Play/Assets/Common/Graphics/BackgroundsDynamic/BackgroundImageEffect.cs
+++ UltraStar Play/Assets/Common/Graphics/BackgroundsDynamic/BackgroundImageEffect.cs
@@ -1,5 +1,5 @@
-using UnityEngine;
 using UniInject;
+using UnityEngine;
 
 // Disable warning about fields that are never assigned, their values are injected.
 #pragma warning disable CS0649
@@ -7,8 +7,22 @@ using UniInject;
 [ExecuteInEditMode]
 public class BackgroundImageEffect : MonoBehaviour, INeedInjection
 {
-    public Material material;
+    [SerializeField] Material material;
     static readonly int _UiTex = Shader.PropertyToID("_UiTex");
+    static readonly int _TransitionTex = Shader.PropertyToID("_TransitionTex");
+    static readonly int _TransitionTime = Shader.PropertyToID("_TransitionTime");
+
+    public static BackgroundImageEffect Instance;
+
+    void Awake()
+    {
+        if (Instance != null)
+        {
+            // Can happen when the parent singleton is loaded with a new scene, this will be deleted with it
+            return;
+        }
+        Instance = this;
+    }
 
     void OnEnable()
     {
@@ -18,13 +32,25 @@ public class BackgroundImageEffect : MonoBehaviour, INeedInjection
         }
     }
 
-    public void SetUiRenderTexture(RenderTexture uiRenderTexture)
-    {
-        material.SetTexture(_UiTex, uiRenderTexture);
-    }
-
     void OnRenderImage(RenderTexture src, RenderTexture dest)
     {
         Graphics.Blit(src, dest, material);
+    }
+
+    public void SetUiRenderTextures(RenderTexture uiRenderTexture, RenderTexture transitionRenderTexture)
+    {
+        material.SetTexture(_UiTex, uiRenderTexture);
+        material.SetTexture(_TransitionTex, transitionRenderTexture);
+    }
+
+    public void EnableTransition(bool enable)
+    {
+        if (enable) material.EnableKeyword("_UI_TRANSITION_ANIM");
+        else material.DisableKeyword("_UI_TRANSITION_ANIM");
+    }
+
+    public void SetTransitionTime(float time)
+    {
+        material.SetFloat(_TransitionTime, time);
     }
 }
--- UltraStar Play/Assets/Common/Scene/UltraStarPlaySceneChangeAnimationControl.cs
+++ UltraStar Play/Assets/Common/Scene/UltraStarPlaySceneChangeAnimationControl.cs
@@ -1,18 +1,13 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using SceneChangeAnimations;
 using UnityEngine;
 using UnityEngine.UIElements;
 using UniInject;
-using UniRx;
 using UnityEngine.SceneManagement;
 
 // Disable warning about fields that are never assigned, their values are injected.
 #pragma warning disable CS0649
 
-public class UltraStarPlaySceneChangeAnimationControl : SceneChangeAnimationControl, INeedInjection
+public class UltraStarPlaySceneChangeAnimationControl : MonoBehaviour, INeedInjection
 {
     private static UltraStarPlaySceneChangeAnimationControl instance;
     public static UltraStarPlaySceneChangeAnimationControl Instance
@@ -31,24 +26,59 @@ public class UltraStarPlaySceneChangeAnimationControl : SceneChangeAnimationCont
         }
     }
 
+    [NonSerialized] public RenderTexture uiCopyRenderTexture;
+    private Action animateAction;
+
     [Inject(SearchMethod = SearchMethods.GetComponentInChildren)]
     private AudioSource audioSource;
 
     [Inject]
     private Settings settings;
 
-    public override void Start()
+    void Start()
     {
+        if (this != instance) return;
+
         UltraStarPlaySceneChangeAnimationControl self = this;
         GameObjectUtils.TryInitSingleInstanceWithDontDestroyOnLoad(ref instance, ref self);
 
-        if (instance == this)
-        {
-            base.Start();
-        }
+        // Start is not called again for this DontDestroyOnLoad-object
+        DontDestroyOnLoad(gameObject);
+
+        uiCopyRenderTexture = new RenderTexture(Screen.width, Screen.height, 24);
     }
 
-    public void StartSceneChangeAnimation(VisualElement visualElement, EScene currentScene, EScene nextScene)
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode loadSceneMode)
+    {
+        if (!settings.GraphicSettings.AnimateSceneChange)
+        {
+            return;
+        }
+
+        animateAction?.Invoke();
+    }
+
+    public void AnimateChangeToScene(Action doLoadSceneAction, Action doAnimateAction)
+    {
+        animateAction = doAnimateAction;
+
+        // Take "screenshot" of "old" scene.
+        UIDocument uiDocument = FindObjectOfType<UIDocument>();
+        Graphics.CopyTexture(uiDocument.panelSettings.targetTexture, uiCopyRenderTexture);
+        doLoadSceneAction();
+    }
+
+    public void StartSceneChangeAnimation(EScene currentScene, EScene nextScene)
     {
         bool skipSceneChangeAnimationSound = nextScene == EScene.SingScene
                                              || currentScene == EScene.SingingResultsScene;
@@ -57,38 +87,29 @@ public class UltraStarPlaySceneChangeAnimationControl : SceneChangeAnimationCont
             PlaySceneChangeAnimationSound();
         }
 
-        VisualElement newUiDocument = FindObjectOfType<UIDocument>().rootVisualElement.Q<VisualElement>("background");
+        VisualElement newUiDocument = FindObjectOfType<UIDocument>().rootVisualElement;
         LeanTween.value(gameObject, 0, 1, 0.3f)
-            .setOnUpdate((float animTimePercent) =>
+            .setOnStart(() =>
             {
-                // Scale and fade in the new UIDocument
-                float tEaseOutSine = Mathf.Sin(animTimePercent * Mathf.PI / 2f);
-                newUiDocument.style.opacity = tEaseOutSine;
-                float scaleIn = Mathf.Lerp(0.8f, 1.0f, tEaseOutSine);
-                newUiDocument.style.scale = new StyleScale(new Scale(new Vector3(scaleIn, scaleIn, 1)));
-
-                // Scale and fade out the snapshot of the old UIDocument
-                float tEaseInSine = 1f - Mathf.Cos(animTimePercent * Mathf.PI / 2f);
-                visualElement.style.opacity = 1 - (tEaseInSine * 2);
-                float scaleOut = Mathf.Lerp(1.0f, 1.0f / 0.4f, tEaseInSine);
-                visualElement.style.scale = new StyleScale(new Scale(new Vector3(scaleOut, scaleOut, 1)));
+                BackgroundImageEffect.Instance.EnableTransition(true);
             })
-            .setOnComplete(visualElement.RemoveFromHierarchy);
+            .setOnUpdate(animTimePercent =>
+            {
+                // Scale and fade out the snapshot of the old UIDocument.
+                // Handled by the background shader to get correct premultiplied
+                // blending and avoid the one-frame flicker issue.
+                BackgroundImageEffect.Instance.SetTransitionTime(animTimePercent);
+            })
+            .setEaseInSine()
+            .setOnComplete(() =>
+            {
+                BackgroundImageEffect.Instance.EnableTransition(false);
+            });
     }
 
     private void PlaySceneChangeAnimationSound()
     {
         audioSource.volume = settings.AudioSettings.SceneChangeSoundVolumePercent / 100f;
         audioSource.Play();
-    }
-
-    protected override void OnSceneLoaded(Scene scene, LoadSceneMode loadSceneMode)
-    {
-        if (!settings.GraphicSettings.AnimateSceneChange)
-        {
-            return;
-        }
-        
-        base.OnSceneLoaded(scene, loadSceneMode);
     }
 }
