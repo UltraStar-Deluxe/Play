7af41333d2bf9ca07f4be6b3aeeef0246585adc7
Andreas Stange <anst.dev@gmail.com>
2022-11-18 12:56:13 +0100

    added boostrap icons and custom VisualElement to use them
    - MaterialIcon and BootstrapIcon both extend new common base class FontIcon
    - load codepoints of font icons from TextAsset

---
+++ UltraStar Play/Packages/playshared/Runtime/UI/BootstrapIcon.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class BootstrapIcon : FontIcon
+{
+    private static Dictionary<string, string> bootstrapIconNameToCodepointCache;
+
+    // UIToolkit factory classes
+    public new class UxmlFactory : UxmlFactory<BootstrapIcon, UxmlTraits> {}
+
+    public new class UxmlTraits : FontIcon.UxmlTraits {}
+
+    protected override bool TryGetCodepointByIconName(string iconName, out string codepoint)
+    {
+        if (TryGetCodepointByIconNameFromTextAsset(
+            iconName,
+            "BootstrapIcons",
+            bootstrapIconNameToCodepointCache,
+            out string codepointDecimal))
+        {
+            string codepointHex = int.Parse(codepointDecimal).ToString("X");
+            codepoint = codepointHex;
+            return true;
+        }
+
+        codepoint = "";
+        return false;
+    }
+
+    protected override string GetIconFontUssClass()
+    {
+        return "bootstrapIcon";
+    }
+}
