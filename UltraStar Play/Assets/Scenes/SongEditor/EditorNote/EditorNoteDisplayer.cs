1f5d9fe606a7f7586cd3f731a8942ed12fded1a2
Andreas Stange <anst.dev@gmail.com>
2022-12-15 08:51:11 +0100

    SongEditor: notes on copy and paste layer are not editable

--- UltraStar Play/Assets/Scenes/SongEditor/EditorNote/EditorNoteDisplayer.cs
+++ UltraStar Play/Assets/Scenes/SongEditor/EditorNote/EditorNoteDisplayer.cs
@@ -548,6 +548,12 @@ public class EditorNoteDisplayer : MonoBehaviour, INeedInjection
             editorNoteControl.ShowLabels();
         }
 
+        if (songEditorLayerManager.TryGetLayer(note, out SongEditorLayer layer)
+            && layer.LayerEnum == ESongEditorLayer.CopyPaste)
+        {
+            editorNoteControl.IsEditable = false;
+        }
+
         return editorNoteControl;
     }
 
