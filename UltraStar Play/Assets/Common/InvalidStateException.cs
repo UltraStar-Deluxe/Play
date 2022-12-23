93d4d71b0ba51fb5391038e958d54474c4dd92c3
Andreas Stange <anst.dev@gmail.com>
2022-12-23 10:44:34 +0100

    SpeechRecognitionManager: fix NPE when modelPath was null

---
+++ UltraStar Play/Assets/Common/InvalidStateException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class InvalidStateException : Exception
+{
+    public InvalidStateException(string message) : base(message)
+    {
+    }
+
+    public InvalidStateException(string message, Exception innerException) : base(message, innerException)
+    {
+    }
+}
