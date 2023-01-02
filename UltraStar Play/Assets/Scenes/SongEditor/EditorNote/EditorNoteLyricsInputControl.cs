ebbb38b55a12b3d4367b78a668cca7e6f2c1d335
Andreas Stange <anst.dev@gmail.com>
2023-01-02 19:37:42 +0100

    SongEditor: split note when using space or semicolon in new lyrics (only when editing single note)
    # Conflicts:
    #	UltraStar Play/Assets/Scenes/SongEditor/SongEditorScene.unity

--- UltraStar Play/Assets/Scenes/SongEditor/EditorNote/EditorNoteLyricsInputControl.cs
+++ UltraStar Play/Assets/Scenes/SongEditor/EditorNote/EditorNoteLyricsInputControl.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
 using UniInject;
+using UnityEngine;
 
 // Disable warning about fields that are never assigned, their values are injected.
 #pragma warning disable CS0649
@@ -10,6 +13,9 @@ public class EditorNoteLyricsInputControl : EditorLyricsInputPopupControl
     [Inject]
     private EditorNoteControl editorNoteControl;
 
+    [Inject]
+    private SongEditorLayerManager layerManager;
+
     protected override string GetInitialText()
     {
         return ShowWhiteSpaceText.ReplaceWhiteSpaceWithVisibleCharacters(editorNoteControl.Note.Text);
@@ -18,7 +24,10 @@ public class EditorNoteLyricsInputControl : EditorLyricsInputPopupControl
     protected override void PreviewNewText(string newText)
     {
         // Immediately apply changed lyrics to notes, but do not record it in the history.
-        ApplyEditModeText(newText, false);
+        string visibleWhiteSpaceText = ShowWhiteSpaceText.ReplaceWhiteSpaceWithVisibleCharacters(newText);
+        editorNoteControl.Note.SetText(visibleWhiteSpaceText);
+        editorNoteControl.SetLyrics(visibleWhiteSpaceText);
+        songMetaChangeEventStream.OnNext(new LyricsChangedEvent { Undoable = false});
     }
 
     protected override void ApplyNewText(string newText)
@@ -30,24 +39,116 @@ public class EditorNoteLyricsInputControl : EditorLyricsInputPopupControl
     {
         string viewModeText = ShowWhiteSpaceText.ReplaceVisibleCharactersWithWhiteSpace(newText);
 
+        if (LyricsUtils.IsOnlyWhitespace(newText))
+        {
+            return;
+        }
+
         // Replace multiple control characters with a single character
         viewModeText = Regex.Replace(viewModeText, @"\s+", " ");
         viewModeText = Regex.Replace(viewModeText, @";+", ";");
 
-        // Replace any text after control characters.
+        // Split note to apply space and semicolon control characters.
         // Otherwise the text would mess up following notes when using the LyricsArea.
-        viewModeText = Regex.Replace(viewModeText, @" .+", " ");
-        viewModeText = Regex.Replace(viewModeText, @";.+", ";");
+        List<Note> notesAfterSplit = SplitNoteForNewText(editorNoteControl.Note, viewModeText);
 
-        // Remove the semicolon to separate notes. In contrast, a leading / trailing space needs to be preserved.
-        viewModeText = viewModeText.Replace(";", "");
-
-        if (!LyricsUtils.IsOnlyWhitespace(newText))
+        if (notesAfterSplit.Count > 1)
         {
-            string visibleWhiteSpaceText = ShowWhiteSpaceText.ReplaceWhiteSpaceWithVisibleCharacters(viewModeText);
-            editorNoteControl.Note.SetText(visibleWhiteSpaceText);
-            editorNoteControl.SetLyrics(visibleWhiteSpaceText);
+            // Note has been split
+            songMetaChangeEventStream.OnNext(new NotesSplitEvent() { Undoable = undoable});
+        }
+        else
+        {
+            viewModeText = viewModeText.Replace(";", "");
+            editorNoteControl.Note.SetText(viewModeText);
+            editorNoteControl.SyncWithNote();
             songMetaChangeEventStream.OnNext(new LyricsChangedEvent { Undoable = undoable});
         }
+    }
+
+    public List<Note> SplitNoteForNewText(Note note, string newText)
+    {
+        List<Note> notesAfterSplit = new List<Note> { note };
+        if (note.Length <= 1
+            || LyricsUtils.IsOnlyWhitespace(newText))
+        {
+            return notesAfterSplit;
+        }
+
+        List<int> splitIndexes = AllIndexesOfCharacterBeforeTextEnd(newText, ' ')
+            .ToList()
+            .Union(AllIndexesOfCharacterBeforeTextEnd(newText, ';').ToList())
+            .Distinct()
+            .ToList();
+        splitIndexes.Sort();
+        if (splitIndexes.IsNullOrEmpty())
+        {
+            // Nothing to split
+            return notesAfterSplit;
+        }
+
+        splitIndexes = splitIndexes
+            .Select(index => index + 1)
+            .ToList();
+
+        if (!splitIndexes.Contains(newText.Length))
+        {
+            splitIndexes.Add(newText.Length);
+        }
+
+        List<int> splitBeats = splitIndexes
+            .Select(index => (int)Math.Floor(note.StartBeat + note.Length * ((double)index / newText.Length)))
+            .ToList();
+
+        // Change original note
+        note.SetEndBeat(splitBeats[0]);
+        note.SetText(newText.Substring(0, splitIndexes[0]));
+
+        int lastSplitIndex = splitIndexes[0];
+        int lastSplitBeat = note.EndBeat;
+
+        // Start from 1 because original note was changed already above
+        for (int i = 1; i < splitIndexes.Count; i++)
+        {
+            int splitIndex = splitIndexes[i];
+            int splitBeat = splitBeats[i];
+
+            int newNoteStartBeat = lastSplitBeat;
+            int newNoteEndBeat = splitBeat;
+            int length = splitIndex - lastSplitIndex;
+            string newNoteText = newText.Substring(lastSplitIndex, length);
+
+            Note newNote = new(note.Type, newNoteStartBeat, newNoteEndBeat - newNoteStartBeat, note.TxtPitch, newNoteText);
+            notesAfterSplit.Add(newNote);
+            newNote.SetSentence(note.Sentence);
+            if (layerManager.TryGetEnumLayer(note, out SongEditorEnumLayer songEditorLayer))
+            {
+                layerManager.AddNoteToEnumLayer(songEditorLayer.LayerEnum, newNote);
+            }
+
+            lastSplitIndex = splitIndex;
+            lastSplitBeat = splitBeat;
+        }
+
+        // Remove semicolon from lyrics. These are only used to separate notes in the song editor.
+        notesAfterSplit.ForEach(currentNote =>
+            currentNote.SetText(currentNote.Text.Replace(";", "")));
+
+        return notesAfterSplit;
+    }
+
+    private static List<int> AllIndexesOfCharacterBeforeTextEnd(string text, char searchChar)
+    {
+        List<int> result = new();
+        for (int i = 0; i < text.Length - 1; i++)
+        {
+            char c = text[i];
+            if (c == searchChar)
+            {
+                result.Add(i);
+            }
+        }
+
+        return result;
     }
 }
