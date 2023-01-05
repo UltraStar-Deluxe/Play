1e8d6b36e021e78b0007f5531b88df9e85de16e0
Sven Kubisch <supernevi@users.noreply.github.com>
2023-01-05 12:11:41 +0100

    Basic webcam-configuration added in options menu #342

---
+++ UltraStar Play/Assets/Scenes/Options/WebcamOptions/WebcamOptionsControl.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using PrimeInputActions;
+using ProTrans;
+using Serilog.Core;
+using UniInject;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+// Disable warning about fields that are never assigned, their values are injected.
+#pragma warning disable CS0649
+
+public class WebcamOptionsControl : MonoBehaviour, INeedInjection, ITranslator
+{
+    [Inject]
+    private SceneNavigator sceneNavigator;
+
+    [Inject(UxmlName = R.UxmlNames.sceneTitle)]
+    private Label sceneTitle;
+
+    [Inject(UxmlName = R.UxmlNames.deviceContainer)]
+    private VisualElement deviceContainer;
+
+    [Inject(UxmlName = R.UxmlNames.webcamRenderContainer)]
+    private Image webcamRenderContainer;
+
+    [Inject(UxmlName = R.UxmlNames.backButton)]
+    private Button backButton;
+
+    [Inject]
+    private Settings settings;
+
+    private LabeledItemPickerControl<WebCamDevice> devicePickerControl;
+    private WebCamTexture webcamTexture;
+
+    private void Start()
+    {
+        InitWebcamPicker();
+
+        backButton.RegisterCallbackButtonTriggered(navigateBack);
+        backButton.Focus();
+
+        InputManager.GetInputAction(R.InputActions.usplay_back).PerformedAsObservable(5).Subscribe(_ => navigateBack());
+    }
+
+    public void UpdateTranslation()
+    {
+        if (!Application.isPlaying && backButton == null)
+        {
+            SceneInjectionManager.Instance.DoInjection();
+        }
+        deviceContainer.Q<Label>().text = TranslationManager.GetTranslation(R.Messages.options_webcam_device);
+        backButton.text = TranslationManager.GetTranslation(R.Messages.back);
+        sceneTitle.text = TranslationManager.GetTranslation(R.Messages.options_webcam_title);
+    }
+
+    private void InitWebcamPicker()
+    {
+        devicePickerControl = new LabeledItemPickerControl<WebCamDevice>(deviceContainer.Q<ItemPicker>(), WebCamTexture.devices.ToList());
+        devicePickerControl.GetLabelTextFunction = device => device.name;
+        if (!TryReSelectLastWebcam() && devicePickerControl.Items.Count > 0)
+        {
+            devicePickerControl.Selection.Value = devicePickerControl.Items[0];
+        }
+        devicePickerControl.Selection
+            .Subscribe(device =>
+            {
+                settings.LastWebcamNameInWebcamOptionsScene = device.name;
+                if (webcamTexture != null && webcamTexture.isPlaying)
+                {
+                    webcamTexture.Stop();
+                }
+                webcamTexture = new WebCamTexture(device.name);
+                Log.Logger.Information("Setting current webcam to '{webcamname}'", webcamTexture.deviceName);
+                webcamTexture.Play();
+
+                webcamRenderContainer.image = webcamTexture;
+            });
+    }
+
+    private void navigateBack()
+    {
+        if (webcamTexture != null && webcamTexture.isPlaying)
+        {
+            webcamTexture.Stop();
+        }
+        sceneNavigator.LoadScene(EScene.OptionsScene);
+    }
+
+    private bool TryReSelectLastWebcam()
+    {
+        if (settings.LastWebcamNameInWebcamOptionsScene.IsNullOrEmpty())
+        {
+            return false;
+        }
+
+        WebCamDevice lastDevice = devicePickerControl.Items
+            .FirstOrDefault(device => device.name == settings.LastWebcamNameInWebcamOptionsScene);
+
+        devicePickerControl.SelectItem(lastDevice);
+        return true;
+    }
+}
