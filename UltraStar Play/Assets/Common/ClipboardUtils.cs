d199b8a28ef54d672fb42b7e2958e0331028dc21
Andreas Stange <anst.dev@gmail.com>
2023-01-26 11:27:17 +0100

    copy to clipboard using Unity's native API

--- UltraStar Play/Assets/Common/ClipboardUtils.cs
+++ UltraStar Play/Assets/Common/ClipboardUtils.cs
@@ -1,29 +1,7 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using UnityEngine;
+﻿using UnityEngine;
 
-// Makes use of https://github.com/yasirkula/UnityIngameDebugConsole
 public static class ClipboardUtils
 {
-#if !UNITY_EDITOR && UNITY_ANDROID
-    private static AndroidJavaClass ajc = null;
-    private static AndroidJavaClass AJC
-    {
-        get
-        {
-            if( ajc == null )
-                ajc = new AndroidJavaClass( "com.yasirkula.unity.DebugConsole" );
-
-            return ajc;
-        }
-    }
-#elif !UNITY_EDITOR && UNITY_IOS
-    [System.Runtime.InteropServices.DllImport( "__Internal" )]
-    private static extern void _DebugConsole_CopyText( string text );
-#endif
-
     public static void CopyToClipboard(string text)
     {
         if (string.IsNullOrEmpty(text))
@@ -31,12 +9,6 @@ public static class ClipboardUtils
             return;
         }
 
-#if UNITY_EDITOR || UNITY_2018_1_OR_NEWER || ( !UNITY_ANDROID && !UNITY_IOS )
         GUIUtility.systemCopyBuffer = text;
-#elif UNITY_ANDROID
-			AJC.CallStatic( "CopyText", Context, log );
-#elif UNITY_IOS
-			_DebugConsole_CopyText( log );
-#endif
     }
 }
