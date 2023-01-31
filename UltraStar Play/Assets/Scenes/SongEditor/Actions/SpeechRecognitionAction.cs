c333a2a9dc9397365afd90495499335d94294561
Andreas Stange <anst.dev@gmail.com>
2023-01-31 15:11:22 +0100

    fix build

--- UltraStar Play/Assets/Scenes/SongEditor/Actions/SpeechRecognitionAction.cs
+++ UltraStar Play/Assets/Scenes/SongEditor/Actions/SpeechRecognitionAction.cs
@@ -1,14 +1,11 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Threading;
 using UniInject;
 using UniRx;
-using UnityEditor.Search;
 using UnityEngine;
 using UnityEngine.UIElements;
-using Vosk;
 
 // Disable warning about fields that are never assigned, their values are injected.
 #pragma warning disable CS0649
