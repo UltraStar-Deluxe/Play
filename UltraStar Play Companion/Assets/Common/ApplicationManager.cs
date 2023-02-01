0f4423c787f1cb91c1587d64ea744d8d18a262d9
Andreas Stange <anst.dev@gmail.com>
2023-02-01 22:19:26 +0100

    control main game mouse via companion app
    - use virtual InputDevice to avoid conflicts with real input device

--- UltraStar Play Companion/Assets/Common/ApplicationManager.cs
+++ UltraStar Play Companion/Assets/Common/ApplicationManager.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using UniInject;
+using UniRx;
 using UnityEngine;
+using UnityEngine.InputSystem.EnhancedTouch;
+using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
 
 public class ApplicationManager : AbstractSingletonBehaviour, INeedInjection
 {
@@ -18,13 +21,23 @@ public class ApplicationManager : AbstractSingletonBehaviour, INeedInjection
     [Range(-1, 60)]
     public int targetFrameRate = 30;
 
-
     [Inject]
     private Settings settings;
 
+    private readonly Subject<Finger> fingerUpEventStream = new();
+    public IObservable<Finger> FingerUpEventStream => fingerUpEventStream;
+
+    private readonly Subject<Finger> fingerDownEventStream = new();
+    public IObservable<Finger> FingerDownEventStream => fingerDownEventStream;
+    
     protected override object GetInstance()
     {
         return Instance;
+    }
+
+    protected override void AwakeSingleton()
+    {
+        EnhancedTouchSupport.Enable();
     }
 
     protected override void StartSingleton()
@@ -37,11 +50,15 @@ public class ApplicationManager : AbstractSingletonBehaviour, INeedInjection
     protected override void OnEnableSingleton()
     {
         Application.logMessageReceivedThreaded += Log.HandleUnityLog;
+        Touch.onFingerDown += OnFingerDown;
+        Touch.onFingerUp += OnFingerUp;
     }
 
     protected override void OnDisableSingleton()
     {
         Application.logMessageReceivedThreaded -= Log.HandleUnityLog;
+        Touch.onFingerDown -= OnFingerDown;
+        Touch.onFingerUp -= OnFingerUp;
     }
 
     void Update()
@@ -88,12 +105,22 @@ public class ApplicationManager : AbstractSingletonBehaviour, INeedInjection
         {
             if (PlatformUtils.IsStandalone)
             {
-                return System.Environment.GetCommandLineArgs();
+                return Environment.GetCommandLineArgs();
             }
             else
             {
                 return Array.Empty<string>();
             }
         }
+    }
+    
+    private void OnFingerUp(Finger obj)
+    {
+       fingerUpEventStream.OnNext(obj);
+    }
+    
+    private void OnFingerDown(Finger obj)
+    {
+       fingerDownEventStream.OnNext(obj);
     }
 }
