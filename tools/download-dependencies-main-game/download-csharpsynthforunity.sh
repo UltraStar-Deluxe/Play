acadb33e69ee7726ac102ee6eecdc5e4ce3c0b08
Andreas Stange <anst.dev@gmail.com>
2023-02-03 13:13:55 +0100

    use full general midi soundbank

--- tools/download-dependencies-main-game/download-csharpsynthforunity.sh
+++ tools/download-dependencies-main-game/download-csharpsynthforunity.sh
@@ -13,8 +13,7 @@ git init
 git remote add origin https://github.com/UltraStar-Deluxe/CSharpSynthForUnity.git
 git config core.sparsecheckout true
 echo "Assets/*" >> .git/info/sparse-checkout
-# UltraStar-Play-subset is a dedicated branch for the UltraStar Play project
-git pull --depth=100 origin UltraStar-Play-subset
+git pull --depth=100 origin master
 # Commit from 12 October 2020: 0e1163517a9e5561cd6862ecd0d032803dea8f51
 git checkout 0e1163517a9e5561cd6862ecd0d032803dea8f51
 
