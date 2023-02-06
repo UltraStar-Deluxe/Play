1312f0f1185517e827a31bd2dd6a4ff56c6635ea
Andreas Stange <anst.dev@gmail.com>
2023-02-06 16:16:46 +0100

    rest api: get / set whole Settings data structure as request content

---
+++ UltraStar Play/Packages/playshared/Runtime/Util/Extensions/HttpListenerRequestExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using System.Net;
+
+public static class HttpListenerRequestExtensions
+{
+    public static string GetBodyAsString(this HttpListenerRequest request)
+    {
+        if (!request.HasEntityBody)
+        {
+            return null;
+        }
+
+        using Stream body = request.InputStream;
+        using StreamReader reader = new(body, request.ContentEncoding);
+        return reader.ReadToEnd();
+    }
+}
