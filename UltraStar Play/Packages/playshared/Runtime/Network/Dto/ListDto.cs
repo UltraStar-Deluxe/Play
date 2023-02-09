a034fc28ee466654c129ad6b27e3f9c1d1bdd51b
Andreas Stange <anst.dev@gmail.com>
2023-02-09 19:59:25 +0100

    optionally init ListDto in constructor

--- UltraStar Play/Packages/playshared/Runtime/Network/Dto/ListDto.cs
+++ UltraStar Play/Packages/playshared/Runtime/Network/Dto/ListDto.cs
@@ -3,4 +3,13 @@
 public class ListDto<T> : JsonSerializable
 {
     public List<T> Items { get; set; } = new();
+
+    public ListDto()
+    {
+    }
+
+    public ListDto(List<T> items)
+    {
+        Items = items;
+    }
 }
