7e12b509b05cc193c8195208cb5a72c8a2636f41
Andreas Stange <anst.dev@gmail.com>
2023-02-09 12:40:41 +0100

    general class to set "firstChild" and "lastChild" uss class on VisualElement

---
+++ UltraStar Play/Packages/playshared/Runtime/UI/SetFirstAndLastChildClassControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using UnityEngine.UIElements;
+
+public class SetFirstAndLastChildClassControl
+{
+    public VisualElement FirstChild { get; private set; }
+    public VisualElement LastChild { get; private set; }
+
+    private readonly VisualElement visualElement;
+
+    public SetFirstAndLastChildClassControl(VisualElement visualElement)
+    {
+        this.visualElement = visualElement;
+        visualElement.RegisterCallback<GeometryChangedEvent>(UpdateChildClasses);
+    }
+
+    private void UpdateChildClasses(GeometryChangedEvent evt)
+    {
+        VisualElement newFirstChild = visualElement.Children().FirstOrDefault();
+        if (newFirstChild != FirstChild)
+        {
+            newFirstChild?.AddToClassList("firstChild");
+            FirstChild?.RemoveFromClassList("firstChild");
+            FirstChild = newFirstChild;
+        }
+        
+        VisualElement newLastChild = visualElement.Children().LastOrDefault();
+        if (newLastChild != LastChild)
+        {
+            newLastChild?.AddToClassList("lastChild");
+            LastChild?.RemoveFromClassList("lastChild");
+            LastChild = newLastChild;
+        }
+    }
+}
