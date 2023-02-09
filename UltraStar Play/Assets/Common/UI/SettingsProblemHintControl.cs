f7a5934ecf14328bb4da4cedb8869e785f196a20
Andreas Stange <anst.dev@gmail.com>
2023-02-09 20:00:24 +0100

    fix create Tooltip without Injection

--- UltraStar Play/Assets/Common/UI/SettingsProblemHintControl.cs
+++ UltraStar Play/Assets/Common/UI/SettingsProblemHintControl.cs
@@ -1,13 +1,9 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using ProTrans;
-using UnityEngine;
-using UnityEngine.UIElements;
 using UniInject;
-using UniRx;
+using UnityEngine.UIElements;
 
 // Disable warning about fields that are never assigned, their values are injected.
 #pragma warning disable CS0649
@@ -17,9 +13,7 @@ public class SettingsProblemHintControl
     public SettingsProblemHintControl(VisualElement visualElement, List<string> settingsProblems, Injector injector)
     {
         visualElement.SetVisibleByDisplay(!settingsProblems.IsNullOrEmpty());
-        TooltipControl settingsProblemTooltipControl = injector
-            .WithRootVisualElement(visualElement)
-            .CreateAndInject<TooltipControl>();
+        TooltipControl settingsProblemTooltipControl = new TooltipControl(visualElement);
         settingsProblemTooltipControl.TooltipText = settingsProblems.JoinWith("\n\n");
     }
 
