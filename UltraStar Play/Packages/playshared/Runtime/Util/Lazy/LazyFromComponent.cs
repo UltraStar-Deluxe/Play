c47ce328ea4df56452c4aa2e7933e875aeaa9018
Andreas Stange <anst.dev@gmail.com>
2023-02-20 18:48:55 +0100

    add LazyFromContext utility class

---
+++ UltraStar Play/Packages/playshared/Runtime/Util/Lazy/LazyFromComponent.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+
+public class LazyFromComponent<T> : LazyFromContext<Component, T>
+{
+    public LazyFromComponent(Func<Component, T> valueProvider)
+        : base(valueProvider)
+    {
+    }
+}
---
+++ UltraStar Play/Packages/playshared/Runtime/Util/Lazy/LazyFromContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class LazyFromContext<CONTEXT, T>
+{
+    private T value;
+    
+    public bool IsValueLoaded { get; private set; }
+
+    private readonly Func<CONTEXT, T> valueProvider;
+
+    public LazyFromContext(Func<CONTEXT, T> valueProvider)
+    {
+        this.valueProvider = valueProvider;
+    }
+
+    public T GetValue(CONTEXT context)
+    {
+        if (!IsValueLoaded)
+        {
+            value = valueProvider.Invoke(context);
+            IsValueLoaded = true;
+        }
+
+        return value;
+    }
+}
