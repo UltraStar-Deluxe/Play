8c5801770151b70647f07c4df9cffd5a0e03ae66
Andreas Stange <anst.dev@gmail.com>
2023-02-24 12:32:08 +0100

    general classes for mic icon with progress bar

---
+++ UltraStar Play/Packages/playshared/Runtime/UI/MicProgressBarControl.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UniInject;
+
+public class MicProgressBarControl : INeedInjection, IInjectionFinishedListener
+{
+    [Inject(UxmlName = R_PlayShared.UxmlNames.micProgressBar)]
+    private RadialProgressBar micProgressBar;
+
+    [Inject]
+    public MicProfile MicProfile { get; set; }
+
+    public Action<MicProfile> OnProgressBarFilled { get; set; }
+
+    public float ProgressBarValue
+    {
+        get => micProgressBar.value;
+        set
+        {
+            micProgressBar.value = value;
+            micProgressBar.SetVisibleByDisplay(value > 0);
+
+            if (value >= micProgressBar.highValue)
+            {
+                OnProgressBarFilled?.Invoke(MicProfile);
+            }
+        }
+    }
+
+    public void OnInjectionFinished()
+    {
+        micProgressBar.HideByDisplay();
+        micProgressBar.progressColor = MicProfile.Color;
+    }
+}
