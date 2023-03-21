567f9252cb59e79a5b6055ea8cdc80a389032d02
Andreas Stange <anst.dev@gmail.com>
2023-03-21 18:07:30 +0100

    vfx: foreground and background effects
    - use effect in sing scene and singing results

---
+++ UltraStar Play/Assets/Common/Vfx/EParticleEffect.cs
@@ -0,0 +1,10 @@
+﻿public enum EParticleEffect
+{
+    HitARed = 1,
+    HitDYellow = 2,
+    FireworkHdrShootSingle = 4,
+    FireworkCyanPurple = 5,
+    LightGlowALoop = 6,
+    ShinyItemLoop = 7,
+    MagicPoof = 8,
+}
