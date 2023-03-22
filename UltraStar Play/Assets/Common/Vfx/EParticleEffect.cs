a189f32dca68d400c069eafb656aec6d08fa7fea
Andreas Stange <anst.dev@gmail.com>
2023-03-22 15:58:15 +0100

    vfx: new particle systems

--- UltraStar Play/Assets/Common/Vfx/EParticleEffect.cs
+++ UltraStar Play/Assets/Common/Vfx/EParticleEffect.cs
@@ -7,4 +7,7 @@
     LightGlowALoop = 6,
     ShinyItemLoop = 7,
     MagicPoof = 8,
+    StarExplosion_ToonProjectiles_Hit5 = 9,
+    FireworksEffect2D_Firework5_BlueStar = 12,
+    FireworksEffect2D_Firework6_YellowStar = 13,
 }
