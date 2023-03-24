1d92c66a8ee43d3f2e1791200843056d66c9ad8e
Andreas Stange <anst.dev@gmail.com>
2023-03-24 18:17:45 +0100

    singign results: new teams results UI

--- UltraStar Play/Assets/Scenes/SingingResults/DefaultSingingResultsSceneDataProvider.cs
+++ UltraStar Play/Assets/Scenes/SingingResults/DefaultSingingResultsSceneDataProvider.cs
@@ -7,6 +7,7 @@ public class DefaultSingingResultsSceneDataProvider : MonoBehaviour, IDefaultSce
     [Range(1, 16)]
     public int playerCount = 1;
 
+    [Range(0, 8)]
     public int partyModeTeams = 5;
 
     public bool isLastPartyModeRound;
