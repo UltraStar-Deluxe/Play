ab85e8c603d207b423ed53818ed57f54edda6070
Andreas Stange <anst.dev@gmail.com>
2023-03-24 16:46:06 +0100

    vfx: add "Confetti FX Pro"

---
+++ UltraStar Play/Assets/Confetti FX Pro/Demo Scenes/Script/DemoScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoScript : MonoBehaviour {
+
+    public GameObject objOriginal;
+    public GameObject objGlow;
+    public GameObject objOutline;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ToggleOriginal(bool selected)
+    {
+        objOriginal.SetActive(selected);
+        objGlow.SetActive(!selected);
+        objOutline.SetActive(!selected);
+    }
+
+    public void ToggleGlow(bool selected)
+    {
+        objOriginal.SetActive(!selected);
+        objGlow.SetActive(selected);
+        objOutline.SetActive(!selected);
+    }
+
+    public void ToggleOutline(bool selected)
+    {
+        objOriginal.SetActive(!selected);
+        objGlow.SetActive(!selected);
+        objOutline.SetActive(selected);
+    }
+
+}
