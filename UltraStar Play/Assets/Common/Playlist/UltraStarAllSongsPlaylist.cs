6bc5701f60148b454f211eb471d89474a21fb1cb
Andreas Stange <anst.dev@gmail.com>
2023-03-27 10:15:40 +0200

    song select: fix playlist chooser

--- UltraStar Play/Assets/Common/Playlist/UltraStarAllSongsPlaylist.cs
+++ UltraStar Play/Assets/Common/Playlist/UltraStarAllSongsPlaylist.cs
@@ -1,9 +1,14 @@
-﻿// Special playlist that includes all songs
+﻿using ProTrans;
+
+/**
+ * Special playlist that includes all songs
+ */
 public class UltraStarAllSongsPlaylist : UltraStarPlaylist
 {
     public static UltraStarAllSongsPlaylist Instance { get; private set; } = new();
     public override bool IsEmpty => SongMetaManager.Instance.GetSongMetas().IsNullOrEmpty();
-
+    public override string Name => TranslationManager.GetTranslation(R.Messages.playlistName_allSongs);
+    
     public UltraStarAllSongsPlaylist()
         : base("")
     {
