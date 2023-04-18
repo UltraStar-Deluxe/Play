f8d0febc460169d3364285d72fe24f5e5f56cbb3
Andreas Stange <anst.dev@gmail.com>
2023-04-18 14:11:54 +0200

    reset not-to-be-persisted settings after loading

--- UltraStar Play/Assets/Common/Model/SettingsManager.cs
+++ UltraStar Play/Assets/Common/Model/SettingsManager.cs
@@ -29,7 +29,7 @@ public class SettingsManager : AbstractSingletonBehaviour
         {
             if (settings == null)
             {
-                Reload();
+                LoadSettings();
             }
             return settings;
         }
@@ -76,7 +76,7 @@ public class SettingsManager : AbstractSingletonBehaviour
         clientIdsWithoutPermission.ForEach(clientId => Settings.HttpApiPermissions.Remove(clientId));
     }
 
-    private void Reload()
+    private void LoadSettings()
     {
         using (new DisposableStopwatch("Loading the settings took <millis> ms"))
         {
@@ -103,7 +103,18 @@ public class SettingsManager : AbstractSingletonBehaviour
                 settings = CreateDefaultSettings();
             }
             OverwriteSettingsWithCommandLineArguments();
+
+            // TODO: Store non-persistent settings in dedicated data structure.
+            ResetNonPersistentSettings();
         }
+    }
+
+    private void ResetNonPersistentSettings()
+    {
+        settings.SongSelectSettings.playlistName = "";
+        settings.SongSelectSettings.micTestActive = false;
+        settings.activeSearchPropertyFilters = new();
+        settings.isShowOnlyDuetsFilterActive = false;
     }
 
     private Settings CreateDefaultSettings()
