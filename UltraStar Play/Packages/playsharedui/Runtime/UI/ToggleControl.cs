5cb2fe0e76a0a26059796e74a3a0b14f030090fc
Andreas Stange <anst.dev@gmail.com>
2023-04-18 13:18:01 +0200

    VisualElementSlideInControl

---
+++ UltraStar Play/Packages/playsharedui/Runtime/UI/ToggleControl.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+public class ToggleControl
+{
+    private readonly Action onBecomeTrue;
+    private readonly Action onBecomeFalse;
+    
+    private bool state;
+    public bool State
+    {
+        get => state;
+        set
+        {
+            if (state == value)
+            {
+                return;
+            }
+
+            state = value;
+            InvokeState();
+        }
+    }
+
+    public ToggleControl(bool initialState, Action onBecomeTrue, Action onBecomeFalse, bool invokeInitialState = true)
+    {
+        this.onBecomeTrue = onBecomeTrue;
+        this.onBecomeFalse = onBecomeFalse;
+        state = initialState;
+
+        if (invokeInitialState)
+        {
+            InvokeState();
+        }
+    }
+
+    public void ToggleState()
+    {
+        State = !State;
+    }
+
+    private void InvokeState()
+    {
+        if (state)
+        {
+            onBecomeTrue?.Invoke();
+        }
+        else
+        {
+            onBecomeFalse?.Invoke();
+        }
+    }
+}
---
+++ UltraStar Play/Packages/playsharedui/Runtime/UI/VisualElementSlideInControl.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class VisualElementSlideInControl
+{
+    private readonly VisualElement visualElement;
+    private readonly ESide2D side;
+    private readonly ToggleControl toggleControl;
+
+    private bool isInitialized;
+
+    private Vector2 ResolvedStyleSize => new Vector2(visualElement.resolvedStyle.width, visualElement.resolvedStyle.height);
+    private Vector2 lastSize;
+    
+    public bool Visible
+    {
+        get => toggleControl.State;
+        set => toggleControl.State = value;
+    }
+
+    public VisualElementSlideInControl(VisualElement visualElement, ESide2D side, bool initiallyVisible)
+    {
+        this.visualElement = visualElement;
+        this.side = side;
+        toggleControl = new ToggleControl(initiallyVisible, SliderIn, SlideOut);
+
+        this.visualElement.RegisterCallback<GeometryChangedEvent>(evt =>
+        {
+            if (!isInitialized)
+            {
+                isInitialized = true;
+                if (initiallyVisible)
+                {
+                    SliderIn();
+                }
+                else
+                {
+                    SlideOut();
+                }
+
+                lastSize = ResolvedStyleSize;
+            }
+            else if (Math.Abs(lastSize.x - ResolvedStyleSize.x) > 1f
+                     || Math.Abs(lastSize.y - ResolvedStyleSize.y) > 1f)
+            {
+                lastSize = ResolvedStyleSize;
+                UpdatePositionWithoutTransition();
+            }
+        });
+    }
+
+    private void UpdatePositionWithoutTransition()
+    {
+        // No animation is done when the units change. Here, we change from unit "auto" to unit "px".
+        if (side == ESide2D.Right)
+        {
+            visualElement.style.right = new StyleLength(StyleKeyword.Auto);
+        }
+        else if (side == ESide2D.Left)
+        {
+            visualElement.style.left = new StyleLength(StyleKeyword.Auto);
+        }
+        else if (side == ESide2D.Top)
+        {
+            visualElement.style.top = new StyleLength(StyleKeyword.Auto);
+        }
+        else if (side == ESide2D.Bottom)
+        {
+            visualElement.style.bottom = new StyleLength(StyleKeyword.Auto);
+        }
+        
+        if (Visible)
+        {
+            SliderIn();
+        }
+        else
+        {
+            SlideOut();
+        }
+    }
+
+    private void SlideOut()
+    {
+        if (side == ESide2D.Right)
+        {
+            visualElement.style.right = -visualElement.resolvedStyle.width;
+        }
+        else if (side == ESide2D.Left)
+        {
+            visualElement.style.left = -visualElement.resolvedStyle.width;
+        }
+        else if (side == ESide2D.Top)
+        {
+            visualElement.style.top = -visualElement.resolvedStyle.height;
+        }
+        else if (side == ESide2D.Bottom)
+        {
+            visualElement.style.bottom = -visualElement.resolvedStyle.height;
+        }
+    }
+
+    private void SliderIn()
+    {
+        if (side == ESide2D.Right)
+        {
+            visualElement.style.right = 0;
+        }
+        else if (side == ESide2D.Left)
+        {
+            visualElement.style.left = 0;
+        }
+        else if (side == ESide2D.Top)
+        {
+            visualElement.style.top = 0;
+        }
+        else if (side == ESide2D.Bottom)
+        {
+            visualElement.style.bottom = 0;
+        }
+    }
+
+    public void ToggleVisible()
+    {
+        Visible = !Visible;
+    }
+}
