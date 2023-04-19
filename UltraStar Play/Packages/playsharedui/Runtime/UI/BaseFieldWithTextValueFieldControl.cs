9f560fa4585db3d864bc965fa0c5ea37bce1d1d8
Andreas Stange <anst.dev@gmail.com>
2023-04-19 12:19:38 +0200

    utility controls

---
+++ UltraStar Play/Packages/playsharedui/Runtime/UI/BaseFieldWithTextValueFieldControl.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine.UIElements;
+
+public class BaseFieldWithTextValueFieldControl<T>
+{
+    public BaseFieldWithTextValueFieldControl(BaseField<T> baseField, TextValueField<T> textField)
+    {
+        // Set TextField when BaseField changes.
+        baseField.RegisterValueChangedCallback(evt =>
+        {
+            if (!Equals(textField.value, evt.newValue))
+            {
+                textField.value = evt.newValue;
+            }
+        });
+        
+        // Set BaseField when TextField changes.
+        textField.RegisterValueChangedCallback(evt =>
+        {
+            if (!Equals(baseField.value, evt.newValue))
+            {
+                baseField.value = evt.newValue;
+            }
+        });
+    }
+}
---
+++ UltraStar Play/Packages/playsharedui/Runtime/UI/SliderIntStepControl.cs
@@ -0,0 +1,35 @@
+﻿using UniRx;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class SliderIntStepControl
+{
+    private int lastFrameCount = Time.frameCount;
+    
+    public SliderIntStepControl(SliderInt sliderInt, int step)
+    {
+        if (step <= 1)
+        {
+            return;
+        }
+        
+        sliderInt.RegisterValueChangedCallback(evt =>
+        {
+            if (lastFrameCount == Time.frameCount)
+            {
+                return;
+            }
+            lastFrameCount = Time.frameCount;
+            
+            int newValue = evt.newValue;
+            int roundedValue = Mathf.RoundToInt((float)newValue / step) * step;
+            if (roundedValue != newValue)
+            {
+                // Set a new value next frame.
+                // Otherwise the rounded value will be overwritten again with the non-rounded value.
+                MainThreadDispatcher.StartCoroutine(CoroutineUtils.ExecuteAfterDelayInFrames(1,
+                    () => sliderInt.value = roundedValue));
+            }
+        });
+    }
+}
