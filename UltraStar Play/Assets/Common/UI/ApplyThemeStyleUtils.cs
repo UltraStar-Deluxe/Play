857e6b39b325ba657fe0f3f8eedcac46e5f8db5a
Andreas Stange <anst.dev@gmail.com>
2023-05-05 12:54:07 +0200

    fix KeyNotFoundException

--- UltraStar Play/Assets/Common/UI/ApplyThemeStyleUtils.cs
+++ UltraStar Play/Assets/Common/UI/ApplyThemeStyleUtils.cs
@@ -213,7 +213,8 @@ public static class ApplyThemeStyleUtils
         {
             listViewToSelectedVisualElement[listView] = listItemAncestor;
         }
-        else if (listViewToSelectedVisualElement[listView] == listItemAncestor)
+        else if (listViewToSelectedVisualElement.TryGetValue(listView, out VisualElement selectedListItemVisualElement)
+                 && selectedListItemVisualElement == listItemAncestor)
         {
             listViewToSelectedVisualElement[listView] = null;
         }
