5825ceeaab25122070ba722f40646939121b6466
Andreas Stange <anst.dev@gmail.com>
2023-05-11 10:41:17 +0200

    fix: context menu popup added under wrong parent element

--- UltraStar Play/Packages/playsharedui/Runtime/UI/ContextMenu/ContextMenuPopupControl.cs
+++ UltraStar Play/Packages/playsharedui/Runtime/UI/ContextMenu/ContextMenuPopupControl.cs
@@ -59,7 +59,7 @@ public class ContextMenuPopupControl : INeedInjection, IInjectionFinishedListene
         visualElement = contextMenuPopupManager.contextMenuUi.CloneTree().Children().First();
         visualElement.style.left = position.x;
         visualElement.style.top = position.y;
-        uiDocument.rootVisualElement.Children().LastOrDefault().Add(visualElement);
+        uiDocument.rootVisualElement.Add(visualElement);
         // Remove dummy items
         visualElement.Clear();
 
