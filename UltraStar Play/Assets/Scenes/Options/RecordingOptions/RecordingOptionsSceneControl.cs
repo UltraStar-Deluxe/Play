b7b9b5f35cebe86bc1169089a820bae997184817
Andreas Stange <anst.dev@gmail.com>
2023-05-16 20:15:19 +0200

    more fine grained noiseSuppression values

--- UltraStar Play/Assets/Scenes/Options/RecordingOptions/RecordingOptionsSceneControl.cs
+++ UltraStar Play/Assets/Scenes/Options/RecordingOptions/RecordingOptionsSceneControl.cs
@@ -16,7 +16,7 @@ using IBinding = UniInject.IBinding;
 public class RecordingOptionsSceneControl : AbstractOptionsSceneControl, ITranslator, IBinder
 {
     private static readonly List<int> amplificationItems = new() { 0, 3, 6, 9, 12, 15, 18 };
-    private static readonly List<int> noiseSuppressionItems= new() { 0, 5, 10, 15, 20, 25, 30 };
+    private static readonly List<int> noiseSuppressionItems= new() { 0, 1, 3, 5, 10, 15, 20, 25, 30 };
 
     [Inject(SearchMethod = SearchMethods.FindObjectOfType)]
     private RecordingOptionsMicVisualizer micVisualizer;
