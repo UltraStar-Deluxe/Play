70f272c7389ca13a55e0e214b93e6b1298b3da2a
Andreas Stange <anst.dev@gmail.com>
2023-06-05 10:42:36 +0200

    song library: ask before deleting existing song folder

---
+++ UltraStar Play/Packages/playshared/Runtime/Util/CollectionUtils.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+public static class CollectionUtils
+{
+    public static void SafeSet<T>(IList<T> list, T value, int index)
+    {
+        if (list == null
+            || index < 0
+            || index >= list.Count)
+        {
+            return;
+        }
+
+        list[index] = value;
+    }
+
+    public static T SafeGet<T>(IReadOnlyList<T> list, int index, T fallbackValue)
+    {
+        if (list == null
+            || index < 0
+            || index >= list.Count)
+        {
+            return fallbackValue;
+        }
+
+        return list[index];
+    }
+}
