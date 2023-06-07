04617d2127a68751dfb5c496e2ed08f3de15972b
Andreas Stange <anst.dev@gmail.com>
2023-06-07 13:06:07 +0200

    fix: no perfect sentence effect scores are disabled

--- UltraStar Play/Assets/Scenes/Sing/PlayerController/PlayerUi/PlayerUiControl.cs
+++ UltraStar Play/Assets/Scenes/Sing/PlayerController/PlayerUi/PlayerUiControl.cs
@@ -349,6 +349,11 @@ public class PlayerUiControl : INeedInjection, IInjectionFinishedListener
 
     private void CreateMultiplePerfectSentenceEffect()
     {
+        if (settings.ScoreMode is EScoreMode.None)
+        {
+            return;
+        }
+        
         EParticleEffect noteAreaEffect = RandomUtils.RandomOfItems(
             EParticleEffect.FireworksEffect2D_Firework5_BlueStar,
             EParticleEffect.FireworksEffect2D_Firework6_YellowStar);
@@ -362,6 +367,11 @@ public class PlayerUiControl : INeedInjection, IInjectionFinishedListener
 
     private void CreateSinglePerfectSentenceEffect()
     {
+        if (settings.ScoreMode is EScoreMode.None)
+        {
+            return;
+        }
+        
         VfxManager.CreateParticleEffect(new ParticleEffectConfig()
         {
             particleEffect = EParticleEffect.ShinyItemLoop,
