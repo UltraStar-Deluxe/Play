0087c6a5c62e0f59c70ff738a71027671e9ba8a6
Andreas Stange <anst.dev@gmail.com>
2023-06-09 09:56:24 +0200

    fix: context menu: focus previously focused element on close

--- UltraStar Play/Packages/playsharedui/Runtime/UI/ContextMenu/ContextMenuControl.cs
+++ UltraStar Play/Packages/playsharedui/Runtime/UI/ContextMenu/ContextMenuControl.cs
@@ -50,6 +50,8 @@ public class ContextMenuControl : INeedInjection, IInjectionFinishedListener, ID
     private readonly Subject<ContextMenuPopupControl> contextMenuClosedEventStream = new();
     public IObservable<ContextMenuPopupControl> ContextMenuClosedEventStream => contextMenuClosedEventStream;
 
+    private VisualElement focusedVisualElementOnOpen;
+    
     public virtual void OnInjectionFinished()
     {
         panelHelper = new PanelHelper(uiDocument);
@@ -122,13 +124,26 @@ public class ContextMenuControl : INeedInjection, IInjectionFinishedListener, ID
             return;
         }
 
+        focusedVisualElementOnOpen = targetVisualElement.focusController?.focusedElement as VisualElement;
+        
         ContextMenuPopupControl contextMenuPopupControl = new(gameObject, position);
         injector.Inject(contextMenuPopupControl);
         FillContextMenuAction(contextMenuPopupControl);
 
-        contextMenuPopupControl.ContextMenuClosedEventStream.Subscribe(_ => contextMenuClosedEventStream.OnNext(contextMenuPopupControl));
+        contextMenuPopupControl.ContextMenuClosedEventStream.Subscribe(_ => OnContextMenuClose(contextMenuPopupControl));
         contextMenuOpenedEventStream.OnNext(contextMenuPopupControl);
         anyContextMenuOpenedEventStream.OnNext(contextMenuPopupControl);
+    }
+
+    private void OnContextMenuClose(ContextMenuPopupControl contextMenuPopupControl)
+    {
+        contextMenuClosedEventStream.OnNext(contextMenuPopupControl);
+        
+        // Focus last element
+        if (focusedVisualElementOnOpen != null)
+        {
+            focusedVisualElementOnOpen.Focus();
+        }
     }
 
     public void Dispose()
