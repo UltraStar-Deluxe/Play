195297647b050fada975df89525c11aaf86b63cd
Andreas Stange <anst.dev@gmail.com>
2023-06-09 15:36:27 +0200

    add demo song package to newly created settings

--- UltraStar Play/Packages/playshared/Runtime/Util/ApplicationUtils.cs
+++ UltraStar Play/Packages/playshared/Runtime/Util/ApplicationUtils.cs
@@ -228,6 +228,11 @@ public static class ApplicationUtils
     {
         return Application.persistentDataPath + $"/{GeneratedFolderName}/Songs";
     }
+    
+    public static string GetDemoSongFolderAbsolutePath()
+    {
+        return GetStreamingAssetsPath("DemoSongs");
+    }
 
     public static bool IsGeneratedAudioFile(string audioFile)
     {
