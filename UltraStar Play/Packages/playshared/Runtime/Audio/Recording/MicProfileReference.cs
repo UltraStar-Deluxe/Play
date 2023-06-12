6aa413e8f638d5483a1b96a618cfa230cf0bbf8e
Andreas Stange <anst.dev@gmail.com>
2023-06-12 11:46:11 +0200

    MicProfileReference: implement equals and hashCode

--- UltraStar Play/Packages/playshared/Runtime/Audio/Recording/MicProfileReference.cs
+++ UltraStar Play/Packages/playshared/Runtime/Audio/Recording/MicProfileReference.cs
@@ -1,16 +1,61 @@
-﻿public class MicProfileReference
+﻿using System;
+
+public class MicProfileReference : IEquatable<MicProfileReference>
 {
-    public string Name { get; private set; }
-    public int ChannelIndex { get; private set; }
+    private readonly string name;
+    public string Name => name;
+    
+    private readonly int channelIndex;
+    public int ChannelIndex => channelIndex;
 
     public MicProfileReference(string name, int channelIndex)
     {
-        Name = name;
-        ChannelIndex = channelIndex;
+        this.name = name;
+        this.channelIndex = channelIndex;
     }
     
     public MicProfileReference(MicProfile micProfile)
         : this(micProfile.Name, micProfile.ChannelIndex)
     {
+    }
+
+    public bool Equals(MicProfileReference other)
+    {
+        if (ReferenceEquals(null, other))
+        {
+            return false;
+        }
+
+        if (ReferenceEquals(this, other))
+        {
+            return true;
+        }
+
+        return name == other.name && channelIndex == other.channelIndex;
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (ReferenceEquals(null, obj))
+        {
+            return false;
+        }
+
+        if (ReferenceEquals(this, obj))
+        {
+            return true;
+        }
+
+        if (obj.GetType() != this.GetType())
+        {
+            return false;
+        }
+
+        return Equals((MicProfileReference)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(name, channelIndex);
     }
 }
