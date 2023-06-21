69dcea8f8a2a35edae752a0aefaee320b722b92f
Andreas Stange <anst.dev@gmail.com>
2023-06-21 22:17:04 +0200

    speech recognition: ignore special segments that do not contain lyrics

--- UltraStar Play/Assets/Common/Audio/SpeechRecognition/SpeechRecognizer.cs
+++ UltraStar Play/Assets/Common/Audio/SpeechRecognition/SpeechRecognizer.cs
@@ -43,27 +43,33 @@ public class SpeechRecognizer
 
         cancellationToken.ThrowIfCancellationRequested();
         
-        SpeechRecognitionResult speechRecognitionResult;
         if (whisperResult != null
-            && !whisperResult.Segments.IsNullOrEmpty()
-            && !whisperResult.Result.StartsWith("[BLANK_AUDIO]", StringComparison.InvariantCultureIgnoreCase)
-            && !whisperResult.Result.StartsWith("[Music]", StringComparison.InvariantCultureIgnoreCase))
+            && !whisperResult.Segments.IsNullOrEmpty())
         {
             TimeSpan offsetToStartIndex = TimeSpan.FromSeconds((double)startIndex / sampleRate);
             string textResult = whisperResult.Result;
             List<SpeechRecognitionWordResult> wordResults = whisperResult.Segments
-                .SelectMany(whisperSegment => whisperSegment.Tokens)
-                .Where(token => !token.IsSpecial && !token.Text.TrimStart().StartsWith("[") && !token.Text.TrimEnd().EndsWith("]"))
+                // Whisper outputs special segments such as [Music], [BLANK_AUDIO], [NOISE], etc. that are irrelevant for the lyrics.
+                .Where(segment => !segment.Text.TrimStart().StartsWith("[")
+                                  && !segment.Text.TrimEnd().EndsWith("]"))
+                .SelectMany(segment => segment.Tokens)
+                .Where(token => !token.IsSpecial
+                                && !token.Text.TrimStart().StartsWith("[")
+                                && !token.Text.TrimEnd().EndsWith("]"))
                 .Select(token => new SpeechRecognitionWordResult(
                     token.Text,
                     token.Timestamp.Start + offsetToStartIndex,
-                    token.Timestamp.End + offsetToStartIndex))
+                    token.Timestamp.End + offsetToStartIndex,
+                    token.Prob))
                 .ToList();
-            SpeechRecognitionWordResult.NormalizeText(wordResults);
-            wordResults = wordResults
-                .Where(wordResult => !StringUtils.IsOnlyWhitespace(wordResult.Text))
-                .ToList();
-            Debug.Log($"Speech recognition result words: {wordResults.Select(it => $"'{it.Text}'").JoinWith("|")}");
+            if (!wordResults.IsNullOrEmpty())
+            {
+                SpeechRecognitionWordResult.NormalizeText(wordResults);
+                wordResults = wordResults
+                    .Where(wordResult => !StringUtils.IsOnlyWhitespace(wordResult.Text))
+                    .ToList();
+                Debug.Log($"Speech recognition result words: {wordResults.Select(it => $"'{it.Text}'").JoinWith("|")}");
+            }
             return new SpeechRecognitionResult(textResult, wordResults);
         }
 
