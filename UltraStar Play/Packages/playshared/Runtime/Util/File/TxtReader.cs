6bbbb4073f0d9b6ebef733f5d597536a81f74214
Andreas Stange <anst.dev@gmail.com>
2023-08-02 10:18:36 +0200

    fix NPE in TxtReader when no encoding detection result was found

--- UltraStar Play/Packages/playshared/Runtime/Util/File/TxtReader.cs
+++ UltraStar Play/Packages/playshared/Runtime/Util/File/TxtReader.cs
@@ -51,15 +51,23 @@ public static class TxtReader
     private static Encoding GuessUnknownFileEncodingUsingUniversalCharsetDetector(string filePath)
     {
         DetectionResult detectionResult = CharsetDetector.DetectFromFile(filePath);
+        if (detectionResult == null
+            || detectionResult.Detected == null)
+        {
+            Debug.LogWarning($"Could not determine encoding of file '{filePath}', using UTF8 as fallback. No encoding detection result found");
+            return Encoding.UTF8;
+        }
+
         Encoding encoding = detectionResult.Detected.Encoding;
         string encodingName = detectionResult.Detected.EncodingName;
         float confidence = detectionResult.Detected.Confidence;
-        if (encoding != null
-            && confidence > 0.6f)
+        if (encoding == null
+            || confidence < 0.6f)
         {
-            return encoding;
+            Debug.LogWarning($"Could not determine encoding of file '{filePath}' with high confidence, using UTF8 as fallback. Encoding detection result was: encoding name: '{encodingName}', C# object '{encoding}', confidence: {confidence}");
+            return Encoding.UTF8;
         }
-        Debug.LogWarning($"Could not determine encoding of file '{filePath}' with high confidence, using UTF8 as fallback. Encoding detection result was: encoding name: '{encodingName}', C# object '{encoding}', confidence: {confidence}");
-        return Encoding.UTF8;
+
+        return encoding;
     }
 }
