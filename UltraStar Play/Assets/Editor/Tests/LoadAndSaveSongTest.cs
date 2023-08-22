8ea710050dc8bc3b4fab27c56b7cc01115781643
Andreas Stange <anst.dev@gmail.com>
2023-08-22 13:59:27 +0200

    unit test for load and write song file

---
+++ UltraStar Play/Assets/Editor/Tests/LoadAndSaveSongTest.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using UnityEngine;
+
+public class LoadAndSaveSongTest
+{
+    private static readonly string folderPath = $"{Application.dataPath}/Editor/Tests/TestSongs";
+
+    [Test]
+    public void LoadAndSaveSongDoesNotChangeFields()
+    {
+        string originalFilePath = $"{folderPath}/LoadAndSaveProperties-TestSong.txt";
+        SongMeta originalSongMeta = LoadSong(originalFilePath);
+
+        string savedFilePath = $"{Application.temporaryCachePath}/LoadAndSaveProperties-TestSong-Saved.txt";
+        UltraStarSongFileWriter.WriteFile(savedFilePath, originalSongMeta);
+
+        SongMeta savedSongMeta = LoadSong(savedFilePath);
+
+        Assert.AreEqual(originalSongMeta.Artist, savedSongMeta.Artist);
+        Assert.AreEqual(originalSongMeta.Background, savedSongMeta.Background);
+        Assert.AreEqual(originalSongMeta.Bpm, savedSongMeta.Bpm);
+        Assert.AreEqual(originalSongMeta.Cover, savedSongMeta.Cover);
+        Assert.AreEqual(originalSongMeta.Edition, savedSongMeta.Edition);
+        Assert.AreEqual(originalSongMeta.End, savedSongMeta.End);
+        Assert.AreEqual(originalSongMeta.Gap, savedSongMeta.Gap);
+        Assert.AreEqual(originalSongMeta.Genre, savedSongMeta.Genre);
+        Assert.AreEqual(originalSongMeta.InstrumentalAudio, savedSongMeta.InstrumentalAudio);
+        Assert.AreEqual(originalSongMeta.Language, savedSongMeta.Language);
+        Assert.AreEqual(originalSongMeta.Mp3, savedSongMeta.Mp3);
+        Assert.AreEqual(originalSongMeta.MusicBrainzRecord, savedSongMeta.MusicBrainzRecord);
+        Assert.AreEqual(originalSongMeta.PreviewEnd, savedSongMeta.PreviewEnd);
+        Assert.AreEqual(originalSongMeta.MedleyEndBeat, savedSongMeta.MedleyEndBeat);
+        Assert.AreEqual(originalSongMeta.MedleyStartBeat, savedSongMeta.MedleyStartBeat);
+        Assert.AreEqual(originalSongMeta.PreviewStart, savedSongMeta.PreviewStart);
+        Assert.AreEqual(originalSongMeta.Relative, savedSongMeta.Relative);
+        Assert.AreEqual(originalSongMeta.Start, savedSongMeta.Start);
+        Assert.AreEqual(originalSongMeta.Title, savedSongMeta.Title);
+        Assert.AreEqual(originalSongMeta.Video, savedSongMeta.Video);
+        Assert.AreEqual(originalSongMeta.VideoGap, savedSongMeta.VideoGap);
+        Assert.AreEqual(originalSongMeta.VocalsAudio, savedSongMeta.VocalsAudio);
+        Assert.AreEqual(originalSongMeta.Year, savedSongMeta.Year);
+
+        Assert.AreEqual("First Vocals", originalSongMeta.VoiceNames["P1"]);
+        Assert.AreEqual("Second Vocals", originalSongMeta.VoiceNames["P2"]);
+        Assert.IsTrue(originalSongMeta.VoiceNames.SequenceEqual(savedSongMeta.VoiceNames), "VoiceNames not equal");
+
+        Assert.AreEqual("42,5", originalSongMeta.GetUnknownHeaderEntry("NUMBERWITHCOMMA"));
+        Assert.AreEqual("43.2", originalSongMeta.GetUnknownHeaderEntry("NUMBERWITHDOT"));
+        Assert.AreEqual("SomeOtherValue", originalSongMeta.GetUnknownHeaderEntry("UNSUPPORTEDFIELD"));
+        Assert.IsTrue(originalSongMeta.UnknownHeaderEntries.SequenceEqual(savedSongMeta.UnknownHeaderEntries), "UnknownHeaderEntries not equal");
+    }
+
+    private SongMeta LoadSong(string path)
+    {
+        return SongMetaBuilder.ParseFile(path, out List<SongIssue> songIssues, null, true);
+    }
+}
