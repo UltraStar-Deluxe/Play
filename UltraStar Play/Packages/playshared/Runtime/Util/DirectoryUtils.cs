b71fe6be876ffffce234698abd454a562aa00f72
Andreas Stange <anst.dev@gmail.com>
2023-09-11 08:32:32 +0200

    copy directory with include and exclude for paths

--- UltraStar Play/Packages/playshared/Runtime/Util/DirectoryUtils.cs
+++ UltraStar Play/Packages/playshared/Runtime/Util/DirectoryUtils.cs
@@ -51,7 +51,11 @@ public static class DirectoryUtils
         Directory.Delete(path, recusive);
     }
 
-    public static void CopyAll(string sourceDirectory, string targetDirectory, LogEventLevel logEventLevel = LogEventLevel.Verbose)
+    public static void CopyAll(
+        string sourceDirectory,
+        string targetDirectory,
+        CopyDirectoryFilter filter = null,
+        LogEventLevel logEventLevel = LogEventLevel.Verbose)
     {
         if (sourceDirectory.IsNullOrEmpty()
             || targetDirectory.IsNullOrEmpty())
@@ -64,10 +68,14 @@ public static class DirectoryUtils
         DirectoryInfo diSource = new DirectoryInfo(sourceDirectory);
         DirectoryInfo diTarget = new DirectoryInfo(targetDirectory);
 
-        CopyAll(diSource, diTarget, logEventLevel);
+        CopyAll(diSource, diTarget, filter, logEventLevel);
     }
 
-    public static void CopyAll(DirectoryInfo source, DirectoryInfo target, LogEventLevel logEventLevel = LogEventLevel.Verbose)
+    public static void CopyAll(
+        DirectoryInfo source,
+        DirectoryInfo target,
+        CopyDirectoryFilter filter = null,
+        LogEventLevel logEventLevel = LogEventLevel.Verbose)
     {
         // https://stackoverflow.com/questions/58744/copy-the-entire-contents-of-a-directory-in-c-sharp
         if (source == null
@@ -75,6 +83,7 @@ public static class DirectoryUtils
         {
             return;
         }
+
         Log.WithLevel(logEventLevel, () => $"Copying folder '{source}' to '{target}'");
 
         Directory.CreateDirectory(target.FullName);
@@ -84,15 +93,51 @@ public static class DirectoryUtils
         {
             string sourceFileName = fi.FullName;
             string targetFileName = Path.Combine(target.FullName, fi.Name);
-            Log.WithLevel(logEventLevel, () => $"Copying '{sourceFileName}' to '{targetFileName}'");
-            fi.CopyTo(targetFileName, true);
+
+            if (filter != null
+                && filter.IsExcluded != null
+                && filter.IsExcluded(sourceFileName))
+            {
+                Log.WithLevel(logEventLevel, () => $"Ignoring '{sourceFileName}'");
+                continue;
+            }
+
+            if (filter == null
+                || filter.IsIncluded == null
+                || filter.IsIncluded(sourceFileName))
+            {
+                Log.WithLevel(logEventLevel, () => $"Copying '{sourceFileName}' to '{targetFileName}'");
+                fi.CopyTo(targetFileName, true);
+            }
+            else
+            {
+                Log.WithLevel(logEventLevel, () => $"Ignoring '{sourceFileName}'");
+            }
         }
 
         // Copy each subdirectory using recursion.
-        foreach (DirectoryInfo diSourceSubDir in source.GetDirectories())
+        foreach (DirectoryInfo sourceSubDir in source.GetDirectories())
         {
-            DirectoryInfo nextTargetSubDir = target.CreateSubdirectory(diSourceSubDir.Name);
-            CopyAll(diSourceSubDir, nextTargetSubDir);
+            string sourceSubDirectoryPath = sourceSubDir.FullName;
+            if (filter != null
+                && filter.IsExcluded != null
+                && filter.IsExcluded(sourceSubDirectoryPath))
+            {
+                Log.WithLevel(logEventLevel, () => $"Ignoring '{sourceSubDirectoryPath}'");
+                continue;
+            }
+
+            if (filter == null
+                || filter.IsIncluded == null
+                || filter.IsIncluded(sourceSubDirectoryPath))
+            {
+                DirectoryInfo nextTargetSubDir = target.CreateSubdirectory(sourceSubDir.Name);
+                CopyAll(sourceSubDir, nextTargetSubDir, filter, logEventLevel);
+            }
+            else
+            {
+                Log.WithLevel(logEventLevel, () => $"Ignoring '{sourceSubDirectoryPath}'");
+            }
         }
     }
 }
