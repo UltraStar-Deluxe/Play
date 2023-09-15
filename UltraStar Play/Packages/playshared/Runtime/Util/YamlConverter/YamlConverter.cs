440f3434936975893beb39a3b3a566643817c0ab
Andreas Stange <anst.dev@gmail.com>
2023-09-15 14:19:53 +0200

    add YamlConverter to parse yml files

---
+++ UltraStar Play/Packages/playshared/Runtime/Util/YamlConverter/YamlConverter.cs
@@ -0,0 +1,16 @@
+﻿using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+public static class YamlConverter
+{
+    public static T FromYaml<T>(string text)
+    {
+        IDeserializer deserializer = new DeserializerBuilder()
+            .WithNamingConvention(CamelCaseNamingConvention.Instance)
+            .IgnoreUnmatchedProperties()
+            .Build();
+
+        T instance = deserializer.Deserialize<T>(text);
+        return instance;
+    }
+}
