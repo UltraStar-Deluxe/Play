78e9eec549044ebe0e38b54615f3960398213be7
Andreas Stange <anst.dev@gmail.com>
2023-09-18 11:58:45 +0200

    add SQLite as Unity package

---
+++ UltraStar Play/Assets/Common/Database/DbConnectionExtensions.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UniInject;
+using UniRx;
+
+// Disable warning about fields that are never assigned, their values are injected.
+#pragma warning disable CS0649
+
+public static class DbConnectionExtensions
+{
+    public static int ExecuteNonQuery(this IDbConnection dbConnection, string command)
+    {
+        IDbCommand dbCommand = dbConnection.CreateCommand();
+        dbCommand.CommandText = command;
+        return dbCommand.ExecuteNonQuery();
+    }
+
+    public static IDataReader ExecuteQuery(this IDbConnection dbConnection, string query)
+    {
+        IDbCommand dbCommand = dbConnection.CreateCommand();
+        dbCommand.CommandText = query;
+        IDataReader reader = dbCommand.ExecuteReader();
+        return reader;
+    }
+
+    public static List<Dictionary<string, object>> ToDictionaryList(this IDataReader reader)
+    {
+        List<Dictionary<string, object>> result = new();
+        while (reader.Read())
+        {
+            Dictionary<string,object> dictionary = Enumerable.Range(0, reader.FieldCount)
+                .ToDictionary(reader.GetName, reader.GetValue);
+            result.Add(dictionary);
+        }
+        return result;
+    }
+}
---
+++ UltraStar Play/Assets/Editor/Tests/SQLiteUtilsTest.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace Editor.Tests
+{
+    public class SQLiteUtilsTest
+    {
+        [Test]
+        public void CreateDatabaseInsertAndRead()
+        {
+            string dbPath = $"{Application.persistentDataPath}/SQLiteTestDatabase.db";
+
+            // Delete old database
+            FileUtils.Delete(dbPath);
+
+            // Open connection, auto close via using statement
+            using IDbConnection dbConnection = SqliteUtils.OpenSqliteConnectionToFile(dbPath);
+
+            // Insert data
+            dbConnection.ExecuteNonQuery("CREATE TABLE IF NOT EXISTS my_table (id INTEGER PRIMARY KEY, name STRING, age INTEGER)");
+            dbConnection.ExecuteNonQuery("INSERT INTO my_table (id, name, age) VALUES (1, 'Alice', 42)");
+            dbConnection.ExecuteNonQuery("INSERT INTO my_table (id, name, age) VALUES (2, 'Bob', 33)");
+
+            // Read data
+            IDataReader dataReader = dbConnection.ExecuteQuery("SELECT * FROM my_table");
+            List<Dictionary<string, object>> dictionaryList = dataReader.ToDictionaryList();
+
+            // Assert data is as expected
+            if (dictionaryList.Count != 2)
+            {
+                Assert.Fail("Unexpected number of rows returned");
+            }
+            Dictionary<string, object> firstRecord = dictionaryList[0];
+            Assert.AreEqual(1, firstRecord["id"]);
+            Assert.AreEqual("Alice", firstRecord["name"]);
+            Assert.AreEqual(42, firstRecord["age"]);
+
+            Dictionary<string, object> secondRecord = dictionaryList[1];
+            Assert.AreEqual(2, secondRecord["id"]);
+            Assert.AreEqual("Bob", secondRecord["name"]);
+            Assert.AreEqual(33, secondRecord["age"]);
+        }
+    }
+}
