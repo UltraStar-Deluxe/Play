51a63678d08e7ce8152479d194f0bc0f131651c7
Andreas Stange <anst.dev@gmail.com>
2023-09-18 10:00:24 +0200

    mods: use ObservableUtils.RunOnNewTaskAsObservable only for top level method, otherwise async Tasks

--- UltraStar Play/Assets/StreamingAssets/Mods/CoverAndBackgroundImageFromMusicBrainz/SongCoverAndBackgroundImageProviderFromMusicbrainz.cs
+++ UltraStar Play/Assets/StreamingAssets/Mods/CoverAndBackgroundImageFromMusicBrainz/SongCoverAndBackgroundImageProviderFromMusicbrainz.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Xml.Linq;
 using Flurl.Http;
 using UniRx;
@@ -9,125 +10,113 @@ using UnityEngine.Networking;
 
 public class SongCoverImageFromMusicBrainzProvider : ISongCoverImageProvider, ISongBackgroundImageProvider
 {
-    private Dictionary<SongMeta, string> songMetaToCoverUri = new Dictionary<SongMeta, string>();
+    private Dictionary<SongMeta, string> songMetaToCoverUriCache = new Dictionary<SongMeta, string>();
 
     public IObservable<string> GetCoverImageUri(SongMeta songMeta)
     {
-        if (songMetaToCoverUri.TryGetValue(songMeta, out string cachedUri))
+        if (songMetaToCoverUriCache.TryGetValue(songMeta, out string cachedCoverUri))
         {
-            if (cachedUri.IsNullOrEmpty())
+            return Observable.Return<string>(cachedCoverUri);
+        }
+
+        return ObservableUtils.RunOnNewTaskAsObservable(async () =>
             {
-                return Observable.Empty<string>();
-            }
-            else
+                string coverUri;
+                try
+                {
+                    string releaseId = await GetMusicBrainzReleaseBySongArtistAndTitleAsync(songMeta.Artist, songMeta.Title);
+                    coverUri = await GetMusicBrainzCoverUriAsync(releaseId);
+                    Debug.Log($"Found cover URI for '{songMeta.Artist} - {songMeta.Title}': {coverUri}");
+                }
+                catch (Exception ex)
+                {
+                    Debug.LogException(ex);
+                    coverUri = "";
+                }
+
+                songMetaToCoverUriCache[songMeta] = coverUri;
+                return coverUri;
+            },
+            Disposable.Empty);
+    }
+
+    private async Task<string> GetMusicBrainzCoverUriAsync(string releaseId)
+    {
+        string escapedReleaseId = UnityWebRequest.EscapeURL(releaseId);
+        string uri = $"https://coverartarchive.org/release/{escapedReleaseId}";
+
+        CoverArtArchiveReleaseResponseJson json = await uri
+            .WithHeader("User-Agent", "Some User Agent")
+            .GetJsonAsync<CoverArtArchiveReleaseResponseJson>();
+
+        if (json != null)
+        {
+            CoverArtArchiveReleaseImageJson imageJson = json.images
+                .FirstOrDefault(it => it.front && !it.thumbnails.IsNullOrEmpty());
+
+            if (imageJson != null)
             {
-                return Observable.Return<string>(cachedUri);
+                // Prefer a small thumbnail image
+                string coverUri;
+                bool success = imageJson.thumbnails.TryGetValue("small", out coverUri);
+                
+                if (!success)
+                {
+                    success = imageJson.thumbnails.TryGetValue("large", out coverUri);
+                }
+
+                if (!success)
+                {
+                    // Elements are sorted from big to small. Take the smallest.
+                    coverUri = imageJson.thumbnails.Values.LastOrDefault();
+                }
+
+                if (!coverUri.IsNullOrEmpty()
+                    && (coverUri.StartsWith("http://")
+                        || coverUri.StartsWith("https://")))
+                {
+                    return coverUri;
+                }
             }
         }
 
-        return GetMusicBrainzReleaseBySongArtistAndTitle(songMeta.Artist, songMeta.Title)
-            .SelectMany(releaseId => GetMusicBrainzCoverUri(releaseId))
-            .SelectMany(coverUri => 
-            {
-                Debug.Log($"Found cover URI for '{songMeta.Artist} - {songMeta.Title}': {coverUri}");
-                
-                // Cache value for following calls
-                songMetaToCoverUri[songMeta] = coverUri;
-
-                return Observable.Return<string>(coverUri);
-            })
-            .CatchIgnore((Exception ex) =>
-            {
-                Debug.LogException(ex);
-                songMetaToCoverUri[songMeta] = "";
-            });
+        throw new Exception($"No front album art found for MusicBrainz release {releaseId}");
     }
 
-    private IObservable<string> GetMusicBrainzCoverUri(string releaseId)
-    {
-        return ObservableUtils.RunOnNewTaskAsObservable(async () =>
-        {
-            string escapedReleaseId = UnityWebRequest.EscapeURL(releaseId);
-            string uri = $"https://coverartarchive.org/release/{escapedReleaseId}";
-
-            CoverArtArchiveReleaseResponseJson json = await uri
-                .WithHeader("User-Agent", "Some User Agent")
-                .GetJsonAsync<CoverArtArchiveReleaseResponseJson>();
-
-            if (json != null)
-            {
-                CoverArtArchiveReleaseImageJson imageJson = json.images
-                    .FirstOrDefault(it => it.front && !it.thumbnails.IsNullOrEmpty());
-
-                if (imageJson != null)
-                {
-                    // Prefer a small thumbnail image
-                    string coverUri;
-                    bool success = imageJson.thumbnails.TryGetValue("small", out coverUri);
-                    
-                    if (!success)
-                    {
-                        success = imageJson.thumbnails.TryGetValue("large", out coverUri);
-                    }
-
-                    if (!success)
-                    {
-                        // Elements are sorted from big to small. Take the smallest.
-                        coverUri = imageJson.thumbnails.Values.LastOrDefault();
-                    }
-
-                    if (!coverUri.IsNullOrEmpty()
-                        && (coverUri.StartsWith("http://")
-                            || coverUri.StartsWith("https://")))
-                    {
-                        return coverUri;
-                    }
-                }
-            }
-
-            throw new Exception($"No front album art found for MusicBrainz release {releaseId}");
-        },
-        Disposable.Empty);
-    }
-
-    private IObservable<string> GetMusicBrainzReleaseBySongArtistAndTitle(
+    private async Task<string> GetMusicBrainzReleaseBySongArtistAndTitleAsync(
         string artist,
         string title)
     {
-        return ObservableUtils.RunOnNewTaskAsObservable(async () =>
+        string escapedTitle = UnityWebRequest.EscapeURL($"{title}");
+        string uri = $"https://musicbrainz.org/ws/2/recording?query={escapedTitle}";
+
+        Debug.Log($"Searching MusicBrainz release for '{artist} - {title}'. URI: '{uri}'");
+        string xml = await uri
+            .WithHeader("User-Agent", "Some User Agent")
+            .GetStringAsync();
+
+        Debug.Log($"MusicBrainz response: {xml}");
+
+        // Select first release in the result
+        XDocument xdocument = XDocument.Parse(xml);
+        XElement xrelease = xdocument
+            .Descendants()
+            .FirstOrDefault(xelement => xelement.Name.LocalName == "release");
+
+        Debug.Log($"MusicBrainz response xrelease: {xrelease}");
+        if (xrelease != null)
         {
-            string escapedTitle = UnityWebRequest.EscapeURL($"{title}");
-            string uri = $"https://musicbrainz.org/ws/2/recording?query={escapedTitle}";
+            // Grab the id of the release
+            string releaseId = xrelease.Attribute("id")?.Value;
+            Debug.Log($"MusicBrainz response xrelease id: {releaseId}");
 
-            Debug.Log($"Searching MusicBrainz release for '{artist} - {title}'. URI: '{uri}'");
-            string xml = await uri
-                .WithHeader("User-Agent", "Some User Agent")
-                .GetStringAsync();
-
-            Debug.Log($"MusicBrainz response: {xml}");
-
-            // Select first release in the result
-            XDocument xdocument = XDocument.Parse(xml);
-            XElement xrelease = xdocument
-                .Descendants()
-                .FirstOrDefault(xelmeent => xelmeent.Name.LocalName == "release");
-
-            Debug.Log($"MusicBrainz response xrelease: {xrelease}");
-            if (xrelease != null)
+            if (!releaseId.IsNullOrEmpty())
             {
-                // Grab the id of the release
-                string releaseId = xrelease.Attribute("id")?.Value;
-                Debug.Log($"MusicBrainz response xrelease id: {releaseId}");
-
-                if (!releaseId.IsNullOrEmpty())
-                {
-                    return releaseId;
-                }
+                return releaseId;
             }
-            
-            throw new Exception($"No MusicBrainz release found for artist '{artist}' and title '{title}'");
-        },
-        Disposable.Empty);
+        }
+        
+        throw new Exception($"No MusicBrainz release found for artist '{artist}' and title '{title}'");
     }
 
     public IObservable<string> GetBackgroundImageUri(SongMeta songMeta)
