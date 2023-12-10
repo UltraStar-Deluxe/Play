9ccc0469257d1badd85002cf4264980be1398496
Andreas Stange <anst.dev@gmail.com>
2023-12-10 20:06:33 +0100

    input simulation: better handling of mouse position and drag events
    - new endpoints to trigger dedicated left mouse button down and up events for dragging

--- UltraStar Play/Assets/Common/RestApi/InputSimulatorRestControl.cs
+++ UltraStar Play/Assets/Common/RestApi/InputSimulatorRestControl.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Net.Http;
 using UniInject;
 using UniRx;
@@ -17,6 +18,13 @@ public class InputSimulatorRestControl : AbstractRestControl, INeedInjection
     private Mouse virtualMouse;
     private Mouse systemMouse;
 
+    private enum ESimulateButtonDirection
+    {
+        Down,
+        Up,
+        DownFollowedByUp
+    }
+
     protected override object GetInstance()
     {
         return Instance;
@@ -28,8 +36,8 @@ public class InputSimulatorRestControl : AbstractRestControl, INeedInjection
         // Mouse.current can later change to the virtual mouse.
         systemMouse = Mouse.current;
 
-        virtualKeyboard = InputSystem.AddDevice<Keyboard>("Virtual Keyboard");
-        virtualMouse = InputSystem.AddDevice<Mouse>("Virtual Mouse");
+        virtualKeyboard = InputSystem.AddDevice<Keyboard>("Custom Virtual Keyboard");
+        virtualMouse = InputSystem.AddDevice<Mouse>("Custom Virtual Mouse");
 
         RegisterNavigationEndpoint("leftArrowKey",
             "Simulate left arrow key press",
@@ -75,19 +83,31 @@ public class InputSimulatorRestControl : AbstractRestControl, INeedInjection
             () => DecreaseVolume());
 
         RegisterNavigationEndpoint("leftMouseButton",
-            "Simulate left mouse button press",
+            "Simulate left mouse button click, i.e. down followed by up",
             virtualMouse,
             () => virtualMouse.leftButton);
 
         RegisterNavigationEndpoint("rightMouseButton",
-            "Simulate left mouse button press",
+            "Simulate right mouse button click, i.e. down followed by up",
             virtualMouse,
             () => virtualMouse.rightButton);
 
         RegisterNavigationEndpoint("middleMouseButton",
-            "Simulate left mouse button press",
+            "Simulate middle mouse button click, i.e. down followed by up",
             virtualMouse,
             () => virtualMouse.middleButton);
+
+        RegisterNavigationEndpoint("leftMouseButtonDown",
+            "Simulate left mouse button down event",
+            virtualMouse,
+            () => virtualMouse.leftButton,
+            ESimulateButtonDirection.Down);
+
+        RegisterNavigationEndpoint("leftMouseButtonUp",
+            "Simulate left mouse button up event",
+            virtualMouse,
+            () => virtualMouse.leftButton,
+            ESimulateButtonDirection.Up);
 
         RegisterMouseDeltaEndpoint();
         RegisterScrollWheelEndpoint();
@@ -135,17 +155,12 @@ public class InputSimulatorRestControl : AbstractRestControl, INeedInjection
             .SetRequiredPermission(HttpApiPermission.WriteInputSimulation)
             .SetCallbackAndAdd(requestData =>
             {
-                if (systemMouse == null)
-                {
-                    return;
-                }
-
                 Log.Debug(() => $"Received input simulation request '{path}' via URL '{requestData.Context.Request.Url}'");
                 bool hasDeltaX = NumberUtils.TryParseDoubleAnyCulture(requestData.PathParameters["deltaX"], out double deltaX);
                 bool hasDeltaY = NumberUtils.TryParseDoubleAnyCulture(requestData.PathParameters["deltaY"], out double deltaY);
                 if (hasDeltaX && hasDeltaY)
                 {
-                    SimulateCurrentMouseDelta(new Vector2((float)deltaX, (float)deltaY));
+                    SimulateSystemMouseDelta(new Vector2((float)deltaX, (float)deltaY));
                 }
             });
     }
@@ -159,11 +174,6 @@ public class InputSimulatorRestControl : AbstractRestControl, INeedInjection
             .SetRequiredPermission(HttpApiPermission.WriteInputSimulation)
             .SetCallbackAndAdd(requestData =>
             {
-                if (systemMouse == null)
-                {
-                    return;
-                }
-
                 Log.Debug(() => $"Received input simulation request '{path}' via URL '{requestData.Context.Request.Url}'");
                 bool hasDeltaX = NumberUtils.TryParseDoubleAnyCulture(requestData.PathParameters["deltaX"], out double deltaX);
                 bool hasDeltaY = NumberUtils.TryParseDoubleAnyCulture(requestData.PathParameters["deltaY"], out double deltaY);
@@ -187,7 +197,7 @@ public class InputSimulatorRestControl : AbstractRestControl, INeedInjection
         }));
     }
 
-    private void SimulateCurrentMouseDelta(Vector2 delta)
+    private void SimulateSystemMouseDelta(Vector2 delta)
     {
         if (delta == Vector2.zero)
         {
@@ -196,21 +206,29 @@ public class InputSimulatorRestControl : AbstractRestControl, INeedInjection
 
         MainThreadDispatcher.StartCoroutine(CoroutineUtils.ExecuteAfterDelayInFrames(0, () =>
         {
-            if (systemMouse == null)
-            {
-                return;
-            }
-            Vector2 currentMousePosition = systemMouse.position.ReadValue();
-            Vector2 newMousePosition = currentMousePosition + delta;
-            using (StateEvent.From(systemMouse, out InputEventPtr eventPtr))
-            {
-                systemMouse.WarpCursorPosition(newMousePosition);
-                InputSystem.QueueEvent(eventPtr);
-            }
+            DoSimulateMouseDelta(delta, systemMouse);
         }));
     }
 
-    private void RegisterNavigationEndpoint(string inputControlName, string description, InputDevice inputDevice, Func<InputControl> inputControlGetter)
+    private void DoSimulateMouseDelta(Vector2 delta, Mouse mouse)
+    {
+        // Use the mouse position from the legacy input API
+        // because the value returned by Unity's newer InputSystem is not accurate when executed in the Unity editor.
+        Vector2 unityInputMousePosition = Input.mousePosition;
+        Vector2 newMousePosition = unityInputMousePosition + delta;
+        using (StateEvent.From(mouse, out InputEventPtr eventPtr))
+        {
+            mouse.WarpCursorPosition(newMousePosition);
+            InputSystem.QueueEvent(eventPtr);
+        }
+    }
+
+    private void RegisterNavigationEndpoint(
+        string inputControlName,
+        string description,
+        InputDevice inputDevice,
+        Func<InputControl> inputControlGetter,
+        ESimulateButtonDirection simulateButtonDirection = ESimulateButtonDirection.DownFollowedByUp)
     {
         string path = $"api/rest/input/{inputControlName}";
         httpServer.CreateEndpoint(HttpMethod.Post, path)
@@ -221,28 +239,40 @@ public class InputSimulatorRestControl : AbstractRestControl, INeedInjection
             {
                 InputControl inputControl = inputControlGetter();
                 Log.Debug(() => $"Received input simulation request {path}");
-                SimulateButtonClick(inputDevice, inputControl);
+                SimulateButtonClick(inputDevice, inputControl, simulateButtonDirection);
             });
     }
 
-    private void SimulateButtonClick(InputDevice inputDevice, InputControl inputControl)
+    private void SimulateButtonClick(InputDevice inputDevice, InputControl inputControl, ESimulateButtonDirection simulateButtonDirection)
     {
-        Log.Debug(() => $"Triggering button click on input control {inputControl} by setting its value to 1 and afterwards to 0");
-        MainThreadDispatcher.StartCoroutine(CoroutineUtils.ExecuteAfterDelayInFrames(0, () =>
+        Log.Debug(() => $"Triggering button event {simulateButtonDirection} on input control {inputControl}");
+
+        if (simulateButtonDirection
+            is ESimulateButtonDirection.Down
+            or ESimulateButtonDirection.DownFollowedByUp)
         {
-            using (StateEvent.From(inputDevice, out InputEventPtr eventPtr))
+            MainThreadDispatcher.StartCoroutine(CoroutineUtils.ExecuteAfterDelayInFrames(0, () =>
             {
-                inputControl.WriteValueIntoEvent(1f, eventPtr);
-                InputSystem.QueueEvent(eventPtr);
-            }
-        }));
-        MainThreadDispatcher.StartCoroutine(CoroutineUtils.ExecuteAfterDelayInFrames(1, () =>
+                using (StateEvent.From(inputDevice, out InputEventPtr eventPtr))
+                {
+                    inputControl.WriteValueIntoEvent(1f, eventPtr);
+                    InputSystem.QueueEvent(eventPtr);
+                }
+            }));
+        }
+
+        if (simulateButtonDirection
+            is ESimulateButtonDirection.Up
+            or ESimulateButtonDirection.DownFollowedByUp)
         {
-            using (StateEvent.From(inputDevice, out InputEventPtr eventPtr))
+            MainThreadDispatcher.StartCoroutine(CoroutineUtils.ExecuteAfterDelayInFrames(1, () =>
             {
-                inputControl.WriteValueIntoEvent(0f, eventPtr);
-                InputSystem.QueueEvent(eventPtr);
-            }
-        }));
+                using (StateEvent.From(inputDevice, out InputEventPtr eventPtr))
+                {
+                    inputControl.WriteValueIntoEvent(0f, eventPtr);
+                    InputSystem.QueueEvent(eventPtr);
+                }
+            }));
+        }
     }
 }
