abb32309247badebf7571cf019e63ac3de47809f
Andreas Stange <anst.dev@gmail.com>
2024-01-07 16:11:35 +0100

    online multiplayer: ArchUnit test that common does not depend on specific online multiplayer

--- UltraStar Play/Assets/Editor/Tests/ArchUnitTests/ArchUnitTests.cs
+++ UltraStar Play/Assets/Editor/Tests/ArchUnitTests/ArchUnitTests.cs
@@ -1,51 +1,31 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using ArchUnitNET.Domain;
 using ArchUnitNET.Domain.Extensions;
 using ArchUnitNET.Fluent.Conditions;
 using ArchUnitNET.Fluent.Predicates;
-using ArchUnitNET.Loader;
 using ArchUnitNET.NUnit;
 using NUnit.Framework;
 using UnityEngine;
 using static ArchUnitNET.Fluent.ArchRuleDefinition;
-using Assembly = System.Reflection.Assembly;
 
-public class CommonArchUnitTests
+public class ArchUnitTests
 {
-    private readonly List<string> gameSpecificRuntimeAssemblyNames = new List<String>()
+    [Test]
+    public void CommonOnlineMultiplayerDoesNotReferenceSpecificOnlineMultiplayer()
     {
-        "playshared",
-        "playsharedui",
-        "Common",
-        "Scenes",
-        "SongEditorScene",
-    };
+        Architecture architecture = ArchUnitTestUtils.LoadArchitectureByAssemblyNames(ArchUnitTestUtils.gameSpecificRuntimeAssemblyNames);
 
-    public Architecture LoadArchitectureByAssemblyNames(List<string> assemblyNames)
-    {
-        List<Assembly> assemblies = assemblyNames
-            .Select(name => Assembly.Load(name))
-            .ToList();
+        IObjectProvider<IType> commonOnlineMultiplayerTypes = Types().That().ResideInNamespace("CommonOnlineMultiplayer");
+        IObjectProvider<IType> steamOnlineMultiplayerTypes = Types().That().ResideInNamespace("SteamOnlineMultiplayer");
 
-        // Log assemblies that are used in the tests
-        string assemblyCsv = assemblies
-            .Select(assembly => assembly.GetName().Name)
-            .ToCsv();
-        Debug.Log($"Loading {assemblies.Count} assembly as architecture for ArchUnit tests: {assemblyCsv}");
-
-        // Create ArchUnit architecture object from assemblies
-        Architecture architecture = new ArchLoader()
-            .LoadAssemblies(assemblies.ToArray())
-            .Build();
-        return architecture;
+        Classes().That().Are(commonOnlineMultiplayerTypes)
+            .Should().NotDependOnAny(steamOnlineMultiplayerTypes)
+            .Check(architecture);
     }
 
     [Test]
     public void ClassesThatUseInjectAttributeShouldImplementINeedInjection()
     {
-        Architecture architecture = LoadArchitectureByAssemblyNames(gameSpecificRuntimeAssemblyNames);
+        Architecture architecture = ArchUnitTestUtils.LoadArchitectureByAssemblyNames(ArchUnitTestUtils.gameSpecificRuntimeAssemblyNames);
 
         // Classes that make use of Inject attribute
         Classes().That().FollowCustomPredicate(HaveMemberWithAttribute("UniInject.InjectAttribute"))
@@ -58,7 +38,7 @@ public class CommonArchUnitTests
     [Test]
     public void ClassesShouldNotBeEmptyInArchUnitTest()
     {
-        Architecture architecture = LoadArchitectureByAssemblyNames(gameSpecificRuntimeAssemblyNames);
+        Architecture architecture = ArchUnitTestUtils.LoadArchitectureByAssemblyNames(ArchUnitTestUtils.gameSpecificRuntimeAssemblyNames);
 
         IntWrapper conditionExecutionCount = new();
         Classes()
