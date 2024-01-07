577820d56380b4c8bd8e3710b706c5661bc49d4d
Andreas Stange <anst.dev@gmail.com>
2024-01-07 16:42:13 +0100

    remove unused ArchUnit test

--- UltraStar Play/Assets/Editor/Tests/ArchUnitTests/CommonArchUnitTests.cs
+++ UltraStar Play/Assets/Editor/Tests/ArchUnitTests/CommonArchUnitTests.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using ArchUnitNET.Domain;
 using ArchUnitNET.Domain.Extensions;
-using ArchUnitNET.Fluent.Conditions;
 using ArchUnitNET.Fluent.Predicates;
 using ArchUnitNET.Loader;
 using ArchUnitNET.NUnit;
@@ -55,43 +54,10 @@ public class CommonArchUnitTests
         .Check(architecture);
     }
 
-    [Test]
-    public void ClassesShouldNotBeEmptyInArchUnitTest()
-    {
-        Architecture architecture = LoadArchitectureByAssemblyNames(gameSpecificRuntimeAssemblyNames);
-
-        IntWrapper conditionExecutionCount = new();
-        Classes()
-            .Should().FollowCustomCondition(CountConditionExecutions(conditionExecutionCount))
-            .Check(architecture);
-
-        Debug.Log($"Condition execution count: {conditionExecutionCount.value}");
-
-        Assert.IsTrue(conditionExecutionCount.value > 0, "condition execution count is zero. Probably no classes in checked assemblies.");
-    }
-
     private static IPredicate<Class> HaveMemberWithAttribute(string attributeFullName)
     {
         return new SimplePredicate<Class>(
             clazz => clazz.Members.AnyMatch(member => member.HasAttribute(attributeFullName)),
             $"have attribute with name {attributeFullName}");
-    }
-
-    private ICondition<Class> CountConditionExecutions(IntWrapper conditionWasExecutedAtLeastOnce)
-    {
-        // This condition is only executed for its side effect,
-        // i.e. to count the executions of the condition.
-        return new SimpleCondition<Class>(
-            clazz =>
-            {
-                conditionWasExecutedAtLeastOnce.value++;
-                return new ConditionResult(clazz, true);
-            },
-            "count condition executions");
-    }
-
-    private class IntWrapper
-    {
-        public int value;
     }
 }
