a2946dfa675b0699c377c501313e52e30603e5ab
Andreas Stange <anst.dev@gmail.com>
2024-01-27 21:54:51 +0100

    tests: prepare Responsible library to write reactive tests

---
+++ UltraStar Play/Assets/PlayModeTests/AbstractPlayModeTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using Responsible;
+using Responsible.Unity;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+
+public abstract class AbstractPlayModeTest
+{
+    protected virtual string TestSceneName => "CommonTestScene";
+
+    protected TestInstructionExecutor Executor { get; set; }
+    protected InputTestFixture VirtualInput { get; set; }
+
+    [UnitySetUp]
+    public IEnumerator UnitySetUp()
+    {
+        this.Executor = new UnityTestInstructionExecutor();
+        VirtualInput = new InputTestFixture();
+
+        yield return LoadTestScene();
+    }
+
+    private IEnumerator LoadTestScene()
+    {
+        if (TestSceneName.IsNullOrEmpty())
+        {
+            yield break;
+        }
+
+        Debug.Log($"Loading scene {TestSceneName}");
+        SceneManager.LoadScene(TestSceneName, LoadSceneMode.Single);
+        yield return new WaitForEndOfFrame();
+    }
+}
---
+++ UltraStar Play/Assets/PlayModeTests/ResponsibleTestExample.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using Responsible;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.TestTools;
+using static Responsible.Bdd.Keywords;
+using static Responsible.Responsibly;
+
+public class ResponsibleTestExample : AbstractPlayModeTest
+{
+    [UnityTest]
+    public IEnumerator ResponsibleExampleTest() => WaitForCondition(
+        "wait for 4 < 11",
+        () => 4 < 11)
+        .ExpectWithinSeconds(1)
+        .ToYieldInstruction(Executor);
+
+    [UnityTest]
+    public IEnumerator ToggleMuteViaF10() => this.Executor.YieldScenario(
+        Scenario("should toggle mute when pressing F10"),
+        Given("loaded main scene and is not muted", LoadMainSceneAssertNotMuted()),
+        When("pressing F10", PressAndReleaseF10Key()),
+        Then("is muted", AssertMuted())
+    );
+
+    private ITestInstruction<object> LoadMainSceneAssertNotMuted() => Do(
+            "load main scene",
+            () => SceneNavigator.Instance.LoadScene(EScene.MainScene))
+        .ContinueWith(
+            WaitForCondition(
+                    "is not muted",
+                    () => !VolumeControl.Instance.IsMuted)
+                .AndThen(WaitForCondition(
+                    "volume not zero",
+                    () => AudioListener.volume > 0))
+                .ExpectWithinSeconds(1));
+
+    private ITestInstruction<object> PressAndReleaseF10Key() => Do(
+        "press and release F10 key",
+        () => VirtualInput.PressAndRelease(Keyboard.current.f10Key));
+
+    private ITestInstruction<object> AssertMuted() => WaitForCondition(
+        "is muted",
+        () => VolumeControl.Instance.IsMuted
+
+            ).AndThen(WaitForCondition(
+        "volume is zero",
+        () => AudioListener.volume <= 0))
+        .ExpectWithinSeconds(1);
+
+}
