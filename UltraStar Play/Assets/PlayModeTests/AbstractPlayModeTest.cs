ec7b88f9553b55c82ca5e2c4fd8993128870a1eb
Andreas Stange <anst.dev@gmail.com>
2024-02-07 21:09:25 +0100

    tests: util method to ignore failing messages

--- UltraStar Play/Assets/PlayModeTests/AbstractPlayModeTest.cs
+++ UltraStar Play/Assets/PlayModeTests/AbstractPlayModeTest.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using NUnit.Framework;
-using NUnit.Framework.Internal;
 using Responsible.Unity;
 using UnityEngine;
 using UnityEngine.InputSystem;
