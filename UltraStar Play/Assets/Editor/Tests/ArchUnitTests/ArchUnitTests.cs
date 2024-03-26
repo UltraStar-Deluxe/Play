ded202c7c2d156b2a8f9e46f0239ebd8517fc747
Andreas Stange <anst.dev@gmail.com>
2024-03-26 22:11:27 +0100

    translation: ArchUnitR rule to not assign strings to UI labels directly

--- UltraStar Play/Assets/Editor/Tests/ArchUnitTests/ArchUnitTests.cs
+++ UltraStar Play/Assets/Editor/Tests/ArchUnitTests/ArchUnitTests.cs
@@ -1,13 +1,38 @@
+using System;
+using System.Collections.Generic;
 using ArchUnitNET.Domain;
 using ArchUnitNET.Domain.Extensions;
+using ArchUnitNET.Fluent.Conditions;
 using ArchUnitNET.Fluent.Predicates;
 using ArchUnitNET.NUnit;
 using NUnit.Framework;
-using UnityEngine;
 using static ArchUnitNET.Fluent.ArchRuleDefinition;
 
 public class ArchUnitTests
 {
+    /**
+     * Checks that UI label setters (e.g. UnityEngine.UIElements.BaseField.label) are not called directly.
+     * Instead, a custom extension method that takes a translation object as input should be used.
+     */
+    // [Test]
+    // [Ignore("Not all label assignments refactored yet to use a translation object with custom extension method")]
+    // public void UiLabelsAreNotAssignedDirectly()
+    // {
+    //     Architecture architecture = ArchUnitTestUtils.LoadArchitectureByAssemblyNames(new List<string>()
+    //     {
+    //         // "playshared",
+    //         // "playsharedui",
+    //         "Common",
+    //         // "Scenes",
+    //         // "SongEditorScene",
+    //     });
+    //
+    //     // Types().That().Are(typeof(GameRoundModifierDialogEntryControl))
+    //     Types()
+    //         .Should().FollowCustomCondition(NotCallLabelSetter())
+    //         .Check(architecture);
+    // }
+
     [Test]
     public void CommonOnlineMultiplayerDoesNotReferenceSpecificOnlineMultiplayer()
     {
@@ -32,6 +57,20 @@ public class ArchUnitTests
         .Should().ImplementInterface("UniInject.INeedInjection")
         // Run check on Architecture
         .Check(architecture);
+    }
+
+    private static ICondition<IType> NotCallLabelSetter()
+    {
+        return new SimpleCondition<IType>(type =>
+            {
+                return new ConditionResult(type, !type.GetCalledMethods().AnyMatch(IsLabelSetter));
+            },
+            $"not call label setter");
+    }
+
+    private static bool IsLabelSetter(MethodMember methodMember)
+    {
+        return string.Equals(methodMember.Name, "set_label(System.String)", StringComparison.InvariantCultureIgnoreCase);
     }
 
     private static IPredicate<Class> HaveMemberWithAttribute(string attributeFullName)
