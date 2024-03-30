721b8717820087f8f3a94033c7234378504c74f7
Andreas Stange <anst.dev@gmail.com>
2024-03-30 17:59:53 +0100

    translations: cache loaded properties files

--- UltraStar Play/Packages/playshared/Runtime/Translations/ResourcesFolderPropertiesFileProvider.cs
+++ UltraStar Play/Packages/playshared/Runtime/Translations/ResourcesFolderPropertiesFileProvider.cs
@@ -1,4 +1,5 @@
-﻿using System.Globalization;
+﻿using System.Collections.Generic;
+using System.Globalization;
 using ProTrans;
 using UnityEngine;
 
@@ -10,14 +11,26 @@ public class ResourcesFolderPropertiesFileProvider : IPropertiesFileProvider
         TranslationConfig.Singleton.PropertiesFileProvider = new ResourcesFolderPropertiesFileProvider();
     }
 
+    private readonly Dictionary<CultureInfo, PropertiesFile> cultureInfoToPropertiesFile = new();
+
     public PropertiesFile GetPropertiesFile(CultureInfo cultureInfo)
     {
+        CultureInfo cultureInfoOrDefault = cultureInfo ?? TranslationConfig.Singleton.DefaultCultureInfo;
+        if (cultureInfoToPropertiesFile.TryGetValue(cultureInfoOrDefault, out PropertiesFile cachedPropertiesFile))
+        {
+            return cachedPropertiesFile;
+        }
+
         string languageAndRegionSuffix = PropertiesFileParser.GetLanguageAndRegionSuffix(cultureInfo);
         TextAsset textAsset = Resources.Load<TextAsset>($"Translations/messages{languageAndRegionSuffix}");
         if (textAsset == null)
         {
             return null;
         }
-        return PropertiesFileParser.ParseText(textAsset.text, cultureInfo);
+
+        PropertiesFile propertiesFile = PropertiesFileParser.ParseText(textAsset.text, cultureInfo);
+        cultureInfoToPropertiesFile[cultureInfoOrDefault] = propertiesFile;
+
+        return propertiesFile;
     }
 }
