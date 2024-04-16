f3662d7c59860c31f1acd5ca77fc2a1108c8e328
Andreas Stange <anst.dev@gmail.com>
2024-04-16 20:39:02 +0200

    vocals isolation: abort job when file format is not supported

--- UltraStar Play/Assets/Common/Audio/AudioSeparation/AudioSeparationManager.cs
+++ UltraStar Play/Assets/Common/Audio/AudioSeparation/AudioSeparationManager.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading;
@@ -46,6 +45,13 @@ public class AudioSeparationManager : MonoBehaviour, INeedInjection
         Job audioSeparationJob = null)
     {
         ProcessSongMetaAsObservable(songMeta, saveSong, audioSeparationJob)
+            .CatchIgnore((Exception ex) =>
+            {
+                Debug.LogException(ex);
+                string errorMessage = $"Vocals isolation failed: {ex.Message}";
+                Debug.LogError(errorMessage);
+                UiManager.CreateNotification(errorMessage);
+            })
             // Subscribe to trigger the observable
             .Subscribe(evt => Debug.Log($"Successfully separated audio: {evt}"));
     }
@@ -56,14 +62,6 @@ public class AudioSeparationManager : MonoBehaviour, INeedInjection
         Job audioSeparationJob = null)
     {
         string audioUri = SongMetaUtils.GetAudioUri(songMeta);
-        string fileExtension = Path.GetExtension(new Uri(audioUri).LocalPath);
-        if (!ApplicationUtils.IsSupportedVocalsSeparationAudioFormat(fileExtension))
-        {
-            UiManager.CreateNotification($"Vocals isolation not supported for this audio file.\n" +
-                                         $"Requires one of {ApplicationUtils.supportedVocalsSeparationAudioFiles.ToCsv(",", "", "")}");
-            return Observable.Empty<AudioSeparationResult>();
-        }
-
         string generatedSongFolderAbsolutePath = SettingsUtils.GetGeneratedSongFolderAbsolutePath(settings);
 
         // Create job to show in UI
@@ -89,30 +87,39 @@ public class AudioSeparationManager : MonoBehaviour, INeedInjection
             ? $"\"{ApplicationUtils.GetStreamingAssetsPath("SpleeterMsvcExe/Spleeter.exe").Replace("/", "\\")}\""
             : "";
 
-        return DoProcessSongMetaAsObservable(
-                songMeta,
-                generatedSongFolderAbsolutePath,
-                cancellationTokenSource.Token,
-                fallbackAudioSeparationCommand,
-                saveSong)
-            // Execute on Background thread
-            .SubscribeOn(Scheduler.ThreadPool)
-            // Notify on Main thread
-            .ObserveOnMainThread()
-            // Handle Exceptions
-            .CatchIgnore((Exception ex) =>
+        return Observable.Create<bool>(o =>
             {
-                Debug.LogException(ex);
-                Debug.LogError($"Vocals isolation failed: {ex.Message}");
-                audioSeparationJob.SetResult(EJobResult.Error);
-                throw ex;
+                string fileExtension = Path.GetExtension(new Uri(audioUri).LocalPath);
+                if (!ApplicationUtils.IsSupportedVocalsSeparationAudioFormat(fileExtension))
+                {
+                    o.OnError(new Exception(
+                        $"Vocals isolation not supported for this audio file.\n" +
+                        $"Requires one of {ApplicationUtils.supportedVocalsSeparationAudioFiles.ToCsv(",", "", "")}"));
+                }
+                o.OnNext(true);
+                return Disposable.Empty;
             })
+            .SelectMany(_ => DoProcessSongMetaAsObservable(
+                        songMeta,
+                        generatedSongFolderAbsolutePath,
+                        cancellationTokenSource.Token,
+                        fallbackAudioSeparationCommand,
+                        saveSong))
+            .SubscribeOn(Scheduler.ThreadPool)
+            .ObserveOnMainThread()
             .Select(audioSeparationResult =>
             {
                 audioSeparationJob.SetResult(EJobResult.Ok);
 
                 audioSeparationFinishedEventStream.OnNext(new AudioSeparationFinishedEvent(songMeta));
                 return audioSeparationResult;
+            })
+            .CatchIgnore((Exception ex) =>
+            {
+                Debug.LogException(ex);
+                Debug.LogError($"Vocals isolation failed: {ex.Message}");
+                audioSeparationJob.SetResult(EJobResult.Error);
+                throw ex;
             });
     }
 
