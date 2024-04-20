0392c8722286fa4cbd803e261d42e78475949d01
Andreas Stange <anst.dev@gmail.com>
2024-04-20 12:05:01 +0200

    menu item to reload current scene with clear cache

--- UltraStar Play/Assets/Common/Input/GlobalInputControl.cs
+++ UltraStar Play/Assets/Common/Input/GlobalInputControl.cs
@@ -4,9 +4,6 @@ using UniRx;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UIElements;
-#if UNITY_EDITOR
-    using UnityEditor;
-#endif
 
 public class GlobalInputControl : AbstractSingletonBehaviour, INeedInjection
 {
@@ -61,49 +58,6 @@ public class GlobalInputControl : AbstractSingletonBehaviour, INeedInjection
             Debug.Log("Toggle UI visibility");
             uiDocument.rootVisualElement.SetVisibleByDisplay(!uiDocument.rootVisualElement.IsVisibleByDisplay());
         }
-
-#if UNITY_EDITOR
-        UpdateEditorOnlyGlobalShortcuts();
-#endif
-    }
-
-#if UNITY_EDITOR
-    private void UpdateEditorOnlyGlobalShortcuts()
-    {
-        if (!Application.isEditor
-            || Keyboard.current == null)
-        {
-            return;
-        }
-
-        if (InputUtils.IsKeyboardAltPressed()
-            && Keyboard.current.rKey.wasReleasedThisFrame)
-        {
-            RefreshAssetDatabase();
-            ReloadCurrentScene();
-        }
-
-        if (InputUtils.IsKeyboardControlPressed()
-            && Keyboard.current.rKey.wasReleasedThisFrame)
-        {
-            // Refresh assets even at runtime
-            RefreshAssetDatabase();
-        }
-    }
-#endif
-
-    private void RefreshAssetDatabase()
-    {
-#if UNITY_EDITOR
-        AssetDatabase.Refresh();
-#endif
-    }
-
-    private void ReloadCurrentScene()
-    {
-        EScene currentScene = sceneRecipeManager.GetCurrentScene();
-        Debug.Log($"Reloading scene: {currentScene}");
-        sceneNavigator.LoadScene(currentScene);
     }
 
     private void ToggleMuteAudio()
