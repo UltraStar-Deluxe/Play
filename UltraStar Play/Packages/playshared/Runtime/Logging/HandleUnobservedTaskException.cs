490eb141ef464e3b8e65dbc512761bdb6509c62b
Andreas Stange <anst.dev@gmail.com>
2024-05-23 18:22:30 +0200

    log exceptions from Tasks

---
+++ UltraStar Play/Packages/playshared/Runtime/Logging/HandleUnobservedTaskException.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using UnityEngine;
+
+public static class HandleUnobservedTaskException
+{
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+    private static void StaticInit()
+    {
+        if (hasRegisteredUnobservedTaskExceptionHandler)
+        {
+            return;
+        }
+
+        hasRegisteredUnobservedTaskExceptionHandler = true;
+        TaskScheduler.UnobservedTaskException += OnUnobservedTaskException;
+        Debug.Log("Registered UnobservedTaskException callback");
+    }
+
+    private static bool hasRegisteredUnobservedTaskExceptionHandler;
+
+    private static void OnUnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs e)
+    {
+        Log.Exception(() => e.Exception);
+    }
+
+}
