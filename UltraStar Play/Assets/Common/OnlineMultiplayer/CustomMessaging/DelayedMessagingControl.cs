94fb2da6127cea4f850f10cbaa5aeb47d51bcab2
Andreas Stange <anst.dev@gmail.com>
2024-05-23 22:21:55 +0200

    online multiplayer: fix simulated delay

--- UltraStar Play/Assets/Common/OnlineMultiplayer/CustomMessaging/DelayedMessagingControl.cs
+++ UltraStar Play/Assets/Common/OnlineMultiplayer/CustomMessaging/DelayedMessagingControl.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using UniRx;
+using Unity.Collections;
 using Unity.Netcode;
 
 namespace CommonOnlineMultiplayer
@@ -16,7 +17,7 @@ namespace CommonOnlineMultiplayer
             this.messagingControl = messagingControl;
         }
 
-        private void DelayAction(Action action)
+        private void DelayMessage(string messageName, IReadOnlyList<ulong> targetNetcodeClientIds, Action action)
         {
             if (DelayInMillis <= 0)
             {
@@ -26,7 +27,7 @@ namespace CommonOnlineMultiplayer
 
             int sleepTimeInMillis = RandomUtils.Range(1, DelayInMillis);
             float sleepTimeInSeconds = sleepTimeInMillis / 1000f;
-            Log.Verbose(() => $"Delaying message by {sleepTimeInMillis} ms");
+            Log.Verbose(() => $"Delaying message '{messageName}' to Netcode clients {targetNetcodeClientIds.JoinWith(", ")} by {sleepTimeInSeconds:F3} seconds");
             MainThreadDispatcher.StartCoroutine(
             CoroutineUtils.ExecuteAfterDelayInSeconds(sleepTimeInSeconds, action));
         }
@@ -41,16 +42,50 @@ namespace CommonOnlineMultiplayer
             return messagingControl.RegisterNamedMessageHandler(messageName, handleMessage);
         }
 
-        public void SendNamedMessageToClients(string messageName, FastBufferWriter fastBufferWriter,
-            IReadOnlyList<ulong> targetNetcodeClientIds, NetworkDelivery networkDelivery = NetworkDelivery.ReliableSequenced)
-        {
-            DelayAction(() => messagingControl.SendNamedMessageToClients(messageName, fastBufferWriter, targetNetcodeClientIds, networkDelivery));
-        }
-
-        public void SendNamedMessageToClient(string messageName, FastBufferWriter fastBufferWriter, ulong targetNetcodeClientId,
+        public void SendNamedMessageToClients(
+            string messageName,
+            FastBufferWriter fastBufferWriter,
+            IReadOnlyList<ulong> targetNetcodeClientIds,
             NetworkDelivery networkDelivery = NetworkDelivery.ReliableSequenced)
         {
-            DelayAction(() => messagingControl.SendNamedMessageToClient(messageName, fastBufferWriter, targetNetcodeClientId, networkDelivery));
+            if (DelayInMillis <= 0)
+            {
+                messagingControl.SendNamedMessageToClients(messageName, fastBufferWriter, targetNetcodeClientIds, networkDelivery);
+                return;
+            }
+
+            FastBufferWriter fastBufferWriterCopy = CopyPersistent(fastBufferWriter);
+            DelayMessage(messageName, targetNetcodeClientIds,
+                () => messagingControl.SendNamedMessageToClients(messageName, fastBufferWriterCopy, targetNetcodeClientIds, networkDelivery));
+        }
+
+        public void SendNamedMessageToClient(
+            string messageName,
+            FastBufferWriter fastBufferWriter,
+            ulong targetNetcodeClientId,
+            NetworkDelivery networkDelivery = NetworkDelivery.ReliableSequenced)
+        {
+            if (DelayInMillis <= 0)
+            {
+                messagingControl.SendNamedMessageToClient(messageName, fastBufferWriter, targetNetcodeClientId, networkDelivery);
+                return;
+            }
+
+            FastBufferWriter fastBufferWriterCopy = CopyPersistent(fastBufferWriter);
+            DelayMessage(messageName, new List<ulong>() {targetNetcodeClientId},
+                () => messagingControl.SendNamedMessageToClient(messageName, fastBufferWriterCopy, targetNetcodeClientId, networkDelivery));
+        }
+
+        /**
+         * The original FastBufferWriter will be disposed by calling code before sending the request with delay.
+         * Thus, this method creates a copy that can be disposed later.
+         */
+        private FastBufferWriter CopyPersistent(FastBufferWriter original)
+        {
+            FastBufferWriter copy = new FastBufferWriter(original.Length, Allocator.Persistent);
+            copy.TryBeginWrite(original.Length);
+            copy.CopyFrom(original);
+            return copy;
         }
     }
 }
