590b2688c3ff43bc36b1c218056ff0fcc1f4ea99
Andreas Stange <anst.dev@gmail.com>
2024-06-16 17:26:02 +0200

    dummy implementation for Vuplex.WebView

---
+++ UltraStar Play/Assets/Vuplex/WebView/VuplexWebViewDummy/CanvasWebViewPrefab.cs
@@ -0,0 +1,21 @@
+﻿#if HAS_VUPLEX_WEBVIEW
+#else
+
+using System;
+using UnityEngine;
+
+namespace Vuplex.WebView
+{
+    public class CanvasWebViewPrefab : MonoBehaviour
+    {
+        public event EventHandler<EventArgs> Initialized;
+        public bool HoveringEnabled { get; set; }
+        public bool ClickingEnabled { get; set; }
+        public bool ScrollingEnabled { get; set; }
+        public bool KeyboardEnabled { get; set; }
+        public bool CursorIconsEnabled { get; set; }
+        public IWebView WebView { get; set; }
+    }
+}
+
+#endif
