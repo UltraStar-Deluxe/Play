80a768f60653a34e9513d850e1b2ef1b33f60dff
Andreas Stange <anst.dev@gmail.com>
2024-06-16 17:17:07 +0200

    dummy implementation for FfmpegUnity

---
+++ UltraStar Play/Assets/FfmpegUnity/FfmpegUnityDummy/FfmpegCommand.cs
@@ -0,0 +1,30 @@
+﻿#if HAS_FFMPEG_UNITY
+#else
+
+using System;
+using UnityEngine;
+
+namespace FfmpegUnity
+{
+    public class FfmpegCommand : MonoBehaviour
+    {
+        public string Options { get; set; }
+        public bool IsRunning { get; set; }
+        public bool IsFinished { get; set; }
+        public TimeSpan DurationTime { get; set; }
+        public double Progress { get; set; }
+        public bool ExecuteOnStart { get; set; }
+        public bool GetProgressOnScript { get; set; }
+        public bool PrintStdErr { get; set; }
+
+        public void StartFfmpeg()
+        {
+        }
+
+        public void StopFfmpeg()
+        {
+        }
+    }
+}
+
+#endif
