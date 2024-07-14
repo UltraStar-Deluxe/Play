cc4d797fe4bb856bf312dda81fd184d814a68ef0
Andreas Stange <anst.dev@gmail.com>
2024-07-14 11:50:57 +0200

    SongMeta: fix quotes in ToString

--- UltraStar Play/Packages/playshared/Runtime/Songs/SongMeta.cs
+++ UltraStar Play/Packages/playshared/Runtime/Songs/SongMeta.cs
@@ -280,7 +280,7 @@ public abstract class SongMeta
 
     public override string ToString()
     {
-        return $"{Artist} - '{Title}'";
+        return $"'{Artist} - {Title}'";
     }
 
     public virtual void CopyValues(SongMeta other)
