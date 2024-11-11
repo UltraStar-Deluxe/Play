8a7a21cc3b5791dd29fe41b1c3cfbf1ce6e870d8
Andreas Stange <anst.dev@gmail.com>
2024-11-11 20:34:36 +0100

    libs: add LrcParser

---
+++ UltraStar Play/Assets/Plugins/Opportunity.LrcParser/ParseException.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Opportunity.LrcParser
+{
+    /// <summary>
+    /// Exception in parsing.
+    /// </summary>
+    public sealed class ParseException : Exception
+    {
+        private static string generateMessage(string data, int pos, string message)
+        {
+            return $@"{message}
+Position: {pos}";
+        }
+
+        internal ParseException(string data, int pos, string message, Exception innerException)
+            : base(generateMessage(data, pos, message), innerException)
+        {
+            this.RawLyrics = data;
+            this.Position = pos;
+        }
+
+        /// <summary>
+        /// Position of exception.
+        /// </summary>
+        public int Position { get; }
+
+        /// <summary>
+        /// Raw lrc data of exception.
+        /// </summary>
+        public string RawLyrics { get; }
+    }
+}
