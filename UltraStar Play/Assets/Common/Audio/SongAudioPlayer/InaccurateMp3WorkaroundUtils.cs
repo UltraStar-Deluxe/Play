9c57ab173a9e820e7d9a560ffc4182edd53b7629
Andreas Stange <anst.dev@gmail.com>
2024-11-18 21:40:13 +0100

    sing scene: do not stream mp3 files, as workaround for Unity's inaccuracy

---
+++ UltraStar Play/Assets/Common/Audio/SongAudioPlayer/InaccurateMp3WorkaroundUtils.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+/**
+ * Workaround for inaccurate mp3 files in Unity: load all samples upfront instead of streaming the audio.
+ * Disadvantage: Unity loads the audio in blocking way when streamAudio is false, which results in UI stutter.
+ * See https://github.com/UltraStar-Deluxe/Play/issues/504
+ */
+// TODO: Remove when fixed by Unity
+public class InaccurateMp3WorkaroundUtils
+{
+    public static bool ShouldStreamAudio(string audioUri)
+    {
+        if (audioUri.IsNullOrEmpty())
+        {
+            return true;
+        }
+
+        bool streamAudio = !audioUri.EndsWith(".mp3", StringComparison.InvariantCultureIgnoreCase);
+        if (!streamAudio)
+        {
+            Debug.Log("Not streaming audio as workaround for Unity's inaccurate mp3");
+        }
+
+        return streamAudio;
+    }
+}
