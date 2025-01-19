0dabe41a46b82978e9d06d1805635605d0ab462a
Andreas Stange <anst.dev@gmail.com>
2025-01-19 12:32:02 +0100

    online multiplayer: initialize NetworkManager singleton before awake

---
+++ UltraStar Play/Assets/Common/OnlineMultiplayer/NetworkManagerInitialization.cs
@@ -0,0 +1,45 @@
+﻿using Unity.Netcode;
+using UnityEngine;
+
+namespace CommonOnlineMultiplayer
+{
+    public static class NetworkManagerInitialization
+    {
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        private static void StaticInit()
+        {
+            InitNetworkManagerSingleton();
+        }
+
+        public static void InitNetworkManagerSingleton()
+        {
+            if (NetworkManager.Singleton != null)
+            {
+                return;
+            }
+
+            Debug.Log("Initializing NetworkManager.Singleton");
+            FindOrCreateNetworkManager().SetSingleton();
+        }
+
+        private static NetworkManager FindOrCreateNetworkManager()
+        {
+            NetworkManager networkManager = NetworkManager.Singleton;
+            if (networkManager != null)
+            {
+                return networkManager;
+            }
+
+            networkManager = GameObject.FindObjectOfType<NetworkManager>();
+            if (networkManager != null)
+            {
+                return networkManager;
+            }
+
+            GameObject networkManagerGameObject = new GameObject();
+            networkManagerGameObject.name = "NetworkManager-RuntimeCreated";
+            networkManager = networkManagerGameObject.AddComponent<NetworkManager>();
+            return networkManager;
+        }
+    }
+}
