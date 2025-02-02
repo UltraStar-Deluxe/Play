7f663591d09e5a03088a05cb50d5bdb749a76c4e
Andreas Stange <anst.dev@gmail.com>
2025-02-02 21:22:02 +0100

    remove unused methods

--- UltraStar Play/Packages/playshared/Runtime/Util/ObservableUtils.cs
+++ UltraStar Play/Packages/playshared/Runtime/Util/ObservableUtils.cs
@@ -6,35 +6,6 @@ using UnityEngine;
 
 public static class ObservableUtils
 {
-    public static IObservable<T> RunOnNewTaskAsObservableElements<T>(Func<Task<List<T>>> function, IDisposable disposable = null)
-    {
-        disposable ??= Disposable.Empty;
-
-        return Observable.Create<T>(o =>
-        {
-            Task.Run(async () =>
-            {
-                try
-                {
-                    List<T> resultList = await function();
-                    if (!resultList.IsNullOrEmpty())
-                    {
-                        resultList.ForEach(result => o.OnNext(result));
-                    }
-                    o.OnCompleted();
-                }
-                catch (Exception ex)
-                {
-                    o.OnError(ex);
-                }
-
-                return disposable;
-            });
-
-            return disposable;
-        });
-    }
-
     public static IObservable<T> RunOnNewTaskAsObservable<T>(Func<Task<T>> function, IDisposable disposable = null)
     {
         disposable = disposable ?? Disposable.Empty;
@@ -61,33 +32,6 @@ public static class ObservableUtils
             });
 
             return disposable;
-        });
-    }
-
-    public static IObservable<List<T>> AllAtOnceUntilErrorOrCompleted<T>(IObservable<T> observable, bool logError = true)
-    {
-        return Observable.Create<List<T>>(o =>
-        {
-            List<T> result = new();
-
-            observable
-                .CatchIgnore((Exception ex) =>
-                {
-                    if (logError)
-                    {
-                        Debug.LogException(ex);
-                    }
-                    o.OnNext(result);
-                    o.OnCompleted();
-                })
-                .DoOnCompleted(() =>
-                {
-                    o.OnNext(result);
-                    o.OnCompleted();
-                })
-                .Subscribe(item => result.Add(item));
-
-            return Disposable.Empty;
         });
     }
 }
