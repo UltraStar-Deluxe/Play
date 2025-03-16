28fd3752387dc7f3596a34fe1501a59f4a8c24f7
Andreas Stange <anst.dev@gmail.com>
2025-03-16 11:40:50 +0100

    companion app: generate human friendly random clientName

--- UltraStar Play Companion/Assets/Common/Settings/Settings.cs
+++ UltraStar Play Companion/Assets/Common/Settings/Settings.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using LiteNetLib;
+using RandomFriendlyNameGenerator;
 
 [Serializable]
 public class Settings : ISettings
@@ -10,7 +12,7 @@ public class Settings : ISettings
      */
     public string ClientId { get; private set; }
 
-    public string ClientName { get; set; } = "MyCompanionApp";
+    public string ClientName { get; set; } = GetRandomClientName();
 
     public string CultureInfoName { get; set; } = "en";
     public MicProfile MicProfile { get; set; } = new MicProfile();
@@ -52,5 +54,17 @@ public class Settings : ISettings
     {
         get => IsDevModeEnabled;
         set => IsDevModeEnabled = value;
+    }
+
+    private static string GetRandomClientName()
+    {
+        return NameGenerator.Identifiers.Get(
+                numberOfNamesToReturn: 10,
+                components: IdentifierComponents.Adjective | IdentifierComponents.Animal,
+                orderStyle: NameOrderingStyle.SilentBobStyle,
+                separator: null,
+                forceSingleLetter: true)
+            // Take the shortest name
+            .FindMinElement(name => name.Length);
     }
 }
