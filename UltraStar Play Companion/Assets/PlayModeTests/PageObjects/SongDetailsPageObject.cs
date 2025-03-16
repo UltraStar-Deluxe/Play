e14de6d018e45c0dfa6f762df64cd37cef86f4fe
Andreas Stange <anst.dev@gmail.com>
2025-03-16 13:47:40 +0100

    companion app: test song details

---
+++ UltraStar Play Companion/Assets/PlayModeTests/PageObjects/SongDetailsPageObject.cs
@@ -0,0 +1,29 @@
+﻿using UniInject;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class SongDetailsPageObject : INeedInjection, IInjectionFinishedListener
+{
+    [Inject]
+    private Injector injector;
+
+    [Inject(UxmlName = R.UxmlNames.songDetailsContainer)]
+    private VisualElement songDetailsContainer;
+
+    private SongListPageObject songListPageObject;
+
+    public void OnInjectionFinished()
+    {
+        songListPageObject = injector.CreateAndInject<SongListPageObject>();
+    }
+
+    public async Awaitable OpenSongDetailsAsync(string songTitle)
+    {
+        await songListPageObject.OpenSongListAsync();
+
+        songListPageObject.SetSearchText(songTitle);
+        songListPageObject.GetFirstSongEntryButton().SendClickEvent();
+        await ConditionUtils.WaitForConditionAsync(() => songDetailsContainer.IsVisibleByDisplay(),
+            new WaitForConditionConfig { description = "shows song details" });
+    }
+}
---
+++ UltraStar Play Companion/Assets/PlayModeTests/SongDetailsTest.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using UniInject;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.UIElements;
+
+public class SongDetailsTest : AbstractConnectedCompanionAppPlayModeTest, IInjectionFinishedListener
+{
+    private readonly string songTitle = "Kryptonite";
+    private readonly string songArtist = "3 Doors Down";
+
+    [Inject]
+    private Injector injector;
+
+    [Inject(UxmlName = R.UxmlNames.songImage)]
+    private VisualElement songImage;
+
+    [Inject(UxmlName = R.UxmlNames.songArtistLabel)]
+    private Label songArtistLabel;
+
+    [Inject(UxmlName = R.UxmlNames.songTitleLabel)]
+    private Label songTitleLabel;
+
+    [Inject(UxmlName = R.UxmlNames.favoriteButton)]
+    private Button favoriteButton;
+
+    [Inject(UxmlName = R.UxmlNames.favoriteIcon)]
+    private VisualElement favoriteIcon;
+
+    [Inject(UxmlName = R.UxmlNames.noFavoriteIcon)]
+    private VisualElement noFavoriteIcon;
+
+    private SongDetailsPageObject songDetailsPageObject;
+
+    public void OnInjectionFinished()
+    {
+        songDetailsPageObject = injector.CreateAndInject<SongDetailsPageObject>();
+    }
+
+    [UnityTest]
+    public IEnumerator ShouldOpenSongDetails() => ShouldOpenSongDetailsAsync();
+    private async Awaitable ShouldOpenSongDetailsAsync()
+    {
+        LogAssertUtils.IgnoreFailingMessages();
+
+        // When
+        await songDetailsPageObject.OpenSongDetailsAsync(songTitle);
+
+        // Then
+        await ConditionUtils.WaitForConditionAsync(() => songTitleLabel.text == songTitle,
+            new WaitForConditionConfig { description = "shows song title" });
+
+        await ConditionUtils.WaitForConditionAsync(() => songArtistLabel.text == songArtist,
+            new WaitForConditionConfig { description = "shows song artist" });
+
+        await ConditionUtils.WaitForConditionAsync(() => songImage.resolvedStyle.backgroundImage.sprite != null,
+            new WaitForConditionConfig { description = "shows cover image" });
+    }
+
+    [UnityTest]
+    public IEnumerator ToggleFavoriteShouldChangeIcon() => ToggleFavoriteShouldChangeIconAsync();
+    private async Awaitable ToggleFavoriteShouldChangeIconAsync()
+    {
+        // Given
+        bool favoriteIconVisibleByDisplay = favoriteIcon.IsVisibleByDisplay();
+        bool noFavoriteIconVisibleByDisplay = noFavoriteIcon.IsVisibleByDisplay();
+
+        // When
+        await songDetailsPageObject.OpenSongDetailsAsync(songTitle);
+        favoriteButton.SendClickEvent();
+
+        // Then
+        await ConditionUtils.WaitForConditionAsync(() =>
+                favoriteIcon.IsVisibleByDisplay() == !favoriteIconVisibleByDisplay
+                && noFavoriteIcon.IsVisibleByDisplay() == !noFavoriteIconVisibleByDisplay,
+            new WaitForConditionConfig { description = "has toggled favorite icon" });
+    }
+}
