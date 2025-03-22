79eb525c882d97a4406776877d5dfdb943db7b0d
Andreas Stange <anst.dev@gmail.com>
2025-03-22 16:05:36 +0100

    add DirectoryScanner, similar to FileScanner

---
+++ UltraStar Play/Assets/Editor/Tests/DirectoryScannerTest/DirectoryScannerTest.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
+public class DirectoryScannerTest
+{
+    private static readonly string folderPath = $"{Application.dataPath}/Editor/Tests/DirectoryScannerTest/";
+
+    private static readonly string searchPattern = "*A";
+
+    [Test]
+    public void NonRecursive()
+    {
+        List<string> folders = DirectoryScanner.GetFolders(folderPath,
+            new DirectoryScannerConfig() { ExcludeHiddenFolders = false, Recursive = false });
+        Assert.AreEqual(2, folders.Count);
+    }
+
+    [Test]
+    public void NonRecursiveSearchPattern()
+    {
+        List<string> folders = DirectoryScanner.GetFolders(folderPath,
+            new DirectoryScannerConfig(searchPattern) { ExcludeHiddenFolders = false, Recursive = false });
+        Assert.AreEqual(1, folders.Count);
+    }
+
+    [Test]
+    public void Recursive()
+    {
+        List<string> folders = DirectoryScanner.GetFolders(folderPath,
+            new DirectoryScannerConfig() { ExcludeHiddenFolders = false, Recursive = true });
+        Assert.AreEqual(5, folders.Count);
+    }
+
+    [Test]
+    public void RecursiveSearchPattern()
+    {
+        List<string> folders = DirectoryScanner.GetFolders(folderPath,
+            new DirectoryScannerConfig(searchPattern) { ExcludeHiddenFolders = false, Recursive = true });
+        Assert.AreEqual(2, folders.Count);
+    }
+
+    [Test]
+    public void RecursiveExcludeHiddenFolders()
+    {
+        List<string> folders = DirectoryScanner.GetFolders(folderPath,
+            new DirectoryScannerConfig() { ExcludeHiddenFolders = true, Recursive = true });
+        Assert.AreEqual(4, folders.Count);
+    }
+}
---
+++ UltraStar Play/Packages/playshared/Runtime/DirectoryScanner/DirectoryScannerConfig.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+public class DirectoryScannerConfig
+{
+    public IReadOnlyCollection<string> SearchPatterns { get; set; }
+    public bool ExcludeHiddenFolders { get; set; } = true;
+    public bool Recursive { get; set; }
+
+    public DirectoryScannerConfig()
+    {
+        SearchPatterns = new List<string> { "*" };
+    }
+
+    public DirectoryScannerConfig(IReadOnlyCollection<string> searchPatterns)
+    {
+        SearchPatterns = searchPatterns;
+    }
+
+    public DirectoryScannerConfig(params string[] fileExtensionPatterns)
+    {
+        SearchPatterns = fileExtensionPatterns;
+    }
+}
