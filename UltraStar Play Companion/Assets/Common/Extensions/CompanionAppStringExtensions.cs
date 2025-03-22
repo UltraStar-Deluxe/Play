d4077e00e49309b42e2138ced7e70e31a899174c
Andreas Stange <anst.dev@gmail.com>
2025-03-22 16:54:41 +0100

    companion app: fix compiler error after refactoring

---
+++ UltraStar Play Companion/Assets/Common/Extensions/CompanionAppStringExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public static class CompanionAppStringExtensions
+{
+    public static string ReplaceOrThrow(this string text, string pattern, string replacement)
+    {
+        if (!text.Contains(pattern))
+        {
+            throw new ArgumentException($"Text '{text}' does not contain pattern '{pattern}'");
+        }
+        return text.Replace(pattern, replacement);
+    }
+}
