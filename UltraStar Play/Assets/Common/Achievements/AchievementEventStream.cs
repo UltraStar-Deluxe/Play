1154742855720c03d6e9c89aafe33a8cc0ce337e
Andreas Stange <anst.dev@gmail.com>
2025-05-06 17:30:24 +0200

    reintegrate: moved common achievement code out of Steam package

---
+++ UltraStar Play/Assets/Common/Achievements/AchievementEventStream.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UniRx;
+
+public class AchievementEventStream : IDisposable, IObservable<AchievementEvent>
+{
+    public static AchievementEventStream Instance { get; private set; } = new();
+
+    private readonly Subject<AchievementEvent> subject = new();
+
+    private AchievementEventStream()
+    {
+        // private constructor for singleton.
+    }
+
+    public void Dispose()
+    {
+        subject.Dispose();
+    }
+
+    public void OnNext(AchievementEvent value)
+    {
+        subject.OnNext(value);
+    }
+
+    public IDisposable Subscribe(IObserver<AchievementEvent> observer)
+    {
+        return subject.Subscribe(observer);
+    }
+}
