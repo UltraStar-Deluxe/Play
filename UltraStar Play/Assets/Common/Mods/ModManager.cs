1a17720b5dc0b59bb6bec40ff2aa6844acce03c6
Andreas Stange <anst.dev@gmail.com>
2025-10-12 16:40:43 +0200

    mods: fix listing mod interfaces

--- UltraStar Play/Assets/Common/Mods/ModManager.cs
+++ UltraStar Play/Assets/Common/Mods/ModManager.cs
@@ -289,8 +289,25 @@ public class ModManager : AbstractSingletonBehaviour, INeedInjection
     private List<Type> GetModInterfaces()
     {
         return AppDomain.CurrentDomain.GetAssemblies()
-            .SelectMany(domainAssembly => domainAssembly.GetTypes())
-            .Where(type => type.IsInterface
+            .SelectMany(assembly => 
+            {
+                try
+                {
+                    return assembly.GetTypes();
+                }
+                catch (Exception ex)
+                {
+                    Debug.LogError($"Failed to get types from assembly {assembly}: " + ex.Message);
+                    Debug.LogException(ex);
+                    if (ex is ReflectionTypeLoadException reflectionTypeLoadException)
+                    {
+                        // Return types that have been loaded so far.
+                        return reflectionTypeLoadException.Types.Where(t => t != null);
+                    }
+                    return Enumerable.Empty<Type>();
+                }
+            })
+            .Where(type => type.IsInterface 
                            && typeof(IMod).IsAssignableFrom(type))
             .ToList();
     }
