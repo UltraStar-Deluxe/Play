6ec3ddce01b1bd19b9c0e7c919b34a269566e173
Andreas Stange <anst.dev@gmail.com>
2026-01-06 17:46:46 +0100

    test that PortAudio includes ASIO Host API

---
+++ UltraStar Play/Assets/Editor/Tests/PortAudioHostApiTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using PortAudioForUnity;
+using UnityEngine;
+
+namespace Editor.Tests
+{
+    public class PortAudioHostApiTest
+    {
+        [Test]
+        public void ShouldIncludeAsioOnWindows()
+        {
+            if (!PlatformUtils.IsWindows)
+            {
+                Debug.Log("Not on Windows, skipping test");
+                return;
+            }
+
+            List<PortAudioHostApi> hostApis = PortAudioUtils.HostApis
+                .Select(portAudioHostApi => PortAudioConversionUtils.ConvertHostApi(portAudioHostApi))
+                .ToList();
+            Debug.Log("Host APIs: " + hostApis.JoinWith(", "));
+            Assert.Contains(PortAudioHostApi.ASIO, hostApis);
+        }
+    }
+}
