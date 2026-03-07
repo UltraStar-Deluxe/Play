0f4ff45da46d3ddd146e924ec8c297f9c40ac33c
Andreas Stange <anst.dev@gmail.com>
2026-03-07 21:11:36 +0100

    sing scene: hide all lyrics box after last phrase, not only the lyrics container

--- UltraStar Play/Assets/Scenes/Sing/Lyrics/SingingLyricsControl.cs
+++ UltraStar Play/Assets/Scenes/Sing/Lyrics/SingingLyricsControl.cs
@@ -11,7 +11,6 @@ using UnityEngine.UIElements;
 
 public class SingingLyricsControl : INeedInjection, IInjectionFinishedListener
 {
-    private const float SpaceWidthInPx = 8;
     private const float MinFontSize = 4;
     private const float MaxFontSizeIterations = 20;
 
@@ -275,13 +274,16 @@ public class SingingLyricsControl : INeedInjection, IInjectionFinishedListener
         }
         else
         {
-            // After last sentence => fade out the current lyrics
+            // After last sentence => fade out the lyrics box
             SortedNotes = new List<Note>();
-            LeanTween.value(gameObject, currentSentenceContainer.resolvedStyle.opacity, 0, 1f)
-                .setOnUpdate(interpolatedValue =>
-                {
-                    currentSentenceContainer.style.opacity = interpolatedValue;
-                });
+            AwaitableUtils.ExecuteAfterDelayInSecondsAsync(gameObject, 2, () =>
+            {
+                LeanTween.value(gameObject, rootVisualElement.resolvedStyle.opacity, 0, 1f)
+                    .setOnUpdate(interpolatedValue =>
+                    {
+                        rootVisualElement.style.opacity = interpolatedValue;
+                    });
+            });
         }
     }
 
