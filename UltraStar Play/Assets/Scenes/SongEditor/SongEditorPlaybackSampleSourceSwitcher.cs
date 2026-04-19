423c8c47d2726973afce70f604f7570142b16ad4
Andreas Stange <anst.dev@gmail.com>
2026-04-19 20:20:18 +0200

    song editor: show notification when automatically switching playback audio

--- UltraStar Play/Assets/Scenes/SongEditor/SongEditorPlaybackSampleSourceSwitcher.cs
+++ UltraStar Play/Assets/Scenes/SongEditor/SongEditorPlaybackSampleSourceSwitcher.cs
@@ -24,20 +24,30 @@ public class SongEditorPlaybackSampleSourceSwitcher : INeedInjection, IInjection
     
     public void OnInjectionFinished()
     {
+        // Delay by 1 frame such that the song editor can initialize first
+        _ = AwaitableUtils.ExecuteAfterDelayInFramesAsync(1, SwitchPlaybackSourceIfNeeded);
+    }
+
+    private void SwitchPlaybackSourceIfNeeded()
+    {
         // Switch to original audio
         if (settings.SongEditorSettings.PlaybackSamplesSource is ESongEditorSamplesSource.Vocals
             && !SongMetaUtils.VocalsAudioResourceExists(songMeta))
         {
             settings.SongEditorSettings.PlaybackSamplesSource = ESongEditorSamplesSource.OriginalMusic;
+            Log.WithClassContext().Information(() => "Switched playback to original audio");
+            NotificationManager.CreateNotification(Translation.Of("No vocals audio found.\nSwitched playback to original audio."));
         }
-        
+
         // Switch to vocals
         audioSeparationManager.AudioSeparationFinishedEventStream
-            .Subscribe(evt =>
+            .Subscribe(_ =>
             {
                 if (settings.SongEditorSettings.PlaybackSamplesSource is ESongEditorSamplesSource.OriginalMusic)
                 {
                     settings.SongEditorSettings.PlaybackSamplesSource = ESongEditorSamplesSource.Vocals;
+                    Log.WithClassContext().Information(() => "Switched playback to vocals audio");
+                    NotificationManager.CreateNotification(Translation.Of("Switched playback to vocals audio."));
                 }
             })
             .AddTo(gameObject);
