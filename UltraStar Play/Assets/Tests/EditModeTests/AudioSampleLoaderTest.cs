8cd37f33f6d708475552ac6b80aac2d126d4a29e
Andreas Stange <anst.dev@gmail.com>
2026-06-24 17:09:32 +0200

    tests: fix missing dependency in dummy object, therefore moved test fixtures to EditModeTests

--- UltraStar Play/Assets/Tests/EditModeTests/AudioSampleLoaderTest.cs
+++ UltraStar Play/Assets/Tests/EditModeTests/AudioSampleLoaderTest.cs
@@ -1,6 +1,8 @@
 ﻿using System.Linq;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using UniInject;
+using UniInject.Extensions;
 using UnityEngine;
 
 public class AudioSampleLoaderTest
@@ -16,6 +18,10 @@ public class AudioSampleLoaderTest
         try
         {
             AudioSampleLoader audioSampleLoader = gameObject.AddComponent<AudioSampleLoader>();
+            UniInjectUtils.GlobalInjector
+                .WithBindingForInstance(new Settings())
+                .Inject(audioSampleLoader);
+
             AudioClip audioClip = await audioSampleLoader.LoadAsAudioClip(videoFilePath);
 
             Assert.NotNull(audioClip);
